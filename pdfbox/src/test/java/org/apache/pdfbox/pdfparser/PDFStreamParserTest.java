9eecb20f9091cbe55233930712dbf551277c9a23
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:41:04 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/pdfparser/PDFStreamParserTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdfparser/PDFStreamParserTest.java
@@ -17,108 +17,106 @@ package org.apache.pdfbox.pdfparser;
 
 import java.io.IOException;
 import java.util.List;
-import junit.framework.TestCase;
+
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
+import org.junit.Assert;
 
-import static org.junit.Assert.assertArrayEquals;
+import junit.framework.TestCase;
 
 /**
  * Tests for PDFStreamParser.
  *
  * @author Tilman Hausherr
  */
-public class PDFStreamParserTest extends TestCase
-{
+public class PDFStreamParserTest extends TestCase {
 
-    /**
-     * Tests for inline images, whether the EI is correctly identified as
-     * ending. To test hasNoFollowingBinData(), the amount of data after EI nust
-     * be at least PDFStreamParser.MAX_BIN_CHAR_TEST_LENGTH
-     *
-     * @throws IOException
-     */
-    public void testInlineImages() throws IOException
-    {
-        testInlineImage2ops("ID\n12345EI Q", "12345", "Q");
-        testInlineImage2ops("ID\n12345EI EMC", "12345", "EMC");
-        testInlineImage2ops("ID\n12345EI Q ", "12345", "Q");
-        testInlineImage2ops("ID\n12345EI EMC ", "12345", "EMC");
-        testInlineImage2ops("ID\n12345EI  Q", "12345", "Q");
-        testInlineImage2ops("ID\n12345EI  EMC", "12345", "EMC");
-        testInlineImage2ops("ID\n12345EI  Q ", "12345", "Q");
-        testInlineImage2ops("ID\n12345EI  EMC ", "12345", "EMC");
+  /**
+   * Tests for inline images, whether the EI is correctly identified as ending. To
+   * test hasNoFollowingBinData(), the amount of data after EI nust be at least
+   * PDFStreamParser.MAX_BIN_CHAR_TEST_LENGTH
+   *
+   * @throws IOException
+   */
+  public void testInlineImages() throws IOException {
+    testInlineImage2ops("ID\n12345EI Q", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI EMC", "12345", "EMC");
+    testInlineImage2ops("ID\n12345EI Q ", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI EMC ", "12345", "EMC");
+    testInlineImage2ops("ID\n12345EI  Q", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI  EMC", "12345", "EMC");
+    testInlineImage2ops("ID\n12345EI  Q ", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI  EMC ", "12345", "EMC");
 
-        testInlineImage2ops("ID\n12345EI \000Q", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI \000Q", "12345", "Q");
 
-        testInlineImage2ops("ID\n12345EI Q                             ", "12345", "Q");
-        testInlineImage2ops("ID\n12345EI EMC                           ", "12345", "EMC");
+    testInlineImage2ops("ID\n12345EI Q                             ", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI EMC                           ", "12345", "EMC");
 
-        testInlineImage1op("ID\n12345EI", "12345");
-        testInlineImage1op("ID\n12345EI                               ", "12345");
+    testInlineImage1op("ID\n12345EI", "12345");
+    testInlineImage1op("ID\n12345EI                               ", "12345");
 
-        testInlineImage2ops("ID\n12345EI                               Q ", "12345", "Q");
-        testInlineImage2ops("ID\n12345EI                               EMC ", "12345", "EMC");
-        testInlineImage2ops("ID\n12345EI                               Q", "12345", "Q");
-        testInlineImage2ops("ID\n12345EI                               EMC", "12345", "EMC");
+    testInlineImage2ops("ID\n12345EI                               Q ", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI                               EMC ", "12345", "EMC");
+    testInlineImage2ops("ID\n12345EI                               Q", "12345", "Q");
+    testInlineImage2ops("ID\n12345EI                               EMC", "12345", "EMC");
 
-        testInlineImage1op("ID\n12EI5EI", "12EI5");
-        testInlineImage1op("ID\n12EI5EI ", "12EI5");
-        testInlineImage1op("ID\n12EI5EIQEI", "12EI5EIQ");
-        testInlineImage2ops("ID\n12EI5EIQEI Q", "12EI5EIQ", "Q");
-        testInlineImage2ops("ID\n12EI5EI Q", "12EI5", "Q");
-        testInlineImage2ops("ID\n12EI5EI Q ", "12EI5", "Q");
-        testInlineImage2ops("ID\n12EI5EI EMC", "12EI5", "EMC");
-        testInlineImage2ops("ID\n12EI5EI EMC ", "12EI5", "EMC");
-        testInlineImage2ops("ID\n12EI5EI                                Q", "12EI5", "Q");
-        testInlineImage2ops("ID\n12EI5EI                                Q ", "12EI5", "Q");
-        testInlineImage2ops("ID\n12EI5EI                                EMC", "12EI5", "EMC");
-        testInlineImage2ops("ID\n12EI5EI                                EMC ", "12EI5", "EMC");
+    testInlineImage1op("ID\n12EI5EI", "12EI5");
+    testInlineImage1op("ID\n12EI5EI ", "12EI5");
+    testInlineImage1op("ID\n12EI5EIQEI", "12EI5EIQ");
+    testInlineImage2ops("ID\n12EI5EIQEI Q", "12EI5EIQ", "Q");
+    testInlineImage2ops("ID\n12EI5EI Q", "12EI5", "Q");
+    testInlineImage2ops("ID\n12EI5EI Q ", "12EI5", "Q");
+    testInlineImage2ops("ID\n12EI5EI EMC", "12EI5", "EMC");
+    testInlineImage2ops("ID\n12EI5EI EMC ", "12EI5", "EMC");
+    testInlineImage2ops("ID\n12EI5EI                                Q", "12EI5", "Q");
+    testInlineImage2ops("ID\n12EI5EI                                Q ", "12EI5", "Q");
+    testInlineImage2ops("ID\n12EI5EI                                EMC", "12EI5", "EMC");
+    testInlineImage2ops("ID\n12EI5EI                                EMC ", "12EI5", "EMC");
 
-        // MAX_BIN_CHAR_TEST_LENGTH is currently 10, test boundaries
-        //                              1234567890
-        testInlineImage2ops("ID\n12EI5EI       EMC ", "12EI5", "EMC");
-        testInlineImage2ops("ID\n12EI5EI        EMC ", "12EI5", "EMC");
-        testInlineImage2ops("ID\n12EI5EI         EMC ", "12EI5", "EMC");
-        testInlineImage2ops("ID\n12EI5EI          EMC ", "12EI5", "EMC");
-        testInlineImage2ops("ID\n12EI5EI       Q   ", "12EI5", "Q");
-        testInlineImage2ops("ID\n12EI5EI        Q   ", "12EI5", "Q");
-        testInlineImage2ops("ID\n12EI5EI         Q   ", "12EI5", "Q");
-        testInlineImage2ops("ID\n12EI5EI          Q   ", "12EI5", "Q");
-    }
+    // MAX_BIN_CHAR_TEST_LENGTH is currently 10, test boundaries
+    // 1234567890
+    testInlineImage2ops("ID\n12EI5EI       EMC ", "12EI5", "EMC");
+    testInlineImage2ops("ID\n12EI5EI        EMC ", "12EI5", "EMC");
+    testInlineImage2ops("ID\n12EI5EI         EMC ", "12EI5", "EMC");
+    testInlineImage2ops("ID\n12EI5EI          EMC ", "12EI5", "EMC");
+    testInlineImage2ops("ID\n12EI5EI       Q   ", "12EI5", "Q");
+    testInlineImage2ops("ID\n12EI5EI        Q   ", "12EI5", "Q");
+    testInlineImage2ops("ID\n12EI5EI         Q   ", "12EI5", "Q");
+    testInlineImage2ops("ID\n12EI5EI          Q   ", "12EI5", "Q");
+  }
 
-    // checks whether there are two operators, one inline image and the named operator
-    private void testInlineImage2ops(String s, String imageDataString, String opName) throws IOException
-    {
-        List<Object> tokens = parseTokenString(s);
+  // checks whether there are two operators, one inline image and the named
+  // operator
+  private void testInlineImage2ops(final String s, final String imageDataString, final String opName)
+      throws IOException {
+    final List<Object> tokens = parseTokenString(s);
 
-        assertEquals(2, tokens.size());
+    TestCase.assertEquals(2, tokens.size());
 
-        assertEquals(OperatorName.BEGIN_INLINE_IMAGE_DATA, ((Operator) tokens.get(0)).getName());
-        assertEquals(imageDataString.length(), ((Operator) tokens.get(0)).getImageData().length);
-        assertArrayEquals(imageDataString.getBytes(), ((Operator) tokens.get(0)).getImageData());
+    TestCase.assertEquals(OperatorName.BEGIN_INLINE_IMAGE_DATA, ((Operator) tokens.get(0)).getName());
+    TestCase.assertEquals(imageDataString.length(), ((Operator) tokens.get(0)).getImageData().length);
+    Assert.assertArrayEquals(imageDataString.getBytes(), ((Operator) tokens.get(0)).getImageData());
 
-        assertEquals(opName, ((Operator) tokens.get(1)).getName());
-    }
+    TestCase.assertEquals(opName, ((Operator) tokens.get(1)).getName());
+  }
 
-    // checks whether there is one operator, one inline image
-    private void testInlineImage1op(String s, String imageDataString) throws IOException
-    {
-        List<Object> tokens = parseTokenString(s);
+  // checks whether there is one operator, one inline image
+  private void testInlineImage1op(final String s, final String imageDataString) throws IOException {
+    final List<Object> tokens = parseTokenString(s);
 
-        assertEquals(1, tokens.size());
+    TestCase.assertEquals(1, tokens.size());
 
-        assertEquals(OperatorName.BEGIN_INLINE_IMAGE_DATA, ((Operator) tokens.get(0)).getName());
-        assertEquals(imageDataString.length(), ((Operator) tokens.get(0)).getImageData().length);
-        assertArrayEquals(imageDataString.getBytes(), ((Operator) tokens.get(0)).getImageData());
-    }
+    TestCase.assertEquals(OperatorName.BEGIN_INLINE_IMAGE_DATA, ((Operator) tokens.get(0)).getName());
+    TestCase.assertEquals(imageDataString.length(), ((Operator) tokens.get(0)).getImageData().length);
+    Assert.assertArrayEquals(imageDataString.getBytes(), ((Operator) tokens.get(0)).getImageData());
+  }
 
-    // parse string and return list of tokens
-    private List<Object> parseTokenString(String s) throws IOException
-    {
-        PDFStreamParser pdfStreamParser = new PDFStreamParser(s.getBytes());
-        pdfStreamParser.parse();
-        return pdfStreamParser.getTokens();
-    }
+  // parse string and return list of tokens
+  private List<Object> parseTokenString(final String s) throws IOException {
+    final PDFStreamParser pdfStreamParser = new PDFStreamParser(s.getBytes());
+    pdfStreamParser.parse();
+    return pdfStreamParser.getTokens();
+  }
 
 }
