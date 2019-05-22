d6b100c1a93ec1099babd0c21cb1de94082c16ac
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:34:47 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetMatrix.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetMatrix.java
@@ -31,37 +31,30 @@ import org.apache.pdfbox.util.Matrix;
  *
  * @author Laurent Huault
  */
-public class SetMatrix extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws MissingOperandException
-    {
-        if (arguments.size() < 6)
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
-        if (!checkArrayTypesClass(arguments, COSNumber.class))
-        {
-            return;
-        }
+public class SetMatrix extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws MissingOperandException {
+    if (arguments.size() < 6)
+      throw new MissingOperandException(operator, arguments);
+    if (!checkArrayTypesClass(arguments, COSNumber.class))
+      return;
 
-        COSNumber a = (COSNumber)arguments.get( 0 );
-        COSNumber b = (COSNumber)arguments.get( 1 );
-        COSNumber c = (COSNumber)arguments.get( 2 );
-        COSNumber d = (COSNumber)arguments.get( 3 );
-        COSNumber e = (COSNumber)arguments.get( 4 );
-        COSNumber f = (COSNumber)arguments.get( 5 );
+    final COSNumber a = (COSNumber) arguments.get(0);
+    final COSNumber b = (COSNumber) arguments.get(1);
+    final COSNumber c = (COSNumber) arguments.get(2);
+    final COSNumber d = (COSNumber) arguments.get(3);
+    final COSNumber e = (COSNumber) arguments.get(4);
+    final COSNumber f = (COSNumber) arguments.get(5);
 
-        Matrix matrix = new Matrix(a.floatValue(), b.floatValue(), c.floatValue(),
-                                   d.floatValue(), e.floatValue(), f.floatValue());
+    final Matrix matrix = new Matrix(a.floatValue(), b.floatValue(), c.floatValue(), d.floatValue(), e.floatValue(),
+        f.floatValue());
 
-        context.setTextMatrix(matrix);
-        context.setTextLineMatrix(matrix.clone());
-    }
+    context.setTextMatrix(matrix);
+    context.setTextLineMatrix(matrix.clone());
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_MATRIX;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_MATRIX;
+  }
 }
