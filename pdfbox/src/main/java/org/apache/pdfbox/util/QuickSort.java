1a128a695ceb94298c074dc1381ff4e4be85c38d
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:05:23 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/util/QuickSort.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/QuickSort.java
@@ -23,113 +23,89 @@ import java.util.List;
 
 /**
  * see http://de.wikipedia.org/wiki/Quicksort.
- * 
+ *
  * @author Uwe Pachler
  * @author Manuel Aristaran
  */
-public final class QuickSort
-{
-    
-    private QuickSort()
-    {
+public final class QuickSort {
+
+  private QuickSort() {
+  }
+
+  private static final Comparator<? extends Comparable> OBJCOMP = (object1, object2) -> object1.compareTo(object2);
+
+  /**
+   * Sorts the given list using the given comparator.
+   *
+   * @param <T>  type of the objects to be sorted.
+   * @param list list to be sorted
+   * @param cmp  comparator used to compare the objects within the list
+   */
+  public static <T> void sort(final List<T> list, final Comparator<T> cmp) {
+    final int size = list.size();
+    if (size < 2)
+      return;
+    QuickSort.quicksort(list, cmp);
+  }
+
+  /**
+   * Sorts the given list using compareTo as comparator.
+   *
+   * @param <T>  type of the objects to be sorted.
+   * @param list list to be sorted
+   */
+  public static <T extends Comparable> void sort(final List<T> list) {
+    QuickSort.sort(list, (Comparator<T>) QuickSort.OBJCOMP);
+  }
+
+  private static <T> void quicksort(final List<T> list, final Comparator<T> cmp) {
+    final Deque<Integer> stack = new ArrayDeque<>();
+    stack.push(0);
+    stack.push(list.size());
+    while (!stack.isEmpty()) {
+      final int right = stack.pop();
+      final int left = stack.pop();
+      if (right - left < 2) {
+        continue;
+      }
+      int p = left + (right - left) / 2;
+      p = QuickSort.partition(list, cmp, p, left, right);
+
+      stack.push(p + 1);
+      stack.push(right);
+
+      stack.push(left);
+      stack.push(p);
     }
-    
-    private static final Comparator<? extends Comparable> OBJCOMP = new Comparator<Comparable>()
-    {
-        @Override
-        public int compare(Comparable object1, Comparable object2)
-        {
-            return object1.compareTo(object2);
-        }
-    };
+  }
 
-    /**
-     * Sorts the given list using the given comparator.
-     * 
-     * @param <T> type of the objects to be sorted.
-     * @param list list to be sorted
-     * @param cmp comparator used to compare the objects within the list
-     */
-    public static <T> void sort(List<T> list, Comparator<T> cmp)
-    {
-        int size = list.size();
-        if (size < 2)
-        {
-            return;
-        }
-        quicksort(list, cmp);
+  private static <T> int partition(final List<T> list, final Comparator<T> cmp, final int p, final int start,
+      final int end) {
+    int l = start;
+    int h = end - 2;
+    final T piv = list.get(p);
+    QuickSort.swap(list, p, end - 1);
+
+    while (l < h) {
+      if (cmp.compare(list.get(l), piv) <= 0) {
+        l++;
+      } else if (cmp.compare(piv, list.get(h)) <= 0) {
+        h--;
+      } else {
+        QuickSort.swap(list, l, h);
+      }
     }
-
-    /**
-     * Sorts the given list using compareTo as comparator.
-     * 
-     * @param <T> type of the objects to be sorted.
-     * @param list list to be sorted
-     */
-    public static <T extends Comparable> void sort(List<T> list)
-    {
-        sort(list, (Comparator<T>) OBJCOMP);
+    int idx = h;
+    if (cmp.compare(list.get(h), piv) < 0) {
+      idx++;
     }
+    QuickSort.swap(list, end - 1, idx);
+    return idx;
+  }
 
-    private static <T> void quicksort(List<T> list, Comparator<T> cmp)
-    {
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(0);
-        stack.push(list.size());
-        while (!stack.isEmpty())
-        {
-            int right = stack.pop();
-            int left = stack.pop();
-            if (right - left < 2)
-            {
-                continue;
-            }
-            int p = left + ((right - left) / 2);
-            p = partition(list, cmp, p, left, right);
-
-            stack.push(p + 1);
-            stack.push(right);
-
-            stack.push(left);
-            stack.push(p);
-        }
-    }
-
-    private static <T> int partition(List<T> list, Comparator<T> cmp, int p, int start, int end)
-    {
-        int l = start;
-        int h = end - 2;
-        T piv = list.get(p);
-        swap(list, p, end - 1);
-
-        while (l < h)
-        {
-            if (cmp.compare(list.get(l), piv) <= 0)
-            {
-                l++;
-            }
-            else if (cmp.compare(piv, list.get(h)) <= 0)
-            {
-                h--;
-            }
-            else
-            {
-                swap(list, l, h);
-            }
-        }
-        int idx = h;
-        if (cmp.compare(list.get(h), piv) < 0)
-        {
-            idx++;
-        }
-        swap(list, end - 1, idx);
-        return idx;
-    }
-
-    private static <T> void swap(List<T> list, int i, int j)
-    {
-        T tmp = list.get(i);
-        list.set(i, list.get(j));
-        list.set(j, tmp);
-    }
+  private static <T> void swap(final List<T> list, final int i, final int j) {
+    final T tmp = list.get(i);
+    list.set(i, list.get(j));
+    list.set(j, tmp);
+  }
 }
