a7e6028ec932f21350d2f95cd7cf866fb1f3a99a
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:36:51 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/ShowTextLineAndSpace.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/ShowTextLineAndSpace.java
@@ -16,38 +16,32 @@
  */
 package org.apache.pdfbox.contentstream.operator.text;
 
+import java.io.IOException;
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
-
-import java.io.IOException;
-import org.apache.pdfbox.contentstream.operator.MissingOperandException;
+import org.apache.pdfbox.cos.COSBase;
 
 /**
  * ": Set word and character spacing, move to next line, and show text.
  *
  * @author Laurent Huault
  */
-public class ShowTextLineAndSpace extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        if (arguments.size() < 3)
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
-        context.processOperator(OperatorName.SET_WORD_SPACING, arguments.subList(0, 1));
-        context.processOperator(OperatorName.SET_CHAR_SPACING, arguments.subList(1, 2));
-        context.processOperator(OperatorName.SHOW_TEXT_LINE, arguments.subList(2, 3));
-    }
+public class ShowTextLineAndSpace extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    if (arguments.size() < 3)
+      throw new MissingOperandException(operator, arguments);
+    context.processOperator(OperatorName.SET_WORD_SPACING, arguments.subList(0, 1));
+    context.processOperator(OperatorName.SET_CHAR_SPACING, arguments.subList(1, 2));
+    context.processOperator(OperatorName.SHOW_TEXT_LINE, arguments.subList(2, 3));
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SHOW_TEXT_LINE_AND_SPACE;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SHOW_TEXT_LINE_AND_SPACE;
+  }
 }
