5a743c5f2794daa5fb10484e7f5203adc0009fca
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:37:55 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/Subsetter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/Subsetter.java
@@ -22,19 +22,18 @@ import java.io.IOException;
 /**
  * Interface for a font subsetter.
  */
-interface Subsetter
-{
-    /**
-     * Adds the given Unicode code point to this subset.
-     * 
-     * @param codePoint Unicode code point
-     */
-    void addToSubset(int codePoint);
+interface Subsetter {
+  /**
+   * Adds the given Unicode code point to this subset.
+   *
+   * @param codePoint Unicode code point
+   */
+  void addToSubset(int codePoint);
 
-    /**
-     * Subset this font now.
-     * 
-     * @throws IOException if the font could not be read
-     */
-    void subset() throws IOException;
+  /**
+   * Subset this font now.
+   *
+   * @throws IOException if the font could not be read
+   */
+  void subset() throws IOException;
 }
