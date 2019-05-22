819e4b4cac32c331ce8b1d8addfbe6c529628c84
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:42:58 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/EndPath.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/EndPath.java
@@ -19,26 +19,23 @@ package org.apache.pdfbox.contentstream.operator.graphics;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
+import org.apache.pdfbox.cos.COSBase;
 
 /**
  * n End the path.
  *
  * @author Ben Litchfield
  */
-public final class EndPath extends GraphicsOperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> operands) throws IOException
-    {
-        context.endPath();
-    }
+public final class EndPath extends GraphicsOperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> operands) throws IOException {
+    context.endPath();
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.ENDPATH;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.ENDPATH;
+  }
 }
