e2a3db0977e647ef045a81c5548133d59df1a4fc
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:42:23 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/TestPDPageTree.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/TestPDPageTree.java
@@ -16,71 +16,58 @@
  */
 package org.apache.pdfbox.pdmodel;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * @author Andrea Vacondio
  *
  */
-public class TestPDPageTree
-{
-    private PDDocument doc;
+public class TestPDPageTree {
+  private PDDocument doc;
 
-    @After
-    public void tearDown() throws IOException
-    {
-        if (doc != null)
-        {
-            doc.close();
-        }
+  @After
+  public void tearDown() throws IOException {
+    if (doc != null) {
+      doc.close();
     }
+  }
 
-    @Test
-    public void indexOfPageFromOutlineDestination() throws IOException
-    {
-        doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("with_outline.pdf"));
-        PDDocumentOutline outline = doc.getDocumentCatalog().getDocumentOutline();
-        for (PDOutlineItem current : outline.children())
-        {
-            if (current.getTitle().contains("Second"))
-            {
-                assertEquals(2, doc.getPages().indexOf(current.findDestinationPage(doc)));
-            }
-        }
+  @Test
+  public void indexOfPageFromOutlineDestination() throws IOException {
+    doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("with_outline.pdf"));
+    final PDDocumentOutline outline = doc.getDocumentCatalog().getDocumentOutline();
+    for (final PDOutlineItem current : outline.children()) {
+      if (current.getTitle().contains("Second")) {
+        Assert.assertEquals(2, doc.getPages().indexOf(current.findDestinationPage(doc)));
+      }
     }
+  }
 
-    @Test
-    public void positiveSingleLevel() throws IOException
-    {
-        doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("with_outline.pdf"));
-        for (int i = 0; i < doc.getNumberOfPages(); i++)
-        {
-            assertEquals(i, doc.getPages().indexOf(doc.getPage(i)));
-        }
+  @Test
+  public void positiveSingleLevel() throws IOException {
+    doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("with_outline.pdf"));
+    for (int i = 0; i < doc.getNumberOfPages(); i++) {
+      Assert.assertEquals(i, doc.getPages().indexOf(doc.getPage(i)));
     }
+  }
 
-    @Test
-    public void positiveMultipleLevel() throws IOException
-    {
-        doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("page_tree_multiple_levels.pdf"));
-        for (int i = 0; i < doc.getNumberOfPages(); i++)
-        {
-            assertEquals(i, doc.getPages().indexOf(doc.getPage(i)));
-        }
+  @Test
+  public void positiveMultipleLevel() throws IOException {
+    doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("page_tree_multiple_levels.pdf"));
+    for (int i = 0; i < doc.getNumberOfPages(); i++) {
+      Assert.assertEquals(i, doc.getPages().indexOf(doc.getPage(i)));
     }
+  }
 
-    @Test
-    public void negative() throws IOException
-    {
-        doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("with_outline.pdf"));
-        assertEquals(-1, doc.getPages().indexOf(new PDPage()));
-    }
+  @Test
+  public void negative() throws IOException {
+    doc = PDDocument.load(TestPDPageTree.class.getResourceAsStream("with_outline.pdf"));
+    Assert.assertEquals(-1, doc.getPages().indexOf(new PDPage()));
+  }
 }
