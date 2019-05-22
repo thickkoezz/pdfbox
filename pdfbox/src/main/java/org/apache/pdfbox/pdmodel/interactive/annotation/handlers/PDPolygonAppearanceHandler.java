03643af8fd852b67d11a688c2df4bcdc94d693fb
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:50:51 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDPolygonAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDPolygonAppearanceHandler.java
@@ -18,17 +18,17 @@
 package org.apache.pdfbox.pdmodel.interactive.annotation.handlers;
 
 import java.io.IOException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSNumber;
+import org.apache.pdfbox.pdmodel.PDAppearanceContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
-import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationPolygon;
-import org.apache.pdfbox.pdmodel.PDAppearanceContentStream;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationMarkup;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationPolygon;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceStream;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDBorderEffectDictionary;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDBorderStyleDictionary;
@@ -37,192 +37,156 @@ import org.apache.pdfbox.pdmodel.interactive.annotation.PDBorderStyleDictionary;
  * Handler to generate the polygon annotations appearance.
  *
  */
-public class PDPolygonAppearanceHandler extends PDAbstractAppearanceHandler
-{
-    private static final Log LOG = LogFactory.getLog(PDPolygonAppearanceHandler.class);
+public class PDPolygonAppearanceHandler extends PDAbstractAppearanceHandler {
+  private static final Log LOG = LogFactory.getLog(PDPolygonAppearanceHandler.class);
 
-    public PDPolygonAppearanceHandler(PDAnnotation annotation)
-    {
-        super(annotation);
+  public PDPolygonAppearanceHandler(final PDAnnotation annotation) {
+    super(annotation);
+  }
+
+  @Override
+  public void generateAppearanceStreams() {
+    generateNormalAppearance();
+    generateRolloverAppearance();
+    generateDownAppearance();
+  }
+
+  @Override
+  public void generateNormalAppearance() {
+    final PDAnnotationPolygon annotation = (PDAnnotationPolygon) getAnnotation();
+    final float lineWidth = getLineWidth();
+    final PDRectangle rect = annotation.getRectangle();
+
+    // Adjust rectangle even if not empty
+    // CTAN-example-Annotations.pdf p2
+    float minX = Float.MAX_VALUE;
+    float minY = Float.MAX_VALUE;
+    float maxX = Float.MIN_VALUE;
+    float maxY = Float.MIN_VALUE;
+
+    final float[][] pathArray = getPathArray(annotation);
+    if (pathArray == null)
+      return;
+    for (final float[] element : pathArray) {
+      for (int j = 0; j < element.length / 2; ++j) {
+        final float x = element[j * 2];
+        final float y = element[j * 2 + 1];
+        minX = Math.min(minX, x);
+        minY = Math.min(minY, y);
+        maxX = Math.max(maxX, x);
+        maxY = Math.max(maxY, y);
+      }
     }
 
-    @Override
-    public void generateAppearanceStreams()
-    {
-        generateNormalAppearance();
-        generateRolloverAppearance();
-        generateDownAppearance();
-    }
+    rect.setLowerLeftX(Math.min(minX - lineWidth, rect.getLowerLeftX()));
+    rect.setLowerLeftY(Math.min(minY - lineWidth, rect.getLowerLeftY()));
+    rect.setUpperRightX(Math.max(maxX + lineWidth, rect.getUpperRightX()));
+    rect.setUpperRightY(Math.max(maxY + lineWidth, rect.getUpperRightY()));
+    annotation.setRectangle(rect);
 
-    @Override
-    public void generateNormalAppearance()
-    {
-        PDAnnotationPolygon annotation = (PDAnnotationPolygon) getAnnotation();
-        float lineWidth = getLineWidth();
-        PDRectangle rect = annotation.getRectangle();
+    try (PDAppearanceContentStream contentStream = getNormalAppearanceAsContentStream()) {
+      final boolean hasStroke = contentStream.setStrokingColorOnDemand(getColor());
 
-        // Adjust rectangle even if not empty
-        // CTAN-example-Annotations.pdf p2
-        float minX = Float.MAX_VALUE;
-        float minY = Float.MAX_VALUE;
-        float maxX = Float.MIN_VALUE;
-        float maxY = Float.MIN_VALUE;
+      final boolean hasBackground = contentStream.setNonStrokingColorOnDemand(annotation.getInteriorColor());
 
-        float[][] pathArray = getPathArray(annotation);
-        if (pathArray == null)
-        {
-            return;
-        }
-        for (int i = 0; i < pathArray.length; ++i)
-        {
-            for (int j = 0; j < pathArray[i].length / 2; ++j)
-            {
-                float x = pathArray[i][j * 2];
-                float y = pathArray[i][j * 2 + 1];
-                minX = Math.min(minX, x);
-                minY = Math.min(minY, y);
-                maxX = Math.max(maxX, x);
-                maxY = Math.max(maxY, y);
+      setOpacity(contentStream, annotation.getConstantOpacity());
+
+      contentStream.setBorderLine(lineWidth, annotation.getBorderStyle(), annotation.getBorder());
+
+      final PDBorderEffectDictionary borderEffect = annotation.getBorderEffect();
+      if (borderEffect != null && borderEffect.getStyle().equals(PDBorderEffectDictionary.STYLE_CLOUDY)) {
+        final CloudyBorder cloudyBorder = new CloudyBorder(contentStream, borderEffect.getIntensity(), lineWidth,
+            getRectangle());
+        cloudyBorder.createCloudyPolygon(pathArray);
+        annotation.setRectangle(cloudyBorder.getRectangle());
+        final PDAppearanceStream appearanceStream = annotation.getNormalAppearanceStream();
+        appearanceStream.setBBox(cloudyBorder.getBBox());
+        appearanceStream.setMatrix(cloudyBorder.getMatrix());
+      } else {
+        // Acrobat applies a padding to each side of the bbox so the line is
+        // completely within the bbox.
+
+        for (int i = 0; i < pathArray.length; i++) {
+          final float[] pointsArray = pathArray[i];
+          // first array shall be of size 2 and specify the moveto operator
+          if (i == 0 && pointsArray.length == 2) {
+            contentStream.moveTo(pointsArray[0], pointsArray[1]);
+          } else {
+            // entries of length 2 shall be treated as lineto operator
+            if (pointsArray.length == 2) {
+              contentStream.lineTo(pointsArray[0], pointsArray[1]);
+            } else if (pointsArray.length == 6) {
+              contentStream.curveTo(pointsArray[0], pointsArray[1], pointsArray[2], pointsArray[3], pointsArray[4],
+                  pointsArray[5]);
             }
+          }
         }
+        contentStream.closePath();
+      }
+      contentStream.drawShape(lineWidth, hasStroke, hasBackground);
+    } catch (final IOException e) {
+      PDPolygonAppearanceHandler.LOG.error(e);
+    }
+  }
 
-        rect.setLowerLeftX(Math.min(minX - lineWidth, rect.getLowerLeftX()));
-        rect.setLowerLeftY(Math.min(minY - lineWidth, rect.getLowerLeftY()));
-        rect.setUpperRightX(Math.max(maxX + lineWidth, rect.getUpperRightX()));
-        rect.setUpperRightY(Math.max(maxY + lineWidth, rect.getUpperRightY()));
-        annotation.setRectangle(rect);
+  private float[][] getPathArray(final PDAnnotationPolygon annotation) {
+    // PDF 2.0: Path takes priority over Vertices
+    float[][] pathArray = annotation.getPath();
+    if (pathArray == null) {
+      // convert PDF 1.* array to PDF 2.0 array
+      final float[] verticesArray = annotation.getVertices();
+      if (verticesArray == null)
+        return null;
+      final int points = verticesArray.length / 2;
+      pathArray = new float[points][2];
+      for (int i = 0; i < points; ++i) {
+        pathArray[i][0] = verticesArray[i * 2];
+        pathArray[i][1] = verticesArray[i * 2 + 1];
+      }
+    }
+    return pathArray;
+  }
 
-        try (PDAppearanceContentStream contentStream = getNormalAppearanceAsContentStream())
-        {
-            boolean hasStroke = contentStream.setStrokingColorOnDemand(getColor());
+  @Override
+  public void generateRolloverAppearance() {
+    // No rollover appearance generated for a polygon annotation
+  }
 
-            boolean hasBackground = contentStream
-                    .setNonStrokingColorOnDemand(annotation.getInteriorColor());
+  @Override
+  public void generateDownAppearance() {
+    // No down appearance generated for a polygon annotation
+  }
 
-            setOpacity(contentStream, annotation.getConstantOpacity());
+  /**
+   * Get the line with of the border.
+   *
+   * Get the width of the line used to draw a border around the annotation. This
+   * may either be specified by the annotation dictionaries Border setting or by
+   * the W entry in the BS border style dictionary. If both are missing the
+   * default width is 1.
+   *
+   * @return the line width
+   */
+  // TODO: according to the PDF spec the use of the BS entry is annotation
+  // specific
+  // so we will leave that to be implemented by individual handlers.
+  // If at the end all annotations support the BS entry this can be handled
+  // here and removed from the individual handlers.
+  float getLineWidth() {
+    final PDAnnotationMarkup annotation = (PDAnnotationMarkup) getAnnotation();
 
-            contentStream.setBorderLine(lineWidth, annotation.getBorderStyle(), annotation.getBorder());
+    final PDBorderStyleDictionary bs = annotation.getBorderStyle();
 
-            PDBorderEffectDictionary borderEffect = annotation.getBorderEffect();
-            if (borderEffect != null && borderEffect.getStyle().equals(PDBorderEffectDictionary.STYLE_CLOUDY))
-            {
-                CloudyBorder cloudyBorder = new CloudyBorder(contentStream,
-                    borderEffect.getIntensity(), lineWidth, getRectangle());
-                cloudyBorder.createCloudyPolygon(pathArray);
-                annotation.setRectangle(cloudyBorder.getRectangle());
-                PDAppearanceStream appearanceStream = annotation.getNormalAppearanceStream();
-                appearanceStream.setBBox(cloudyBorder.getBBox());
-                appearanceStream.setMatrix(cloudyBorder.getMatrix());
-            }
-            else
-            {
-                // Acrobat applies a padding to each side of the bbox so the line is
-                // completely within the bbox.
+    if (bs != null)
+      return bs.getWidth();
 
-                for (int i = 0; i < pathArray.length; i++)
-                {
-                    float[] pointsArray = pathArray[i];
-                    // first array shall be of size 2 and specify the moveto operator
-                    if (i == 0 && pointsArray.length == 2)
-                    {
-                        contentStream.moveTo(pointsArray[0], pointsArray[1]);
-                    }
-                    else
-                    {
-                        // entries of length 2 shall be treated as lineto operator
-                        if (pointsArray.length == 2)
-                        {
-                            contentStream.lineTo(pointsArray[0], pointsArray[1]);
-                        }
-                        else if (pointsArray.length == 6)
-                        {
-                            contentStream.curveTo(pointsArray[0], pointsArray[1],
-                                    pointsArray[2], pointsArray[3],
-                                    pointsArray[4], pointsArray[5]);
-                        }
-                    }
-                }
-                contentStream.closePath();
-            }
-            contentStream.drawShape(lineWidth, hasStroke, hasBackground);
-        }
-        catch (IOException e)
-        {
-            LOG.error(e);
-        }
+    final COSArray borderCharacteristics = annotation.getBorder();
+    if (borderCharacteristics.size() >= 3) {
+      final COSBase base = borderCharacteristics.getObject(2);
+      if (base instanceof COSNumber)
+        return ((COSNumber) base).floatValue();
     }
 
-    private float[][] getPathArray(PDAnnotationPolygon annotation)
-    {
-        // PDF 2.0: Path takes priority over Vertices
-        float[][] pathArray = annotation.getPath();
-        if (pathArray == null)
-        {
-            // convert PDF 1.* array to PDF 2.0 array
-            float[] verticesArray = annotation.getVertices();
-            if (verticesArray == null)
-            {
-                return null;
-            }
-            int points = verticesArray.length / 2;
-            pathArray = new float[points][2];
-            for (int i = 0; i < points; ++i)
-            {
-                pathArray[i][0] = verticesArray[i * 2];
-                pathArray[i][1] = verticesArray[i * 2 + 1];
-            }
-        }
-        return pathArray;
-    }
-
-    @Override
-    public void generateRolloverAppearance()
-    {
-        // No rollover appearance generated for a polygon annotation
-    }
-
-    @Override
-    public void generateDownAppearance()
-    {
-        // No down appearance generated for a polygon annotation
-    }
-
-    /**
-     * Get the line with of the border.
-     * 
-     * Get the width of the line used to draw a border around the annotation.
-     * This may either be specified by the annotation dictionaries Border
-     * setting or by the W entry in the BS border style dictionary. If both are
-     * missing the default width is 1.
-     * 
-     * @return the line width
-     */
-    // TODO: according to the PDF spec the use of the BS entry is annotation
-    // specific
-    // so we will leave that to be implemented by individual handlers.
-    // If at the end all annotations support the BS entry this can be handled
-    // here and removed from the individual handlers.
-    float getLineWidth()
-    {
-        PDAnnotationMarkup annotation = (PDAnnotationMarkup) getAnnotation();
-
-        PDBorderStyleDictionary bs = annotation.getBorderStyle();
-
-        if (bs != null)
-        {
-            return bs.getWidth();
-        }
-
-        COSArray borderCharacteristics = annotation.getBorder();
-        if (borderCharacteristics.size() >= 3)
-        {
-            COSBase base = borderCharacteristics.getObject(2);
-            if (base instanceof COSNumber)
-            {
-                return ((COSNumber) base).floatValue();
-            }
-        }
-
-        return 1;
-    }
+    return 1;
+  }
 }
