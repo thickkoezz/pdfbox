8b59e993cca531f0aa394e71cc4fb3b3d30f38d6
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:34:14 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetFlatness.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetFlatness.java
@@ -19,38 +19,32 @@ package org.apache.pdfbox.contentstream.operator.state;
 
 import java.io.IOException;
 import java.util.List;
+
 import org.apache.pdfbox.contentstream.operator.MissingOperandException;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSNumber;
 
 /**
  * i: Set the flatness tolerance.
  *
  * @author John Hewson
  */
-public class SetFlatness extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> operands) throws IOException
-    {
-        if (operands.isEmpty())
-        {
-            throw new MissingOperandException(operator, operands);
-        }
-        if (!checkArrayTypesClass(operands, COSNumber.class))
-        {
-            return;
-        }
-        COSNumber value = (COSNumber) operands.get(0);
-        context.getGraphicsState().setFlatness(value.floatValue());
-    }
+public class SetFlatness extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> operands) throws IOException {
+    if (operands.isEmpty())
+      throw new MissingOperandException(operator, operands);
+    if (!checkArrayTypesClass(operands, COSNumber.class))
+      return;
+    final COSNumber value = (COSNumber) operands.get(0);
+    context.getGraphicsState().setFlatness(value.floatValue());
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_FLATNESS;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_FLATNESS;
+  }
 }
