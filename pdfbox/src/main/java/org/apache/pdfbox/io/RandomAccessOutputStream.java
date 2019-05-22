25b03f252c13856643fd5d86173171f407c7383c
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:06:03 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccessOutputStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccessOutputStream.java
@@ -25,36 +25,33 @@ import java.io.OutputStream;
  * @author Ben Litchfield
  * @author John Hewson
  */
-public class RandomAccessOutputStream extends OutputStream
-{
-    private final RandomAccessWrite writer;
+public class RandomAccessOutputStream extends OutputStream {
+  private final RandomAccessWrite writer;
 
-    /**
-     * Constructor to create a new output stream which writes to the given RandomAccessWrite.
-     *
-     * @param writer The random access writer for output
-     */
-    public RandomAccessOutputStream(RandomAccessWrite writer)
-    {
-        this.writer = writer;
-        // we don't have to maintain a position, as each COSStream can only have one writer.
-    }
-    
-    @Override
-    public void write(byte[] b, int offset, int length) throws IOException
-    {
-        writer.write(b, offset, length);
-    }
+  /**
+   * Constructor to create a new output stream which writes to the given
+   * RandomAccessWrite.
+   *
+   * @param writer The random access writer for output
+   */
+  public RandomAccessOutputStream(final RandomAccessWrite writer) {
+    this.writer = writer;
+    // we don't have to maintain a position, as each COSStream can only have one
+    // writer.
+  }
 
-    @Override
-    public void write(byte[] b) throws IOException
-    {
-        writer.write(b);
-    }
+  @Override
+  public void write(final byte[] b, final int offset, final int length) throws IOException {
+    writer.write(b, offset, length);
+  }
 
-    @Override
-    public void write(int b) throws IOException
-    {
-        writer.write(b);
-    }
+  @Override
+  public void write(final byte[] b) throws IOException {
+    writer.write(b);
+  }
+
+  @Override
+  public void write(final int b) throws IOException {
+    writer.write(b);
+  }
 }
