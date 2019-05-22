83dc6f7738328325a2160ac1533be7bc44f17e54
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:50:01 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFAnnotationPolygon.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFAnnotationPolygon.java
@@ -36,142 +36,122 @@ import org.w3c.dom.Element;
  * @author Ben Litchfield
  * @author Johanneke Lamberink
  */
-public class FDFAnnotationPolygon extends FDFAnnotation
-{
-    private static final Log LOG = LogFactory.getLog(FDFAnnotationPolygon.class);
-    /**
-     * COS Model value for SubType entry.
-     */
-    public static final String SUBTYPE = "Polygon";
+public class FDFAnnotationPolygon extends FDFAnnotation {
+  private static final Log LOG = LogFactory.getLog(FDFAnnotationPolygon.class);
+  /**
+   * COS Model value for SubType entry.
+   */
+  public static final String SUBTYPE = "Polygon";
 
-    /**
-     * Default constructor.
-     */
-    public FDFAnnotationPolygon()
-    {
-        super();
-        annot.setName(COSName.SUBTYPE, SUBTYPE);
+  /**
+   * Default constructor.
+   */
+  public FDFAnnotationPolygon() {
+    super();
+    annot.setName(COSName.SUBTYPE, FDFAnnotationPolygon.SUBTYPE);
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param a An existing FDF Annotation.
+   */
+  public FDFAnnotationPolygon(final COSDictionary a) {
+    super(a);
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param element An XFDF element.
+   *
+   * @throws IOException If there is an error extracting information from the
+   *                     element.
+   */
+  public FDFAnnotationPolygon(final Element element) throws IOException {
+    super(element);
+    annot.setName(COSName.SUBTYPE, FDFAnnotationPolygon.SUBTYPE);
+
+    initVertices(element);
+    final String color = element.getAttribute("interior-color");
+    if (color != null && color.length() == 7 && color.charAt(0) == '#') {
+      final int colorValue = Integer.parseInt(color.substring(1, 7), 16);
+      setInteriorColor(new Color(colorValue));
     }
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param a An existing FDF Annotation.
-     */
-    public FDFAnnotationPolygon(COSDictionary a)
-    {
-        super(a);
+  private void initVertices(final Element element) throws IOException {
+    final XPath xpath = XPathFactory.newInstance().newXPath();
+    try {
+      final String vertices = xpath.evaluate("vertices", element);
+      if (vertices == null || vertices.isEmpty())
+        throw new IOException("Error: missing element 'vertices'");
+      final String[] verticesValues = vertices.split(",|;");
+      final float[] values = new float[verticesValues.length];
+      for (int i = 0; i < verticesValues.length; i++) {
+        values[i] = Float.parseFloat(verticesValues[i]);
+      }
+      setVertices(values);
+    } catch (final XPathExpressionException e) {
+      FDFAnnotationPolygon.LOG.debug("Error while evaluating XPath expression for polygon vertices", e);
     }
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param element An XFDF element.
-     *
-     * @throws IOException If there is an error extracting information from the element.
-     */
-    public FDFAnnotationPolygon(Element element) throws IOException
-    {
-        super(element);
-        annot.setName(COSName.SUBTYPE, SUBTYPE);
+  /**
+   * This will set the coordinates of the vertices.
+   *
+   * @param vertices array of floats [x1, y1, x2, y2, ...] vertex coordinates in
+   *                 default user space.
+   */
+  public final void setVertices(final float[] vertices) {
+    final COSArray newVertices = new COSArray();
+    newVertices.setFloatArray(vertices);
+    annot.setItem(COSName.VERTICES, newVertices);
+  }
 
-        initVertices(element);
-        String color = element.getAttribute("interior-color");
-        if (color != null && color.length() == 7 && color.charAt(0) == '#')
-        {
-            int colorValue = Integer.parseInt(color.substring(1, 7), 16);
-            setInteriorColor(new Color(colorValue));
-        }
+  /**
+   * This will get the coordinates of the vertices.
+   *
+   * @return array of floats [x1, y1, x2, y2, ...] vertex coordinates in default
+   *         user space.
+   */
+  public float[] getVertices() {
+    final COSArray array = (COSArray) annot.getDictionaryObject(COSName.VERTICES);
+    if (array != null)
+      return array.toFloatArray();
+    else
+      return null; // Should never happen as this is a required item
+  }
+
+  /**
+   * This will set interior color of the drawn area.
+   *
+   * @param color The interior color of the drawn area.
+   */
+  public final void setInteriorColor(final Color color) {
+    COSArray array = null;
+    if (color != null) {
+      final float[] colors = color.getRGBColorComponents(null);
+      array = new COSArray();
+      array.setFloatArray(colors);
     }
+    annot.setItem(COSName.IC, array);
+  }
 
-    private void initVertices(Element element) throws IOException
-    {
-        XPath xpath = XPathFactory.newInstance().newXPath();
-        try
-        {
-            String vertices = xpath.evaluate("vertices", element);
-            if (vertices == null || vertices.isEmpty())
-            {
-                throw new IOException("Error: missing element 'vertices'");
-            }
-            String[] verticesValues = vertices.split(",|;");
-            float[] values = new float[verticesValues.length];
-            for (int i = 0; i < verticesValues.length; i++)
-            {
-                values[i] = Float.parseFloat(verticesValues[i]);
-            }
-            setVertices(values);
-        }
-        catch (XPathExpressionException e)
-        {
-            LOG.debug("Error while evaluating XPath expression for polygon vertices", e);
-        }
+  /**
+   * This will get interior color of the drawn area.
+   *
+   * @return object representing the color.
+   */
+  public Color getInteriorColor() {
+    Color retval = null;
+    final COSArray array = (COSArray) annot.getDictionaryObject(COSName.IC);
+    if (array != null) {
+      final float[] rgb = array.toFloatArray();
+      if (rgb.length >= 3) {
+        retval = new Color(rgb[0], rgb[1], rgb[2]);
+      }
     }
-
-    /**
-     * This will set the coordinates of the vertices.
-     *
-     * @param vertices array of floats [x1, y1, x2, y2, ...] vertex coordinates in default user space.
-     */
-    public final void setVertices(float[] vertices)
-    {
-        COSArray newVertices = new COSArray();
-        newVertices.setFloatArray(vertices);
-        annot.setItem(COSName.VERTICES, newVertices);
-    }
-
-    /**
-     * This will get the coordinates of the vertices.
-     *
-     * @return array of floats [x1, y1, x2, y2, ...] vertex coordinates in default user space.
-     */
-    public float[] getVertices()
-    {
-        COSArray array = (COSArray) annot.getDictionaryObject(COSName.VERTICES);
-        if (array != null)
-        {
-            return array.toFloatArray();
-        }
-        else
-        {
-            return null; // Should never happen as this is a required item
-        }
-    }
-
-    /**
-     * This will set interior color of the drawn area.
-     *
-     * @param color The interior color of the drawn area.
-     */
-    public final void setInteriorColor(Color color)
-    {
-        COSArray array = null;
-        if (color != null)
-        {
-            float[] colors = color.getRGBColorComponents(null);
-            array = new COSArray();
-            array.setFloatArray(colors);
-        }
-        annot.setItem(COSName.IC, array);
-    }
-
-    /**
-     * This will get interior color of the drawn area.
-     *
-     * @return object representing the color.
-     */
-    public Color getInteriorColor()
-    {
-        Color retval = null;
-        COSArray array = (COSArray) annot.getDictionaryObject(COSName.IC);
-        if (array != null)
-        {
-            float[] rgb = array.toFloatArray();
-            if (rgb.length >= 3)
-            {
-                retval = new Color(rgb[0], rgb[1], rgb[2]);
-            }
-        }
-        return retval;
-    }
+    return retval;
+  }
 }
