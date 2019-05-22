a45c63449fcbe33c06aad9debda38142ca13a2cc
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:35:57 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetTextLeading.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetTextLeading.java
@@ -18,29 +18,26 @@ package org.apache.pdfbox.contentstream.operator.text;
 
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSNumber;
 
 /**
  * TL: Set text leading.
  *
  * @author Laurent Huault
  */
-public class SetTextLeading extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments)
-    {
-        COSNumber leading = (COSNumber)arguments.get( 0 );
-        context.getGraphicsState().getTextState().setLeading( leading.floatValue() );
-    }
+public class SetTextLeading extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) {
+    final COSNumber leading = (COSNumber) arguments.get(0);
+    context.getGraphicsState().getTextState().setLeading(leading.floatValue());
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_TEXT_LEADING;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_TEXT_LEADING;
+  }
 }
