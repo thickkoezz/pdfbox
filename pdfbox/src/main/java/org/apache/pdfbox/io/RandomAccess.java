879f64fa36fb1824c4ea5f2401f4b58b8c07decd
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:05:41 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccess.java
+++ pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccess.java
@@ -17,12 +17,11 @@
 package org.apache.pdfbox.io;
 
 /**
- * An interface to allow data to be stored completely in memory or
- * to use a scratch file on the disk.
+ * An interface to allow data to be stored completely in memory or to use a
+ * scratch file on the disk.
  *
  * @author Ben Litchfield
  */
-public interface RandomAccess extends RandomAccessRead, RandomAccessWrite
-{
-    // super interface for both read and write
+public interface RandomAccess extends RandomAccessRead, RandomAccessWrite {
+  // super interface for both read and write
 }
