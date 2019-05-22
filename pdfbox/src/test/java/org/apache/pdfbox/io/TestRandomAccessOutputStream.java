bc76326e399d24f1c09437bbc38ec6101a15f61b
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:42:44 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/io/TestRandomAccessOutputStream.java
+++ pdfbox/src/test/java/org/apache/pdfbox/io/TestRandomAccessOutputStream.java
@@ -18,101 +18,96 @@ package org.apache.pdfbox.io;
 
 import java.io.File;
 import java.io.IOException;
+
 import junit.framework.TestCase;
 
 /**
  * This is a unit test for RandomAccessOutputStream.
- * 
- * @author Fredrik Kjellberg 
+ *
+ * @author Fredrik Kjellberg
  */
-public class TestRandomAccessOutputStream extends TestCase
-{
-    private final File testResultsDir = new File("target/test-output");
+public class TestRandomAccessOutputStream extends TestCase {
+  private final File testResultsDir = new File("target/test-output");
 
-    @Override
-    protected void setUp() throws Exception
-    {
-        super.setUp();
-        testResultsDir.mkdirs();
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    testResultsDir.mkdirs();
+  }
+
+  public void testWrite() throws IOException {
+    RandomAccessOutputStream out;
+    byte[] buffer;
+
+    final File file = new File(testResultsDir, "raf-outputstream.bin");
+
+    file.delete();
+
+    final RandomAccessFile raFile = new RandomAccessFile(file, "rw");
+
+    // Test single byte writes
+    buffer = createDataSequence(16, 10);
+    out = new RandomAccessOutputStream(raFile);
+    for (final byte b : buffer) {
+      out.write(b);
+    }
+    TestCase.assertEquals(16, raFile.length());
+    TestCase.assertEquals(16, raFile.getPosition());
+    out.close();
+
+    // Test no write
+    out = new RandomAccessOutputStream(raFile);
+    TestCase.assertEquals(16, raFile.length());
+    TestCase.assertEquals(16, raFile.getPosition());
+    out.close();
+
+    // Test buffer writes
+    buffer = createDataSequence(8, 30);
+    out = new RandomAccessOutputStream(raFile);
+    out.write(buffer);
+    TestCase.assertEquals(24, raFile.length());
+    TestCase.assertEquals(24, raFile.getPosition());
+    out.close();
+
+    // Test partial buffer writes
+    buffer = createDataSequence(16, 50);
+    out = new RandomAccessOutputStream(raFile);
+    out.write(buffer, 8, 4);
+    out.write(buffer, 4, 2);
+    TestCase.assertEquals(30, raFile.length());
+    TestCase.assertEquals(30, raFile.getPosition());
+    out.close();
+
+    // Verify written data
+    buffer = new byte[(int) raFile.length()];
+    raFile.seek(0);
+    TestCase.assertEquals(buffer.length, raFile.read(buffer, 0, buffer.length));
+    TestCase.assertEquals(10, buffer[0]);
+    TestCase.assertEquals(11, buffer[1]);
+    TestCase.assertEquals(25, buffer[15]);
+
+    TestCase.assertEquals(30, buffer[16]);
+    TestCase.assertEquals(31, buffer[17]);
+    TestCase.assertEquals(37, buffer[23]);
+
+    TestCase.assertEquals(58, buffer[24]);
+    TestCase.assertEquals(59, buffer[25]);
+    TestCase.assertEquals(60, buffer[26]);
+    TestCase.assertEquals(61, buffer[27]);
+    TestCase.assertEquals(54, buffer[28]);
+    TestCase.assertEquals(55, buffer[29]);
+
+    // Cleanup
+    raFile.close();
+    file.delete();
+  }
+
+  protected byte[] createDataSequence(final int length, final int firstByteValue) {
+    final byte[] buffer = new byte[length];
+    for (int i = 0; i < buffer.length; i++) {
+      buffer[i] = (byte) (firstByteValue + i);
     }
 
-    public void testWrite() throws IOException
-    {
-        RandomAccessOutputStream out;
-        byte[] buffer;
-
-        File file = new File(testResultsDir, "raf-outputstream.bin");
-
-        file.delete();
-
-        RandomAccessFile raFile = new RandomAccessFile(file, "rw");
-
-        // Test single byte writes
-        buffer = createDataSequence(16, 10);
-        out = new RandomAccessOutputStream(raFile);
-        for (byte b : buffer)
-        {
-            out.write(b);
-        }
-        assertEquals(16, raFile.length());
-        assertEquals(16, raFile.getPosition());
-        out.close();
-        
-        // Test no write
-        out = new RandomAccessOutputStream(raFile);
-        assertEquals(16, raFile.length());
-        assertEquals(16, raFile.getPosition());
-        out.close();
-        
-        // Test buffer writes
-        buffer = createDataSequence(8, 30);
-        out = new RandomAccessOutputStream(raFile);
-        out.write(buffer);
-        assertEquals(24, raFile.length());
-        assertEquals(24, raFile.getPosition());
-        out.close();
-        
-        // Test partial buffer writes
-        buffer = createDataSequence(16, 50);
-        out = new RandomAccessOutputStream(raFile);
-        out.write(buffer, 8, 4);
-        out.write(buffer, 4, 2);
-        assertEquals(30, raFile.length());
-        assertEquals(30, raFile.getPosition());
-        out.close();
-
-        // Verify written data
-        buffer = new byte[(int) raFile.length()];
-        raFile.seek(0);
-        assertEquals(buffer.length, raFile.read(buffer, 0, buffer.length));
-        assertEquals(10, buffer[0]);
-        assertEquals(11, buffer[1]);
-        assertEquals(25, buffer[15]);
-
-        assertEquals(30, buffer[16]);
-        assertEquals(31, buffer[17]);
-        assertEquals(37, buffer[23]);
-
-        assertEquals(58, buffer[24]);
-        assertEquals(59, buffer[25]);
-        assertEquals(60, buffer[26]);
-        assertEquals(61, buffer[27]);
-        assertEquals(54, buffer[28]);
-        assertEquals(55, buffer[29]);
-
-        // Cleanup
-        raFile.close();
-        file.delete();
-    }
-
-    protected byte[] createDataSequence(int length, int firstByteValue)
-    {
-        byte[] buffer = new byte[length];
-        for (int i = 0; i < buffer.length; i++)
-        {
-            buffer[i] = (byte) (firstByteValue + i);
-        }
-
-        return buffer;
-    }
+    return buffer;
+  }
 }
