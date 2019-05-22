9fcc1b606729d29e58ed6d201edc1ad9f099cea2
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:37:05 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/SigningSupport.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/SigningSupport.java
@@ -16,51 +16,42 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.digitalsignature;
 
-import org.apache.pdfbox.pdfwriter.COSWriter;
-
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.pdfbox.pdfwriter.COSWriter;
+
 /**
- * Class to be used when creating PDF signatures externally. COSWriter is used to obtain data to be
- * signed and set the resulted CMS signature.
+ * Class to be used when creating PDF signatures externally. COSWriter is used
+ * to obtain data to be signed and set the resulted CMS signature.
  *
  */
-public class SigningSupport implements ExternalSigningSupport, Closeable
-{
-    private COSWriter cosWriter;
+public class SigningSupport implements ExternalSigningSupport, Closeable {
+  private COSWriter cosWriter;
 
-    public SigningSupport(COSWriter cosWriter)
-    {
-        this.cosWriter = cosWriter;
-    }
+  public SigningSupport(final COSWriter cosWriter) {
+    this.cosWriter = cosWriter;
+  }
 
-    @Override
-    public InputStream getContent() throws IOException
-    {
-        return cosWriter.getDataToSign();
-    }
+  @Override
+  public InputStream getContent() throws IOException {
+    return cosWriter.getDataToSign();
+  }
 
-    @Override
-    public void setSignature(byte[] signature) throws IOException
-    {
-        cosWriter.writeExternalSignature(signature);
-    }
+  @Override
+  public void setSignature(final byte[] signature) throws IOException {
+    cosWriter.writeExternalSignature(signature);
+  }
 
-    @Override
-    public void close() throws IOException
-    {
-        if (cosWriter != null)
-        {
-            try
-            {
-                cosWriter.close();
-            }
-            finally
-            {
-                cosWriter = null;
-            }
-        }
+  @Override
+  public void close() throws IOException {
+    if (cosWriter != null) {
+      try {
+        cosWriter.close();
+      } finally {
+        cosWriter = null;
+      }
     }
+  }
 }
--- pdfbox/src/main/java/org/apache/pdfbox/util/SmallMap.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/SmallMap.java
@@ -25,364 +25,296 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Map implementation with a smallest possible memory usage.
- * It should only be used for maps with small number of items
- * (e.g. &lt;30) since most operations have an O(n) complexity.
- * Thus it should be used in cases with large number of map
- * objects, each having only few items.
- * 
- * <p><code>null</code> is not supported for keys or values.</p>
+ * Map implementation with a smallest possible memory usage. It should only be
+ * used for maps with small number of items (e.g. &lt;30) since most operations
+ * have an O(n) complexity. Thus it should be used in cases with large number of
+ * map objects, each having only few items.
+ *
+ * <p>
+ * <code>null</code> is not supported for keys or values.
+ * </p>
  */
-public class SmallMap<K, V> implements Map<K, V>
-{
-    /**
-     * stores key-value pair as 2 objects; key first; in case of empty map this might be <code>null</code>
-     */
-    private Object[] mapArr;
+public class SmallMap<K, V> implements Map<K, V> {
+  /**
+   * stores key-value pair as 2 objects; key first; in case of empty map this
+   * might be <code>null</code>
+   */
+  private Object[] mapArr;
 
-    /** Creates empty map. */
-    public SmallMap()
-    {
-    }
-    
-    /** Creates map filled with entries from provided map. */
-    public SmallMap(Map<? extends K, ? extends V> initMap)
-    {
-        putAll(initMap);
-    }
-    
-    /**
-     * Returns index of key within map-array or <code>-1</code>
-     * if key is not found (or key is <code>null</code>).
-     */
-    private int findKey(Object key)
-    {
-        if (isEmpty() || (key==null))
-        {
-            return -1;
-        }
-        
-        for ( int aIdx = 0; aIdx < mapArr.length; aIdx+=2 )
-        {
-            if (key.equals(mapArr[aIdx]))
-            {
-                return aIdx;
-            }
-        }
-        
-        return -1;
-    }
-    
-    /**
-     * Returns index of value within map-array or <code>-1</code>
-     * if value is not found (or value is <code>null</code>).
-     */
-    private int findValue(Object value)
-    {
-        if (isEmpty() || (value==null))
-        {
-            return -1;
-        }
-        
-        for ( int aIdx = 1; aIdx < mapArr.length; aIdx+=2 )
-        {
-            if (value.equals(mapArr[aIdx]))
-            {
-                return aIdx;
-            }
-        }
-        
-        return -1;
-    }
-    
-    @Override
-    public int size()
-    {
-        return mapArr == null ? 0 : mapArr.length >> 1;
+  /** Creates empty map. */
+  public SmallMap() {
+  }
+
+  /** Creates map filled with entries from provided map. */
+  public SmallMap(final Map<? extends K, ? extends V> initMap) {
+    putAll(initMap);
+  }
+
+  /**
+   * Returns index of key within map-array or <code>-1</code> if key is not found
+   * (or key is <code>null</code>).
+   */
+  private int findKey(final Object key) {
+    if (isEmpty() || key == null)
+      return -1;
+
+    for (int aIdx = 0; aIdx < mapArr.length; aIdx += 2) {
+      if (key.equals(mapArr[aIdx]))
+        return aIdx;
     }
 
-    @Override
-    public boolean isEmpty()
-    {
-        return (mapArr == null) || (mapArr.length == 0);
+    return -1;
+  }
+
+  /**
+   * Returns index of value within map-array or <code>-1</code> if value is not
+   * found (or value is <code>null</code>).
+   */
+  private int findValue(final Object value) {
+    if (isEmpty() || value == null)
+      return -1;
+
+    for (int aIdx = 1; aIdx < mapArr.length; aIdx += 2) {
+      if (value.equals(mapArr[aIdx]))
+        return aIdx;
     }
 
-    @Override
-    public boolean containsKey(Object key)
-    {
-        return findKey(key) >= 0;
-    }
+    return -1;
+  }
 
-    @Override
-    public boolean containsValue(Object value)
-    {
-        return findValue(value) >= 0;
-    }
+  @Override
+  public int size() {
+    return mapArr == null ? 0 : mapArr.length >> 1;
+  }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public V get(Object key)
-    {
-        int kIdx = findKey(key);
-        
-        return kIdx < 0 ? null : (V) mapArr[kIdx+1];
-    }
+  @Override
+  public boolean isEmpty() {
+    return mapArr == null || mapArr.length == 0;
+  }
 
-    @Override
-    public V put(K key, V value)
-    {
-        if ((key == null) || (value == null))
-        {
-            throw new NullPointerException( "Key or value must not be null.");
-        }
-        
-        if (mapArr == null)
-        {
-            mapArr = new Object[] { key, value };
-            return null;
-        }
-        else
-        {
-            int kIdx = findKey(key);
-            
-            if (kIdx < 0)
-            {
-                // key unknown
-                int oldLen = mapArr.length;
-                Object[] newMapArr = new Object[oldLen+2];
-                System.arraycopy(mapArr, 0, newMapArr, 0, oldLen);
-                newMapArr[oldLen] = key;
-                newMapArr[oldLen+1] = value;
-                mapArr = newMapArr;
-                return null;
-            }
-            else
-            {
-                // key exists; replace value
-                @SuppressWarnings("unchecked")
-                V oldValue = (V) mapArr[kIdx+1];
-                mapArr[kIdx+1] = value;
-                return oldValue;
-            }
-        }
-    }
+  @Override
+  public boolean containsKey(final Object key) {
+    return findKey(key) >= 0;
+  }
 
-    @Override
-    public V remove(Object key)
-    {
-        int kIdx = findKey(key);
-        
-        if (kIdx < 0)
-        {
-            // not found
-            return null;
-        }
+  @Override
+  public boolean containsValue(final Object value) {
+    return findValue(value) >= 0;
+  }
 
+  @SuppressWarnings("unchecked")
+  @Override
+  public V get(final Object key) {
+    final int kIdx = findKey(key);
+
+    return kIdx < 0 ? null : (V) mapArr[kIdx + 1];
+  }
+
+  @Override
+  public V put(final K key, final V value) {
+    if (key == null || value == null)
+      throw new NullPointerException("Key or value must not be null.");
+
+    if (mapArr == null) {
+      mapArr = new Object[] { key, value };
+      return null;
+    } else {
+      final int kIdx = findKey(key);
+
+      if (kIdx < 0) {
+        // key unknown
+        final int oldLen = mapArr.length;
+        final Object[] newMapArr = new Object[oldLen + 2];
+        System.arraycopy(mapArr, 0, newMapArr, 0, oldLen);
+        newMapArr[oldLen] = key;
+        newMapArr[oldLen + 1] = value;
+        mapArr = newMapArr;
+        return null;
+      } else {
+        // key exists; replace value
         @SuppressWarnings("unchecked")
-        V oldValue = (V) mapArr[kIdx+1];
-        int oldLen = mapArr.length;
-        
-        if (oldLen == 2)
-        {
-            // was last entry
-            mapArr = null;
-        }
-        else
-        {
-            Object[] newMapArr = new Object[oldLen-2];
-            System.arraycopy(mapArr, 0, newMapArr, 0, kIdx);
-            System.arraycopy(mapArr, kIdx+2, newMapArr, kIdx, oldLen - kIdx - 2);
-            mapArr = newMapArr;
-        }
-        
+        final V oldValue = (V) mapArr[kIdx + 1];
+        mapArr[kIdx + 1] = value;
         return oldValue;
+      }
+    }
+  }
+
+  @Override
+  public V remove(final Object key) {
+    final int kIdx = findKey(key);
+
+    if (kIdx < 0)
+      // not found
+      return null;
+
+    @SuppressWarnings("unchecked")
+    final V oldValue = (V) mapArr[kIdx + 1];
+    final int oldLen = mapArr.length;
+
+    if (oldLen == 2) {
+      // was last entry
+      mapArr = null;
+    } else {
+      final Object[] newMapArr = new Object[oldLen - 2];
+      System.arraycopy(mapArr, 0, newMapArr, 0, kIdx);
+      System.arraycopy(mapArr, kIdx + 2, newMapArr, kIdx, oldLen - kIdx - 2);
+      mapArr = newMapArr;
     }
 
-    @Override
-    public final void putAll(Map<? extends K, ? extends V> otherMap)
-    {
-        if ((mapArr == null) || (mapArr.length == 0))
-        {
-            // existing map is empty
-            mapArr = new Object[otherMap.size() << 1];
-            int aIdx = 0;
-            for (Entry<? extends K, ? extends V> entry : otherMap.entrySet())
-            {
-                if ((entry.getKey() == null) || (entry.getValue() == null))
-                {
-                    throw new NullPointerException( "Key or value must not be null.");
-                }
-                
-                mapArr[aIdx++] = entry.getKey();
-                mapArr[aIdx++] = entry.getValue();
-            }
+    return oldValue;
+  }
+
+  @Override
+  public final void putAll(final Map<? extends K, ? extends V> otherMap) {
+    if (mapArr == null || mapArr.length == 0) {
+      // existing map is empty
+      mapArr = new Object[otherMap.size() << 1];
+      int aIdx = 0;
+      for (final Entry<? extends K, ? extends V> entry : otherMap.entrySet()) {
+        if (entry.getKey() == null || entry.getValue() == null)
+          throw new NullPointerException("Key or value must not be null.");
+
+        mapArr[aIdx++] = entry.getKey();
+        mapArr[aIdx++] = entry.getValue();
+      }
+    } else {
+      final int oldLen = mapArr.length;
+      // first increase array size to hold all to put entries as if they have unknown
+      // keys
+      // reduce after adding all to the required size
+      Object[] newMapArr = new Object[oldLen + (otherMap.size() << 1)];
+      System.arraycopy(mapArr, 0, newMapArr, 0, oldLen);
+
+      int newIdx = oldLen;
+      for (final Entry<? extends K, ? extends V> entry : otherMap.entrySet()) {
+        if (entry.getKey() == null || entry.getValue() == null)
+          throw new NullPointerException("Key or value must not be null.");
+
+        final int existKeyIdx = findKey(entry.getKey());
+
+        if (existKeyIdx >= 0) {
+          // existing key
+          newMapArr[existKeyIdx + 1] = entry.getValue();
+        } else {
+          // new key
+          newMapArr[newIdx++] = entry.getKey();
+          newMapArr[newIdx++] = entry.getValue();
         }
-        else
-        {
-            int oldLen = mapArr.length;
-            // first increase array size to hold all to put entries as if they have unknown keys
-            // reduce after adding all to the required size
-            Object[] newMapArr = new Object[oldLen+(otherMap.size() << 1)];
-            System.arraycopy(mapArr, 0, newMapArr, 0, oldLen);
-            
-            int newIdx = oldLen;
-            for (Entry<? extends K, ? extends V> entry : otherMap.entrySet())
-            {
-                if ((entry.getKey() == null) || (entry.getValue() == null))
-                {
-                    throw new NullPointerException( "Key or value must not be null.");
-                }
-                
-                int existKeyIdx = findKey(entry.getKey());
-                
-                if (existKeyIdx >= 0)
-                {
-                    // existing key
-                    newMapArr[existKeyIdx+1] = entry.getValue();
-                }
-                else
-                {
-                    // new key
-                    newMapArr[newIdx++] = entry.getKey();
-                    newMapArr[newIdx++] = entry.getValue();
-                }
-            }
+      }
 
-            if (newIdx < newMapArr.length)
-            {
-                Object[] reducedMapArr = new Object[newIdx];
-                System.arraycopy(newMapArr, 0, reducedMapArr, 0, newIdx);
-                newMapArr = reducedMapArr;
-            }
-            
-            mapArr = newMapArr;
-        }
+      if (newIdx < newMapArr.length) {
+        final Object[] reducedMapArr = new Object[newIdx];
+        System.arraycopy(newMapArr, 0, reducedMapArr, 0, newIdx);
+        newMapArr = reducedMapArr;
+      }
+
+      mapArr = newMapArr;
+    }
+  }
+
+  @Override
+  public void clear() {
+    mapArr = null;
+  }
+
+  /**
+   * Returns a set view of the keys contained in this map.
+   *
+   * <p>
+   * The current implementation does not allow changes to the returned key set
+   * (which would have to be reflected in the underlying map.
+   * </p>
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public Set<K> keySet() {
+    if (isEmpty())
+      return Collections.emptySet();
+
+    final Set<K> keys = new LinkedHashSet<>();
+    for (int kIdx = 0; kIdx < mapArr.length; kIdx += 2) {
+      keys.add((K) mapArr[kIdx]);
+    }
+    return Collections.unmodifiableSet(keys);
+  }
+
+  /**
+   * Returns a collection of the values contained in this map.
+   *
+   * <p>
+   * The current implementation does not allow changes to the returned collection
+   * (which would have to be reflected in the underlying map.
+   * </p>
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public Collection<V> values() {
+    if (isEmpty())
+      return Collections.emptySet();
+
+    final List<V> values = new ArrayList<>(mapArr.length >> 1);
+    for (int vIdx = 1; vIdx < mapArr.length; vIdx += 2) {
+      values.add((V) mapArr[vIdx]);
+    }
+    return Collections.unmodifiableList(values);
+  }
+
+  private class SmallMapEntry implements Entry<K, V> {
+    private final int keyIdx;
+
+    SmallMapEntry(final int keyInMapIdx) {
+      keyIdx = keyInMapIdx;
     }
 
-    @Override
-    public void clear()
-    {
-        mapArr = null;
-    }
-
-    /**
-     * Returns a set view of the keys contained in this map.
-     * 
-     * <p>The current implementation does not allow changes to the
-     * returned key set (which would have to be reflected in the
-     * underlying map.</p>
-     */
     @SuppressWarnings("unchecked")
     @Override
-    public Set<K> keySet()
-    {
-        if (isEmpty())
-        {
-            return Collections.emptySet();
-        }
-        
-        Set<K> keys = new LinkedHashSet<>();
-        for (int kIdx = 0; kIdx < mapArr.length; kIdx+=2)
-        {
-            keys.add((K)mapArr[kIdx]);
-        }
-        return Collections.unmodifiableSet( keys );
+    public K getKey() {
+      return (K) mapArr[keyIdx];
     }
 
-    /**
-     * Returns a collection of the values contained in this map.
-     * 
-     * <p>The current implementation does not allow changes to the
-     * returned collection (which would have to be reflected in the
-     * underlying map.</p>
-     */
     @SuppressWarnings("unchecked")
     @Override
-    public Collection<V> values()
-    {
-        if (isEmpty())
-        {
-            return Collections.emptySet();
-        }
-        
-        List<V> values = new ArrayList<>(mapArr.length >> 1);
-        for (int vIdx = 1; vIdx < mapArr.length; vIdx+=2)
-        {
-            values.add((V)mapArr[vIdx]);
-        }
-        return Collections.unmodifiableList( values );
+    public V getValue() {
+      return (V) mapArr[keyIdx + 1];
     }
 
-    private class SmallMapEntry implements Entry<K, V>
-    {
-        private final int keyIdx;
-        
-        SmallMapEntry(int keyInMapIdx)
-        {
-            keyIdx = keyInMapIdx;
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public K getKey()
-        {
-            return (K)mapArr[keyIdx];
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public V getValue()
-        {
-            return (V)mapArr[keyIdx+1];
-        }
-
-        @Override
-        public V setValue(V value)
-        {
-            if (value == null)
-            {
-                throw new NullPointerException( "Key or value must not be null.");
-            }
-
-            V oldValue = getValue();
-            mapArr[keyIdx+1] = value;
-            return oldValue;
-        }
-        
-        @Override
-        public int hashCode()
-        {
-            return getKey().hashCode();
-        }
-        
-        @Override
-        public boolean equals(Object obj)
-        {
-            if (!(obj instanceof SmallMap.SmallMapEntry))
-            {
-                return false;
-            }
-            @SuppressWarnings("unchecked")
-            SmallMapEntry other = (SmallMapEntry) obj;
-            
-            return getKey().equals(other.getKey()) && getValue().equals(other.getValue());
-        }
-    }
-    
     @Override
-    public Set<java.util.Map.Entry<K, V>> entrySet()
-    {
-        if (isEmpty())
-        {
-            return Collections.emptySet();
-        }
-        
-        Set<java.util.Map.Entry<K, V>> entries = new LinkedHashSet<>();
-        for (int kIdx = 0; kIdx < mapArr.length; kIdx+=2)
-        {
-            entries.add(new SmallMapEntry(kIdx));
-        }
-        return Collections.unmodifiableSet( entries );
+    public V setValue(final V value) {
+      if (value == null)
+        throw new NullPointerException("Key or value must not be null.");
+
+      final V oldValue = getValue();
+      mapArr[keyIdx + 1] = value;
+      return oldValue;
     }
+
+    @Override
+    public int hashCode() {
+      return getKey().hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+      if (!(obj instanceof SmallMap.SmallMapEntry))
+        return false;
+      @SuppressWarnings("unchecked")
+      final SmallMapEntry other = (SmallMapEntry) obj;
+
+      return getKey().equals(other.getKey()) && getValue().equals(other.getValue());
+    }
+  }
+
+  @Override
+  public Set<java.util.Map.Entry<K, V>> entrySet() {
+    if (isEmpty())
+      return Collections.emptySet();
+
+    final Set<java.util.Map.Entry<K, V>> entries = new LinkedHashSet<>();
+    for (int kIdx = 0; kIdx < mapArr.length; kIdx += 2) {
+      entries.add(new SmallMapEntry(kIdx));
+    }
+    return Collections.unmodifiableSet(entries);
+  }
 
 }
