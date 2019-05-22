0db0d24c94d03a49ec02cfa22e2cc29610404cc1
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:41:41 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripperByArea.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripperByArea.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.pdfbox.pdmodel.PDPage;
 
 /**
@@ -30,138 +31,122 @@ import org.apache.pdfbox.pdmodel.PDPage;
  *
  * @author Ben Litchfield
  */
-public class PDFTextStripperByArea extends PDFTextStripper
-{
-    private final List<String> regions = new ArrayList<>();
-    private final Map<String, Rectangle2D> regionArea = new HashMap<>();
-    private final Map<String, ArrayList<List<TextPosition>>> regionCharacterList
-            = new HashMap<>();
-    private final Map<String, StringWriter> regionText = new HashMap<>();
+public class PDFTextStripperByArea extends PDFTextStripper {
+  private final List<String> regions = new ArrayList<>();
+  private final Map<String, Rectangle2D> regionArea = new HashMap<>();
+  private final Map<String, ArrayList<List<TextPosition>>> regionCharacterList = new HashMap<>();
+  private final Map<String, StringWriter> regionText = new HashMap<>();
 
-    /**
-     * Constructor.
-     * @throws IOException If there is an error loading properties.
-     */
-    public PDFTextStripperByArea() throws IOException
-    {
-        super.setShouldSeparateByBeads(false);
-    }
+  /**
+   * Constructor.
+   *
+   * @throws IOException If there is an error loading properties.
+   */
+  public PDFTextStripperByArea() throws IOException {
+    super.setShouldSeparateByBeads(false);
+  }
 
-    /**
-     * This method does nothing in this derived class, because beads and regions are incompatible. Beads are
-     * ignored when stripping by area.
-     *
-     * @param aShouldSeparateByBeads The new grouping of beads.
-     */
-    @Override
-    public final void setShouldSeparateByBeads(boolean aShouldSeparateByBeads)
-    {
-    }
+  /**
+   * This method does nothing in this derived class, because beads and regions are
+   * incompatible. Beads are ignored when stripping by area.
+   *
+   * @param aShouldSeparateByBeads The new grouping of beads.
+   */
+  @Override
+  public final void setShouldSeparateByBeads(final boolean aShouldSeparateByBeads) {
+  }
 
-   /**
-     * Add a new region to group text by.
-     *
-     * @param regionName The name of the region.
-     * @param rect The rectangle area to retrieve the text from.
-     */
-    public void addRegion( String regionName, Rectangle2D rect )
-    {
-        regions.add( regionName );
-        regionArea.put( regionName, rect );
-    }
+  /**
+   * Add a new region to group text by.
+   *
+   * @param regionName The name of the region.
+   * @param rect       The rectangle area to retrieve the text from.
+   */
+  public void addRegion(final String regionName, final Rectangle2D rect) {
+    regions.add(regionName);
+    regionArea.put(regionName, rect);
+  }
 
-    /**
-     * Delete a region to group text by. If the region does not exist, this method does nothing.
-     *
-     * @param regionName The name of the region to delete.
-     */
-    public void removeRegion(String regionName)
-    {
-        regions.remove(regionName);
-        regionArea.remove(regionName);
-    }
-    
-    /**
-     * Get the list of regions that have been setup.
-     *
-     * @return A list of java.lang.String objects to identify the region names.
-     */
-    public List<String> getRegions()
-    {
-        return regions;
-    }
+  /**
+   * Delete a region to group text by. If the region does not exist, this method
+   * does nothing.
+   *
+   * @param regionName The name of the region to delete.
+   */
+  public void removeRegion(final String regionName) {
+    regions.remove(regionName);
+    regionArea.remove(regionName);
+  }
 
-    /**
-     * Get the text for the region, this should be called after extractRegions().
-     *
-     * @param regionName The name of the region to get the text from.
-     * @return The text that was identified in that region.
-     */
-    public String getTextForRegion( String regionName )
-    {
-        StringWriter text = regionText.get( regionName );
-        return text.toString();
-    }
+  /**
+   * Get the list of regions that have been setup.
+   *
+   * @return A list of java.lang.String objects to identify the region names.
+   */
+  public List<String> getRegions() {
+    return regions;
+  }
 
-    /**
-     * Process the page to extract the region text.
-     *
-     * @param page The page to extract the regions from.
-     * @throws IOException If there is an error while extracting text.
-     */
-    public void extractRegions( PDPage page ) throws IOException
-    {
-        for (String region : regions)
-        {
-            setStartPage(getCurrentPageNo());
-            setEndPage(getCurrentPageNo());
-            //reset the stored text for the region so this class
-            //can be reused.
-            String regionName = region;
-            ArrayList<List<TextPosition>> regionCharactersByArticle = new ArrayList<>();
-            regionCharactersByArticle.add( new ArrayList<TextPosition>() );
-            regionCharacterList.put( regionName, regionCharactersByArticle );
-            regionText.put( regionName, new StringWriter() );
-        }
-        
-        if( page.hasContents() )
-        {
-            processPage( page );
-        }
+  /**
+   * Get the text for the region, this should be called after extractRegions().
+   *
+   * @param regionName The name of the region to get the text from.
+   * @return The text that was identified in that region.
+   */
+  public String getTextForRegion(final String regionName) {
+    final StringWriter text = regionText.get(regionName);
+    return text.toString();
+  }
+
+  /**
+   * Process the page to extract the region text.
+   *
+   * @param page The page to extract the regions from.
+   * @throws IOException If there is an error while extracting text.
+   */
+  public void extractRegions(final PDPage page) throws IOException {
+    for (final String region : regions) {
+      setStartPage(getCurrentPageNo());
+      setEndPage(getCurrentPageNo());
+      // reset the stored text for the region so this class
+      // can be reused.
+      final String regionName = region;
+      final ArrayList<List<TextPosition>> regionCharactersByArticle = new ArrayList<>();
+      regionCharactersByArticle.add(new ArrayList<TextPosition>());
+      regionCharacterList.put(regionName, regionCharactersByArticle);
+      regionText.put(regionName, new StringWriter());
     }
 
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void processTextPosition( TextPosition text )
-    {
-        for (String region : regionArea.keySet())
-        {
-            Rectangle2D rect = regionArea.get( region );
-            if( rect.contains( text.getX(), text.getY() ) )
-            {
-                charactersByArticle = regionCharacterList.get( region );
-                super.processTextPosition( text );
-            }
-        }
+    if (page.hasContents()) {
+      processPage(page);
     }
+  }
 
-    
-    /**
-     * This will print the processed page text to the output stream.
-     *
-     * @throws IOException If there is an error writing the text.
-     */
-    @Override
-    protected void writePage() throws IOException
-    {
-        for (String region : regionArea.keySet())
-        {
-            charactersByArticle = regionCharacterList.get( region );
-            output = regionText.get( region );
-            super.writePage();
-        }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void processTextPosition(final TextPosition text) {
+    for (final String region : regionArea.keySet()) {
+      final Rectangle2D rect = regionArea.get(region);
+      if (rect.contains(text.getX(), text.getY())) {
+        charactersByArticle = regionCharacterList.get(region);
+        super.processTextPosition(text);
+      }
     }
+  }
+
+  /**
+   * This will print the processed page text to the output stream.
+   *
+   * @throws IOException If there is an error writing the text.
+   */
+  @Override
+  protected void writePage() throws IOException {
+    for (final String region : regionArea.keySet()) {
+      charactersByArticle = regionCharacterList.get(region);
+      output = regionText.get(region);
+      super.writePage();
+    }
+  }
 }
