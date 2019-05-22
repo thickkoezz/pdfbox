12842555c4abf4b1770c76762c6819e3a2d15dd2
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:14:01 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionFactory.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionFactory.java
@@ -20,87 +20,82 @@ import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 
 /**
- * This class will take a dictionary and determine which type of action to create.
+ * This class will take a dictionary and determine which type of action to
+ * create.
  *
  * @author Ben Litchfield
- * 
+ *
  */
-public final class PDActionFactory
-{
-    /**
-     * Utility Class.
-     */
-    private PDActionFactory()
-    {
-        //utility class
-    }
+public final class PDActionFactory {
+  /**
+   * Utility Class.
+   */
+  private PDActionFactory() {
+    // utility class
+  }
 
-    /**
-     * This will create the correct type of action based on the type specified
-     * in the dictionary.
-     *
-     * @param action An action dictionary.
-     *
-     * @return An action of the correct type.
-     */
-    public static PDAction createAction( COSDictionary action )
-    {
-        PDAction retval = null;
-        if( action != null)
-        {
-            String type = action.getNameAsString(COSName.S);
-            if (type != null)
-            {
-                switch (type)
-                {
-                    case PDActionJavaScript.SUB_TYPE:
-                        retval = new PDActionJavaScript(action);
-                        break;
-                    case PDActionGoTo.SUB_TYPE:
-                        retval = new PDActionGoTo(action);
-                        break;
-                    case PDActionLaunch.SUB_TYPE:
-                        retval = new PDActionLaunch(action);
-                        break;
-                    case PDActionRemoteGoTo.SUB_TYPE:
-                        retval = new PDActionRemoteGoTo(action);
-                        break;
-                    case PDActionURI.SUB_TYPE:
-                        retval = new PDActionURI(action);
-                        break;
-                    case PDActionNamed.SUB_TYPE:
-                        retval = new PDActionNamed(action);
-                        break;
-                    case PDActionSound.SUB_TYPE:
-                        retval = new PDActionSound(action);
-                        break;
-                    case PDActionMovie.SUB_TYPE:
-                        retval = new PDActionMovie(action);
-                        break;
-                    case PDActionImportData.SUB_TYPE:
-                        retval = new PDActionImportData(action);
-                        break;
-                    case PDActionResetForm.SUB_TYPE:
-                        retval = new PDActionResetForm(action);
-                        break;
-                    case PDActionHide.SUB_TYPE:
-                        retval = new PDActionHide(action);
-                        break;
-                    case PDActionSubmitForm.SUB_TYPE:
-                        retval = new PDActionSubmitForm(action);
-                        break;
-                    case PDActionThread.SUB_TYPE:
-                        retval = new PDActionThread(action);
-                        break;
-                    case PDActionEmbeddedGoTo.SUB_TYPE:
-                        retval = new PDActionEmbeddedGoTo(action);
-                        break;
-                    default:
-                        break;
-                }
-            }
+  /**
+   * This will create the correct type of action based on the type specified in
+   * the dictionary.
+   *
+   * @param action An action dictionary.
+   *
+   * @return An action of the correct type.
+   */
+  public static PDAction createAction(final COSDictionary action) {
+    PDAction retval = null;
+    if (action != null) {
+      final String type = action.getNameAsString(COSName.S);
+      if (type != null) {
+        switch (type) {
+        case PDActionJavaScript.SUB_TYPE:
+          retval = new PDActionJavaScript(action);
+          break;
+        case PDActionGoTo.SUB_TYPE:
+          retval = new PDActionGoTo(action);
+          break;
+        case PDActionLaunch.SUB_TYPE:
+          retval = new PDActionLaunch(action);
+          break;
+        case PDActionRemoteGoTo.SUB_TYPE:
+          retval = new PDActionRemoteGoTo(action);
+          break;
+        case PDActionURI.SUB_TYPE:
+          retval = new PDActionURI(action);
+          break;
+        case PDActionNamed.SUB_TYPE:
+          retval = new PDActionNamed(action);
+          break;
+        case PDActionSound.SUB_TYPE:
+          retval = new PDActionSound(action);
+          break;
+        case PDActionMovie.SUB_TYPE:
+          retval = new PDActionMovie(action);
+          break;
+        case PDActionImportData.SUB_TYPE:
+          retval = new PDActionImportData(action);
+          break;
+        case PDActionResetForm.SUB_TYPE:
+          retval = new PDActionResetForm(action);
+          break;
+        case PDActionHide.SUB_TYPE:
+          retval = new PDActionHide(action);
+          break;
+        case PDActionSubmitForm.SUB_TYPE:
+          retval = new PDActionSubmitForm(action);
+          break;
+        case PDActionThread.SUB_TYPE:
+          retval = new PDActionThread(action);
+          break;
+        case PDActionEmbeddedGoTo.SUB_TYPE:
+          retval = new PDActionEmbeddedGoTo(action);
+          break;
+        default:
+          break;
         }
-        return retval;
+      }
     }
+    return retval;
+  }
 
 }
