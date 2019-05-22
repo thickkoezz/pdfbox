0f8438459ec0c0b09c65a78ec5585a5bd36cc3a5
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:15:15 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionResetForm.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionResetForm.java
@@ -25,68 +25,62 @@ import org.apache.pdfbox.cos.COSName;
 /**
  * @author Timur Kamalov
  */
-public class PDActionResetForm extends PDAction
-{
+public class PDActionResetForm extends PDAction {
 
-    /**
-     * This type of action this object represents.
-     */
-    public static final String SUB_TYPE = "ResetForm";
+  /**
+   * This type of action this object represents.
+   */
+  public static final String SUB_TYPE = "ResetForm";
 
-    /**
-     * Default constructor.
-     */
-    public PDActionResetForm()
-    {
-        setSubType(SUB_TYPE);
-    }
+  /**
+   * Default constructor.
+   */
+  public PDActionResetForm() {
+    setSubType(PDActionResetForm.SUB_TYPE);
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param a The action dictionary.
-     */
-    public PDActionResetForm(COSDictionary a)
-    {
-        super(a);
-    }
+  /**
+   * Constructor.
+   *
+   * @param a The action dictionary.
+   */
+  public PDActionResetForm(final COSDictionary a) {
+    super(a);
+  }
 
-    /**
-     * An array identifying which fields to include in the submission or which to exclude, depending
-     * on the setting of the Include/Exclude flag in the Flags entry
-     *
-     * @return the array of fields
-     */
-    public COSArray getFields()
-    {
-        COSBase retval = this.action.getDictionaryObject(COSName.FIELDS);
-        return retval instanceof COSArray ? (COSArray) retval : null;
-    }
+  /**
+   * An array identifying which fields to include in the submission or which to
+   * exclude, depending on the setting of the Include/Exclude flag in the Flags
+   * entry
+   *
+   * @return the array of fields
+   */
+  public COSArray getFields() {
+    final COSBase retval = action.getDictionaryObject(COSName.FIELDS);
+    return retval instanceof COSArray ? (COSArray) retval : null;
+  }
 
-    /**
-     * @param array the array of fields
-     */
-    public void setFields(COSArray array)
-    {
-        this.action.setItem(COSName.FIELDS, array);
-    }
+  /**
+   * @param array the array of fields
+   */
+  public void setFields(final COSArray array) {
+    action.setItem(COSName.FIELDS, array);
+  }
 
-    /**
-     * A set of flags specifying various characteristics of the action
-     *
-     * @return the flags
-     */
-    public int getFlags()
-    {
-        return this.action.getInt(COSName.FLAGS, 0);
-    }
+  /**
+   * A set of flags specifying various characteristics of the action
+   *
+   * @return the flags
+   */
+  public int getFlags() {
+    return action.getInt(COSName.FLAGS, 0);
+  }
 
-    /**
-     * @param flags the flags
-     */
-    public void setFlags(int flags)
-    {
-        this.action.setInt(COSName.FLAGS, flags);
-    }
+  /**
+   * @param flags the flags
+   */
+  public void setFlags(final int flags) {
+    action.setInt(COSName.FLAGS, flags);
+  }
 
 }
