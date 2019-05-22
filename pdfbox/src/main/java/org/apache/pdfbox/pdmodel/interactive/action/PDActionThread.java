1f10eb7a6ad1d7976a6515e1a73faedfb92578c0
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:15:37 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionThread.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionThread.java
@@ -17,12 +17,12 @@
 
 package org.apache.pdfbox.pdmodel.interactive.action;
 
+import java.io.IOException;
+
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.pdmodel.common.filespecification.PDFileSpecification;
-
-import java.io.IOException;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDFileSpecification;
 
 /**
  *
@@ -30,85 +30,76 @@ import org.apache.pdfbox.cos.COSName;
  *
  * @author Evgeniy Muravitskiy
  */
-public class PDActionThread extends PDAction
-{
+public class PDActionThread extends PDAction {
 
-    /**
-     * This type of action this object represents.
-     */
-    public static final String SUB_TYPE = "Thread";
+  /**
+   * This type of action this object represents.
+   */
+  public static final String SUB_TYPE = "Thread";
 
-    /**
-     * Default constructor.
-     */
-    public PDActionThread()
-    {
-        setSubType(SUB_TYPE);
-    }
+  /**
+   * Default constructor.
+   */
+  public PDActionThread() {
+    setSubType(PDActionThread.SUB_TYPE);
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param a The action dictionary.
-     */
-    public PDActionThread(COSDictionary a)
-    {
-        super(a);
-    }
+  /**
+   * Constructor.
+   *
+   * @param a The action dictionary.
+   */
+  public PDActionThread(final COSDictionary a) {
+    super(a);
+  }
 
-    /**
-     * @return The D entry of the specific thread action dictionary.
-     */
-    // Dictionary, Integer or String.
-    public COSBase getD()
-    {
-        return action.getDictionaryObject(COSName.D);
-    }
+  /**
+   * @return The D entry of the specific thread action dictionary.
+   */
+  // Dictionary, Integer or String.
+  public COSBase getD() {
+    return action.getDictionaryObject(COSName.D);
+  }
 
-    /**
-     * @param d The destination.
-     */
-    public void setD(COSBase d)
-    {
-        action.setItem(COSName.D, d);
-    }
+  /**
+   * @param d The destination.
+   */
+  public void setD(final COSBase d) {
+    action.setItem(COSName.D, d);
+  }
 
-    /**
-     * This will get the file in which the destination is located.
-     *
-     * @return The F entry of the specific thread action dictionary.
-     * @throws IOException If there is an error creating the file spec.
-     */
-    public PDFileSpecification getFile() throws IOException
-    {
-        return PDFileSpecification.createFS(action.getDictionaryObject(COSName.F));
-    }
+  /**
+   * This will get the file in which the destination is located.
+   *
+   * @return The F entry of the specific thread action dictionary.
+   * @throws IOException If there is an error creating the file spec.
+   */
+  public PDFileSpecification getFile() throws IOException {
+    return PDFileSpecification.createFS(action.getDictionaryObject(COSName.F));
+  }
 
-    /**
-     * This will set the file in which the destination is located.
-     *
-     * @param fs The file specification.
-     */
-    public void setFile(PDFileSpecification fs)
-    {
-        action.setItem(COSName.F, fs);
-    }
+  /**
+   * This will set the file in which the destination is located.
+   *
+   * @param fs The file specification.
+   */
+  public void setFile(final PDFileSpecification fs) {
+    action.setItem(COSName.F, fs);
+  }
 
-    /**
-     * @return The B entry of the specific thread action dictionary.
-     */
-    // Dictionary or Integer.
-    public COSBase getB()
-    {
-        return action.getDictionaryObject(COSName.B);
-    }
+  /**
+   * @return The B entry of the specific thread action dictionary.
+   */
+  // Dictionary or Integer.
+  public COSBase getB() {
+    return action.getDictionaryObject(COSName.B);
+  }
 
-    /**
-     * @param b The destination.
-     */
-    public void setB(COSBase b)
-    {
-        action.setItem(COSName.B, b);
-    }
+  /**
+   * @param b The destination.
+   */
+  public void setB(final COSBase b) {
+    action.setItem(COSName.B, b);
+  }
 
 }
