a44c461062fb747375d4f88bcf541dbb265d0640
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:57:40 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDTextField.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDTextField.java
@@ -17,248 +17,227 @@
 package org.apache.pdfbox.pdmodel.interactive.form;
 
 import java.io.IOException;
+
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 
 /**
- * A text field is a box or space for text fill-in data typically entered from a keyboard.
- * The text may be restricted to a single line or may be permitted to span multiple lines
+ * A text field is a box or space for text fill-in data typically entered from a
+ * keyboard. The text may be restricted to a single line or may be permitted to
+ * span multiple lines
  *
  * @author sug
  */
-public final class PDTextField extends PDVariableText
-{
-    private static final int FLAG_MULTILINE = 1 << 12;
-    private static final int FLAG_PASSWORD = 1 << 13;
-    private static final int FLAG_FILE_SELECT = 1 << 20;
-    private static final int FLAG_DO_NOT_SPELL_CHECK = 1 << 22;
-    private static final int FLAG_DO_NOT_SCROLL = 1 << 23;
-    private static final int FLAG_COMB = 1 << 24;
-    private static final int FLAG_RICH_TEXT = 1 << 25;
-    
-    /**
-     * @see PDField#PDField(PDAcroForm)
-     *
-     * @param acroForm The acroform.
-     */
-    public PDTextField(PDAcroForm acroForm)
-    {
-        super(acroForm);
-        getCOSObject().setItem(COSName.FT, COSName.TX);
-    }
+public final class PDTextField extends PDVariableText {
+  private static final int FLAG_MULTILINE = 1 << 12;
+  private static final int FLAG_PASSWORD = 1 << 13;
+  private static final int FLAG_FILE_SELECT = 1 << 20;
+  private static final int FLAG_DO_NOT_SPELL_CHECK = 1 << 22;
+  private static final int FLAG_DO_NOT_SCROLL = 1 << 23;
+  private static final int FLAG_COMB = 1 << 24;
+  private static final int FLAG_RICH_TEXT = 1 << 25;
 
-    /**
-     * Constructor.
-     * 
-     * @param acroForm The form that this field is part of.
-     * @param field the PDF object to represent as a field.
-     * @param parent the parent node of the node
-     */
-    PDTextField(PDAcroForm acroForm, COSDictionary field, PDNonTerminalField parent)
-    {
-        super(acroForm, field, parent);
-    }
+  /**
+   * @see PDField#PDField(PDAcroForm)
+   *
+   * @param acroForm The acroform.
+   */
+  public PDTextField(final PDAcroForm acroForm) {
+    super(acroForm);
+    getCOSObject().setItem(COSName.FT, COSName.TX);
+  }
 
-    /**
-     * @return true if the field is multiline
-     */
-    public boolean isMultiline()
-    {
-        return getCOSObject().getFlag(COSName.FF, FLAG_MULTILINE);
-    }
+  /**
+   * Constructor.
+   *
+   * @param acroForm The form that this field is part of.
+   * @param field    the PDF object to represent as a field.
+   * @param parent   the parent node of the node
+   */
+  PDTextField(final PDAcroForm acroForm, final COSDictionary field, final PDNonTerminalField parent) {
+    super(acroForm, field, parent);
+  }
 
-    /**
-     * Set the multiline bit.
-     *
-     * @param multiline The value for the multiline.
-     */
-    public void setMultiline(boolean multiline)
-    {
-        getCOSObject().setFlag(COSName.FF, FLAG_MULTILINE, multiline);
-    }
+  /**
+   * @return true if the field is multiline
+   */
+  public boolean isMultiline() {
+    return getCOSObject().getFlag(COSName.FF, PDTextField.FLAG_MULTILINE);
+  }
 
-    /**
-     * @return true if the field is a password field.
-     */
-    public boolean isPassword()
-    {
-        return getCOSObject().getFlag(COSName.FF, FLAG_PASSWORD);
-    }
+  /**
+   * Set the multiline bit.
+   *
+   * @param multiline The value for the multiline.
+   */
+  public void setMultiline(final boolean multiline) {
+    getCOSObject().setFlag(COSName.FF, PDTextField.FLAG_MULTILINE, multiline);
+  }
 
-    /**
-     * Set the password bit.
-     *
-     * @param password The value for the password.
-     */
-    public void setPassword(boolean password)
-    {
-        getCOSObject().setFlag(COSName.FF, FLAG_PASSWORD, password);
-    }
+  /**
+   * @return true if the field is a password field.
+   */
+  public boolean isPassword() {
+    return getCOSObject().getFlag(COSName.FF, PDTextField.FLAG_PASSWORD);
+  }
 
-    /**
-     * @return true if the field is a file select field.
-     */
-    public boolean isFileSelect()
-    {
-        return getCOSObject().getFlag(COSName.FF, FLAG_FILE_SELECT);
-    }
+  /**
+   * Set the password bit.
+   *
+   * @param password The value for the password.
+   */
+  public void setPassword(final boolean password) {
+    getCOSObject().setFlag(COSName.FF, PDTextField.FLAG_PASSWORD, password);
+  }
 
-    /**
-     * Set the file select bit.
-     *
-     * @param fileSelect The value for the fileSelect.
-     */
-    public void setFileSelect(boolean fileSelect)
-    {
-        getCOSObject().setFlag(COSName.FF, FLAG_FILE_SELECT, fileSelect);
-    }
+  /**
+   * @return true if the field is a file select field.
+   */
+  public boolean isFileSelect() {
+    return getCOSObject().getFlag(COSName.FF, PDTextField.FLAG_FILE_SELECT);
+  }
 
-    /**
-     * @return true if the field is not suppose to spell check.
-     */
-    public boolean doNotSpellCheck()
-    {
-        return getCOSObject().getFlag(COSName.FF, FLAG_DO_NOT_SPELL_CHECK);
-    }
+  /**
+   * Set the file select bit.
+   *
+   * @param fileSelect The value for the fileSelect.
+   */
+  public void setFileSelect(final boolean fileSelect) {
+    getCOSObject().setFlag(COSName.FF, PDTextField.FLAG_FILE_SELECT, fileSelect);
+  }
 
-    /**
-     * Set the doNotSpellCheck bit.
-     *
-     * @param doNotSpellCheck The value for the doNotSpellCheck.
-     */
-    public void setDoNotSpellCheck(boolean doNotSpellCheck)
-    {
-        getCOSObject().setFlag(COSName.FF, FLAG_DO_NOT_SPELL_CHECK, doNotSpellCheck);
-    }
+  /**
+   * @return true if the field is not suppose to spell check.
+   */
+  public boolean doNotSpellCheck() {
+    return getCOSObject().getFlag(COSName.FF, PDTextField.FLAG_DO_NOT_SPELL_CHECK);
+  }
 
-    /**
-     * @return true if the field is not suppose to scroll.
-     */
-    public boolean doNotScroll()
-    {
-        return getCOSObject().getFlag(COSName.FF, FLAG_DO_NOT_SCROLL);
-    }
+  /**
+   * Set the doNotSpellCheck bit.
+   *
+   * @param doNotSpellCheck The value for the doNotSpellCheck.
+   */
+  public void setDoNotSpellCheck(final boolean doNotSpellCheck) {
+    getCOSObject().setFlag(COSName.FF, PDTextField.FLAG_DO_NOT_SPELL_CHECK, doNotSpellCheck);
+  }
 
-    /**
-     * Set the doNotScroll bit.
-     *
-     * @param doNotScroll The value for the doNotScroll.
-     */
-    public void setDoNotScroll(boolean doNotScroll)
-    {
-        getCOSObject().setFlag(COSName.FF, FLAG_DO_NOT_SCROLL, doNotScroll);
-    }
+  /**
+   * @return true if the field is not suppose to scroll.
+   */
+  public boolean doNotScroll() {
+    return getCOSObject().getFlag(COSName.FF, PDTextField.FLAG_DO_NOT_SCROLL);
+  }
 
-    /**
-     * @return true if the field is not suppose to comb the text display.
-     */
-    public boolean isComb()
-    {
-        return getCOSObject().getFlag(COSName.FF, FLAG_COMB);
-    }
+  /**
+   * Set the doNotScroll bit.
+   *
+   * @param doNotScroll The value for the doNotScroll.
+   */
+  public void setDoNotScroll(final boolean doNotScroll) {
+    getCOSObject().setFlag(COSName.FF, PDTextField.FLAG_DO_NOT_SCROLL, doNotScroll);
+  }
 
-    /**
-     * Set the comb bit.
-     *
-     * @param comb The value for the comb.
-     */
-    public void setComb(boolean comb)
-    {
-        getCOSObject().setFlag(COSName.FF, FLAG_COMB, comb);
-    }
+  /**
+   * @return true if the field is not suppose to comb the text display.
+   */
+  public boolean isComb() {
+    return getCOSObject().getFlag(COSName.FF, PDTextField.FLAG_COMB);
+  }
 
-    /**
-     * @return true if the field is a rich text field.
-     */
-    public boolean isRichText()
-    {
-        return getCOSObject().getFlag(COSName.FF, FLAG_RICH_TEXT);
-    }
+  /**
+   * Set the comb bit.
+   *
+   * @param comb The value for the comb.
+   */
+  public void setComb(final boolean comb) {
+    getCOSObject().setFlag(COSName.FF, PDTextField.FLAG_COMB, comb);
+  }
 
-    /**
-     * Set the richText bit.
-     *
-     * @param richText The value for the richText.
-     */
-    public void setRichText(boolean richText)
-    {
-        getCOSObject().setFlag(COSName.FF, FLAG_RICH_TEXT, richText);
-    }
-    
-    /**
-     * Returns the maximum number of characters of the text field.
-     * 
-     * @return the maximum number of characters, returns -1 if the value isn't present
-     */
-    public int getMaxLen()
-    {
-        return getCOSObject().getInt(COSName.MAX_LEN);
-    }
+  /**
+   * @return true if the field is a rich text field.
+   */
+  public boolean isRichText() {
+    return getCOSObject().getFlag(COSName.FF, PDTextField.FLAG_RICH_TEXT);
+  }
 
-    /**
-     * Sets the maximum number of characters of the text field.
-     * 
-     * @param maxLen the maximum number of characters
-     */
-    public void setMaxLen(int maxLen)
-    {
-        getCOSObject().setInt(COSName.MAX_LEN, maxLen);
-    }
+  /**
+   * Set the richText bit.
+   *
+   * @param richText The value for the richText.
+   */
+  public void setRichText(final boolean richText) {
+    getCOSObject().setFlag(COSName.FF, PDTextField.FLAG_RICH_TEXT, richText);
+  }
 
-    /**
-     * Set the plain text value of this field, and try to update the visual appearance.
-     * 
-     * @param value Plain text
-     * @throws IOException if the value could not be set
-     */
-    @Override
-    public void setValue(String value) throws IOException
-    {
-        getCOSObject().setString(COSName.V, value);
-        applyChange();
-    }
+  /**
+   * Returns the maximum number of characters of the text field.
+   *
+   * @return the maximum number of characters, returns -1 if the value isn't
+   *         present
+   */
+  public int getMaxLen() {
+    return getCOSObject().getInt(COSName.MAX_LEN);
+  }
 
-    /**
-     * Sets the default value of this field.
-     *
-     * @param value Plain text
-     */
-    public void setDefaultValue(String value)
-    {
-        getCOSObject().setString(COSName.DV, value);
-    }
+  /**
+   * Sets the maximum number of characters of the text field.
+   *
+   * @param maxLen the maximum number of characters
+   */
+  public void setMaxLen(final int maxLen) {
+    getCOSObject().setInt(COSName.MAX_LEN, maxLen);
+  }
 
-    /**
-     * Returns the value of this field, or an empty string.
-     * 
-     * @return A non-null string.
-     */
-    public String getValue()
-    {
-        return getStringOrStream(getInheritableAttribute(COSName.V));
-    }
+  /**
+   * Set the plain text value of this field, and try to update the visual
+   * appearance.
+   *
+   * @param value Plain text
+   * @throws IOException if the value could not be set
+   */
+  @Override
+  public void setValue(final String value) throws IOException {
+    getCOSObject().setString(COSName.V, value);
+    applyChange();
+  }
 
-    /**
-     * Returns the default value of this field, or an empty string.
-     *
-     * @return A non-null string.
-     */
-    public String getDefaultValue()
-    {
-        return getStringOrStream(getInheritableAttribute(COSName.DV));
-    }
+  /**
+   * Sets the default value of this field.
+   *
+   * @param value Plain text
+   */
+  public void setDefaultValue(final String value) {
+    getCOSObject().setString(COSName.DV, value);
+  }
 
-    @Override
-    public String getValueAsString()
-    {
-        return getValue();
-    }
-    
-    @Override
-    void constructAppearances() throws IOException
-    {
-        AppearanceGeneratorHelper apHelper;
-        apHelper = new AppearanceGeneratorHelper(this);
-        apHelper.setAppearanceValue(getValue());
-    }
+  /**
+   * Returns the value of this field, or an empty string.
+   *
+   * @return A non-null string.
+   */
+  public String getValue() {
+    return getStringOrStream(getInheritableAttribute(COSName.V));
+  }
+
+  /**
+   * Returns the default value of this field, or an empty string.
+   *
+   * @return A non-null string.
+   */
+  public String getDefaultValue() {
+    return getStringOrStream(getInheritableAttribute(COSName.DV));
+  }
+
+  @Override
+  public String getValueAsString() {
+    return getValue();
+  }
+
+  @Override
+  void constructAppearances() throws IOException {
+    AppearanceGeneratorHelper apHelper;
+    apHelper = new AppearanceGeneratorHelper(this);
+    apHelper.setAppearanceValue(getValue());
+  }
 }
