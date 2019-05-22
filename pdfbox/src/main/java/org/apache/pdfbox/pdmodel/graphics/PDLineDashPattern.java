05190d00938ebafafdd454137c4f05aeb55a2ca8
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:45:48 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/PDLineDashPattern.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/PDLineDashPattern.java
@@ -16,78 +16,74 @@
  */
 package org.apache.pdfbox.pdmodel.graphics;
 
+import java.util.Arrays;
+
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSInteger;
-
 import org.apache.pdfbox.pdmodel.common.COSObjectable;
 
-import java.util.Arrays;
-
 /**
- * A line dash pattern for stroking paths.
- * Instances of PDLineDashPattern are immutable.
+ * A line dash pattern for stroking paths. Instances of PDLineDashPattern are
+ * immutable.
+ *
  * @author Ben Litchfield
  * @author John Hewson
  */
-public final class PDLineDashPattern implements COSObjectable
-{
-    private final int phase;
-    private final float[] array;
+public final class PDLineDashPattern implements COSObjectable {
+  private final int phase;
+  private final float[] array;
 
-    /**
-     * Creates a new line dash pattern, with no dashes and a phase of 0.
-     */
-    public PDLineDashPattern()
-    {
-        array = new float[] { };
-        phase = 0;
-    }
+  /**
+   * Creates a new line dash pattern, with no dashes and a phase of 0.
+   */
+  public PDLineDashPattern() {
+    array = new float[] {};
+    phase = 0;
+  }
 
-    /**
-     * Creates a new line dash pattern from a dash array and phase.
-     * @param array the dash array
-     * @param phase the phase
-     */
-    public PDLineDashPattern(COSArray array, int phase)
-    {
-        this.array = array.toFloatArray();
-        this.phase = phase;
-    }
+  /**
+   * Creates a new line dash pattern from a dash array and phase.
+   *
+   * @param array the dash array
+   * @param phase the phase
+   */
+  public PDLineDashPattern(final COSArray array, final int phase) {
+    this.array = array.toFloatArray();
+    this.phase = phase;
+  }
 
-    @Override
-    public COSBase getCOSObject()
-    {
-        COSArray cos = new COSArray();
-        COSArray patternArray = new COSArray();
-        patternArray.setFloatArray(array);
-        cos.add(patternArray);
-        cos.add(COSInteger.get(phase));
-        return cos;
-    }
+  @Override
+  public COSBase getCOSObject() {
+    final COSArray cos = new COSArray();
+    final COSArray patternArray = new COSArray();
+    patternArray.setFloatArray(array);
+    cos.add(patternArray);
+    cos.add(COSInteger.get(phase));
+    return cos;
+  }
 
-    /**
-     * Returns the dash phase.
-     * This specifies the distance into the dash pattern at which to start the dash.
-     * @return the dash phase
-     */
-    public int getPhase()
-    {
-        return phase;
-    }
+  /**
+   * Returns the dash phase. This specifies the distance into the dash pattern at
+   * which to start the dash.
+   *
+   * @return the dash phase
+   */
+  public int getPhase() {
+    return phase;
+  }
 
-    /**
-     * Returns the dash array.
-     * @return the dash array, never null.
-     */
-    public float[] getDashArray()
-    {
-        return array.clone();
-    }
+  /**
+   * Returns the dash array.
+   *
+   * @return the dash array, never null.
+   */
+  public float[] getDashArray() {
+    return array.clone();
+  }
 
-    @Override
-    public String toString()
-    {
-        return "PDLineDashPattern{array=" + Arrays.toString(array) + ", phase=" + phase + "}";
-    }
+  @Override
+  public String toString() {
+    return "PDLineDashPattern{array=" + Arrays.toString(array) + ", phase=" + phase + "}";
+  }
 }
