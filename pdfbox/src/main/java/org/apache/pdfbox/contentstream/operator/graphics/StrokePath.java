21bb88274bc8190d278afcedf82b0167cb798a77
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:37:51 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/StrokePath.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/StrokePath.java
@@ -16,30 +16,26 @@
  */
 package org.apache.pdfbox.contentstream.operator.graphics;
 
+import java.io.IOException;
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
-
-import java.io.IOException;
+import org.apache.pdfbox.cos.COSBase;
 
 /**
  * S Stroke the path.
  *
  * @author Ben Litchfield
  */
-public final class StrokePath extends GraphicsOperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> operands) throws IOException
-    {
-        context.strokePath();
-    }
+public final class StrokePath extends GraphicsOperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> operands) throws IOException {
+    context.strokePath();
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.STROKE_PATH;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.STROKE_PATH;
+  }
 }
