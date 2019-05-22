aaacb2dc3902193afcfcca6c2590abb419ffcb7b
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:16:41 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationCaret.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationCaret.java
@@ -27,99 +27,93 @@ import org.apache.pdfbox.pdmodel.interactive.annotation.handlers.PDCaretAppearan
  *
  * @author Paul King
  */
-public class PDAnnotationCaret extends PDAnnotationMarkup
-{
-    /**
-     * The type of annotation.
-     */
-    public static final String SUB_TYPE = "Caret";
+public class PDAnnotationCaret extends PDAnnotationMarkup {
+  /**
+   * The type of annotation.
+   */
+  public static final String SUB_TYPE = "Caret";
 
-    private PDAppearanceHandler customAppearanceHandler;
+  private PDAppearanceHandler customAppearanceHandler;
 
-    public PDAnnotationCaret()
-    {
-        getCOSObject().setName(COSName.SUBTYPE, SUB_TYPE);
-    }
+  public PDAnnotationCaret() {
+    getCOSObject().setName(COSName.SUBTYPE, PDAnnotationCaret.SUB_TYPE);
+  }
 
-    /**
-     * Creates a Caret annotation from a COSDictionary, expected to be a correct object definition.
-     *
-     * @param field the PDF object to represent as a field.
-     */
-    public PDAnnotationCaret(COSDictionary field)
-    {
-        super(field);
-    }
-    
-    /**
-     * This will set the difference between the annotations "outer" rectangle defined by
-     * /Rect and boundaries of the underlying.
-     * 
-     * <p>This will set an equal difference for all sides</p>
-     * 
-     * @param difference from the annotations /Rect entry
-     */
-    public void setRectDifferences(float difference) {
-        setRectDifferences(difference, difference, difference, difference);
-    }
-    
-    /**
-     * This will set the difference between the annotations "outer" rectangle defined by
-     * /Rect and the border.
-     * 
-     * @param differenceLeft left difference from the annotations /Rect entry
-     * @param differenceTop top difference from the annotations /Rect entry
-     * @param differenceRight right difference from  the annotations /Rect entry
-     * @param differenceBottom bottom difference from the annotations /Rect entry
-     * 
-     */
-    public void setRectDifferences(float differenceLeft, float differenceTop, float differenceRight, float differenceBottom)
-    {
-        COSArray margins = new COSArray();
-        margins.add(new COSFloat(differenceLeft));
-        margins.add(new COSFloat(differenceTop));
-        margins.add(new COSFloat(differenceRight));
-        margins.add(new COSFloat(differenceBottom));
-        getCOSObject().setItem(COSName.RD, margins);    
-    }
-    
-    /**
-     * This will get the margin between the annotations "outer" rectangle defined by
-     * /Rect and the boundaries of the underlying caret.
-     * 
-     * @return the differences. If the entry hasn't been set am empty array is returned.
-     */
-    public float[] getRectDifferences()
-    {
-        COSBase margin = getCOSObject().getItem(COSName.RD);
-        if (margin instanceof COSArray)
-        {
-            return ((COSArray) margin).toFloatArray();
-        }
-        return new float[]{};
-    }
-    
-    /**
-     * Set a custom appearance handler for generating the annotations appearance streams.
-     * 
-     * @param appearanceHandler
-     */
-    public void setCustomAppearanceHandler(PDAppearanceHandler appearanceHandler)
-    {
-        customAppearanceHandler = appearanceHandler;
-    }
+  /**
+   * Creates a Caret annotation from a COSDictionary, expected to be a correct
+   * object definition.
+   *
+   * @param field the PDF object to represent as a field.
+   */
+  public PDAnnotationCaret(final COSDictionary field) {
+    super(field);
+  }
 
-    @Override
-    public void constructAppearances()
-    {
-        if (customAppearanceHandler == null)
-        {
-            PDCaretAppearanceHandler appearanceHandler = new PDCaretAppearanceHandler(this);
-            appearanceHandler.generateAppearanceStreams();
-        }
-        else
-        {
-            customAppearanceHandler.generateAppearanceStreams();
-        }
+  /**
+   * This will set the difference between the annotations "outer" rectangle
+   * defined by /Rect and boundaries of the underlying.
+   *
+   * <p>
+   * This will set an equal difference for all sides
+   * </p>
+   *
+   * @param difference from the annotations /Rect entry
+   */
+  public void setRectDifferences(final float difference) {
+    setRectDifferences(difference, difference, difference, difference);
+  }
+
+  /**
+   * This will set the difference between the annotations "outer" rectangle
+   * defined by /Rect and the border.
+   *
+   * @param differenceLeft   left difference from the annotations /Rect entry
+   * @param differenceTop    top difference from the annotations /Rect entry
+   * @param differenceRight  right difference from the annotations /Rect entry
+   * @param differenceBottom bottom difference from the annotations /Rect entry
+   *
+   */
+  public void setRectDifferences(final float differenceLeft, final float differenceTop, final float differenceRight,
+      final float differenceBottom) {
+    final COSArray margins = new COSArray();
+    margins.add(new COSFloat(differenceLeft));
+    margins.add(new COSFloat(differenceTop));
+    margins.add(new COSFloat(differenceRight));
+    margins.add(new COSFloat(differenceBottom));
+    getCOSObject().setItem(COSName.RD, margins);
+  }
+
+  /**
+   * This will get the margin between the annotations "outer" rectangle defined by
+   * /Rect and the boundaries of the underlying caret.
+   *
+   * @return the differences. If the entry hasn't been set am empty array is
+   *         returned.
+   */
+  public float[] getRectDifferences() {
+    final COSBase margin = getCOSObject().getItem(COSName.RD);
+    if (margin instanceof COSArray)
+      return ((COSArray) margin).toFloatArray();
+    return new float[] {};
+  }
+
+  /**
+   * Set a custom appearance handler for generating the annotations appearance
+   * streams.
+   *
+   * @param appearanceHandler
+   */
+  public void setCustomAppearanceHandler(final PDAppearanceHandler appearanceHandler) {
+    customAppearanceHandler = appearanceHandler;
+  }
+
+  @Override
+  public void constructAppearances() {
+    if (customAppearanceHandler == null) {
+      final PDCaretAppearanceHandler appearanceHandler = new PDCaretAppearanceHandler(this);
+      appearanceHandler.generateAppearanceStreams();
+    } else {
+      customAppearanceHandler.generateAppearanceStreams();
     }
+  }
 }
