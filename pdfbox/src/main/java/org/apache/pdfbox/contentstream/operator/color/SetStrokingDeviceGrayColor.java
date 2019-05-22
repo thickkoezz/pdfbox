8a7cf6fecc0086532ff45a31e824ef9f8c4d9aaa
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:35:44 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetStrokingDeviceGrayColor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetStrokingDeviceGrayColor.java
@@ -16,34 +16,31 @@
  */
 package org.apache.pdfbox.contentstream.operator.color;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
-import org.apache.pdfbox.contentstream.operator.Operator;
-import org.apache.pdfbox.contentstream.operator.OperatorName;
-
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.pdfbox.contentstream.operator.Operator;
+import org.apache.pdfbox.contentstream.operator.OperatorName;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
+
 /**
- * G: Set the stroking colour space to DeviceGray and set the gray level to use for stroking
- * operations.
+ * G: Set the stroking colour space to DeviceGray and set the gray level to use
+ * for stroking operations.
  *
  * @author John Hewson
  */
-public class SetStrokingDeviceGrayColor extends SetStrokingColor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        PDColorSpace cs = context.getResources().getColorSpace(COSName.DEVICEGRAY);
-        context.getGraphicsState().setStrokingColorSpace(cs);
-        super.process(operator, arguments);
-    }
+public class SetStrokingDeviceGrayColor extends SetStrokingColor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    final PDColorSpace cs = context.getResources().getColorSpace(COSName.DEVICEGRAY);
+    context.getGraphicsState().setStrokingColorSpace(cs);
+    super.process(operator, arguments);
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.STROKING_COLOR_GRAY;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.STROKING_COLOR_GRAY;
+  }
 }
