c14580f65b443f3a61b6e2600bab99ffeb885b90
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:46:48 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDMMType1Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDMMType1Font.java
@@ -16,24 +16,22 @@
  */
 package org.apache.pdfbox.pdmodel.font;
 
-import org.apache.pdfbox.cos.COSDictionary;
-
 import java.io.IOException;
+
+import org.apache.pdfbox.cos.COSDictionary;
 
 /**
  * Type 1 Multiple Master Font.
  *
  * @author Ben Litchfield
  */
-public class PDMMType1Font extends PDType1Font
-{
-    /**
-     * Creates an MMType1Font from a Font dictionary in a PDF.
-     *
-     * @param fontDictionary font dictionary
-     */
-    public PDMMType1Font(COSDictionary fontDictionary) throws IOException
-    {
-        super(fontDictionary);
-    }
+public class PDMMType1Font extends PDType1Font {
+  /**
+   * Creates an MMType1Font from a Font dictionary in a PDF.
+   *
+   * @param fontDictionary font dictionary
+   */
+  public PDMMType1Font(final COSDictionary fontDictionary) throws IOException {
+    super(fontDictionary);
+  }
 }
