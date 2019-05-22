ffcc00fb85e2b054de79600c258ba68cbc3ad72f
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:22:39 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAppearanceStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAppearanceStream.java
@@ -22,31 +22,28 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
 
 /**
- * An appearance stream is a form XObject, a self-contained content stream that shall be rendered inside the annotation
- * rectangle.
+ * An appearance stream is a form XObject, a self-contained content stream that
+ * shall be rendered inside the annotation rectangle.
  *
  * @author Ben Litchfield
  * @author John Hewson
  */
-public class PDAppearanceStream extends PDFormXObject
-{
-    /**
-     * Creates a Form XObject for reading.
-     * 
-     * @param stream The XObject stream
-     */
-    public PDAppearanceStream(COSStream stream)
-    {
-        super(stream);
-    }
+public class PDAppearanceStream extends PDFormXObject {
+  /**
+   * Creates a Form XObject for reading.
+   *
+   * @param stream The XObject stream
+   */
+  public PDAppearanceStream(final COSStream stream) {
+    super(stream);
+  }
 
-    /**
-     * Creates a Form Image XObject for writing, in the given document.
-     * 
-     * @param document The current document
-     */
-    public PDAppearanceStream(PDDocument document)
-    {
-        super(document);
-    }
+  /**
+   * Creates a Form Image XObject for writing, in the given document.
+   *
+   * @param document The current document
+   */
+  public PDAppearanceStream(final PDDocument document) {
+    super(document);
+  }
 }
