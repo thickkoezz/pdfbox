6083e7a285d0e754f6553461c285101ee93d71f8
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:27:23 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceColorSpace.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceColorSpace.java
@@ -20,21 +20,19 @@ import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSName;
 
 /**
- * Device colour spaces directly specify colours or shades of gray produced by an output device.
+ * Device colour spaces directly specify colours or shades of gray produced by
+ * an output device.
  *
  * @author John Hewson
  */
-public abstract class PDDeviceColorSpace extends PDColorSpace
-{
-    @Override
-    public String toString()
-    {
-        return getName();
-    }
+public abstract class PDDeviceColorSpace extends PDColorSpace {
+  @Override
+  public String toString() {
+    return getName();
+  }
 
-    @Override
-    public COSBase getCOSObject()
-    {
-        return COSName.getPDFName(getName());
-    }
+  @Override
+  public COSBase getCOSObject() {
+    return COSName.getPDFName(getName());
+  }
 }
