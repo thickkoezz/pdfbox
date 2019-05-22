516f58d08d7dba979f160788153cb3084ebdbc33
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:34:36 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSNumber.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSNumber.java
@@ -23,83 +23,64 @@ import java.io.IOException;
  *
  * @author Ben Litchfield
  */
-public abstract class COSNumber extends COSBase
-{
-    /**
-     * This will get the float value of this number.
-     *
-     * @return The float value of this object.
-     */
-    public abstract float floatValue();
+public abstract class COSNumber extends COSBase {
+  /**
+   * This will get the float value of this number.
+   *
+   * @return The float value of this object.
+   */
+  public abstract float floatValue();
 
-    /**
-     * This will get the double value of this number.
-     *
-     * @return The double value of this number.
-     */
-    public abstract double doubleValue();
+  /**
+   * This will get the double value of this number.
+   *
+   * @return The double value of this number.
+   */
+  public abstract double doubleValue();
 
-    /**
-     * This will get the integer value of this number.
-     *
-     * @return The integer value of this number.
-     */
-    public abstract int intValue();
+  /**
+   * This will get the integer value of this number.
+   *
+   * @return The integer value of this number.
+   */
+  public abstract int intValue();
 
-    /**
-     * This will get the long value of this number.
-     *
-     * @return The long value of this number.
-     */
-    public abstract long longValue();
+  /**
+   * This will get the long value of this number.
+   *
+   * @return The long value of this number.
+   */
+  public abstract long longValue();
 
-    /**
-     * This factory method will get the appropriate number object.
-     *
-     * @param number The string representation of the number.
-     *
-     * @return A number object, either float or int.
-     *
-     * @throws IOException If the string is not a number.
-     */
-    public static COSNumber get( String number ) throws IOException
-    {
-        if (number.length() == 1) 
-        {
-            char digit = number.charAt(0);
-            if ('0' <= digit && digit <= '9') 
-            {
-                return COSInteger.get(digit - '0');
-            } 
-            else if (digit == '-' || digit == '.') 
-            {
-                // See https://issues.apache.org/jira/browse/PDFBOX-592
-                return COSInteger.ZERO;
-            } 
-            else 
-            {
-                throw new IOException("Not a number: " + number);
-            }
-        } 
-        else if (number.indexOf('.') == -1 && (number.toLowerCase().indexOf('e') == -1)) 
-        {
-            try
-            {
-                if (number.charAt(0) == '+')
-                {
-                    return COSInteger.get(Long.parseLong(number.substring(1)));
-                }
-                return COSInteger.get(Long.parseLong(number));
-            }
-            catch( NumberFormatException e )
-            {
-                // might be a huge number, see PDFBOX-3116
-                return new COSFloat(number);
-            }
-        } 
-        else 
-        {
-            return new COSFloat(number);
-        }
-    }
+  /**
+   * This factory method will get the appropriate number object.
+   *
+   * @param number The string representation of the number.
+   *
+   * @return A number object, either float or int.
+   *
+   * @throws IOException If the string is not a number.
+   */
+  public static COSNumber get(final String number) throws IOException {
+    if (number.length() == 1) {
+      final char digit = number.charAt(0);
+      if ('0' <= digit && digit <= '9')
+        return COSInteger.get(digit - '0');
+      else if (digit == '-' || digit == '.')
+        // See https://issues.apache.org/jira/browse/PDFBOX-592
+        return COSInteger.ZERO;
+      else
+        throw new IOException("Not a number: " + number);
+    } else if (number.indexOf('.') == -1 && number.toLowerCase().indexOf('e') == -1) {
+      try {
+        if (number.charAt(0) == '+')
+          return COSInteger.get(Long.parseLong(number.substring(1)));
+        return COSInteger.get(Long.parseLong(number));
+      } catch (final NumberFormatException e) {
+        // might be a huge number, see PDFBOX-3116
+        return new COSFloat(number);
+      }
+    } else
+      return new COSFloat(number);
+  }
 }
