f32a0463816e46e847500b9ebf5ee3a821ca95c4
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:29:55 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/util/Charsets.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/Charsets.java
@@ -24,25 +24,25 @@ import java.nio.charset.Charset;
  *
  * @author John Hewson
  */
-public final class Charsets
-{
-    private Charsets() {}
+public final class Charsets {
+  private Charsets() {
+  }
 
-    /*** ASCII charset */
-    public static final Charset US_ASCII = Charset.forName("US-ASCII");
+  /*** ASCII charset */
+  public static final Charset US_ASCII = Charset.forName("US-ASCII");
 
-    /*** UTF-16BE charset */
-    public static final Charset UTF_16BE = Charset.forName("UTF-16BE");
+  /*** UTF-16BE charset */
+  public static final Charset UTF_16BE = Charset.forName("UTF-16BE");
 
-    /*** UTF-16LE charset */
-    public static final Charset UTF_16LE = Charset.forName("UTF-16LE");
-    
-    /*** ISO-8859-1 charset */
-    public static final Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
+  /*** UTF-16LE charset */
+  public static final Charset UTF_16LE = Charset.forName("UTF-16LE");
 
-    /*** Windows-1252 charset */
-    public static final Charset WINDOWS_1252 = Charset.forName("Windows-1252");
+  /*** ISO-8859-1 charset */
+  public static final Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
 
-    /*** UTF-8 charset */
-    public static final Charset UTF_8 = Charset.forName("UTF-8");
+  /*** Windows-1252 charset */
+  public static final Charset WINDOWS_1252 = Charset.forName("Windows-1252");
+
+  /*** UTF-8 charset */
+  public static final Charset UTF_8 = Charset.forName("UTF-8");
 }
