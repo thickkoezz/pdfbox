93cf4db03935cc3d20d3aa108a90ff15538c1a95
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:36:15 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetWordSpacing.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetWordSpacing.java
@@ -18,38 +18,31 @@ package org.apache.pdfbox.contentstream.operator.text;
 
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSNumber;
 
 /**
  * Tw: Set word spacing.
  *
  * @author Laurent Huault
  */
-public class SetWordSpacing extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments)
-    {
-        if (arguments.isEmpty())
-        {
-            return;
-        }
-        COSBase base = arguments.get(0);
-        if (!(base instanceof COSNumber))
-        {
-            return;
-        }
-        COSNumber wordSpacing = (COSNumber) base;
-        context.getGraphicsState().getTextState().setWordSpacing( wordSpacing.floatValue() );
-    }
+public class SetWordSpacing extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) {
+    if (arguments.isEmpty())
+      return;
+    final COSBase base = arguments.get(0);
+    if (!(base instanceof COSNumber))
+      return;
+    final COSNumber wordSpacing = (COSNumber) base;
+    context.getGraphicsState().getTextState().setWordSpacing(wordSpacing.floatValue());
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_WORD_SPACING;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_WORD_SPACING;
+  }
 }
