17b40f80420809181936835f71625905a81b315e
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:16:03 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionURI.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionURI.java
@@ -28,94 +28,82 @@ import org.apache.pdfbox.util.Charsets;
  * @author Ben Litchfield
  * @author Panagiotis Toumasis
  */
-public class PDActionURI extends PDAction
-{
-    /**
-     * This type of action this object represents.
-     */
-    public static final String SUB_TYPE = "URI";
+public class PDActionURI extends PDAction {
+  /**
+   * This type of action this object represents.
+   */
+  public static final String SUB_TYPE = "URI";
 
-    /**
-     * Default constructor.
-     */
-    public PDActionURI()
-    {
-        setSubType(SUB_TYPE);
-    }
+  /**
+   * Default constructor.
+   */
+  public PDActionURI() {
+    setSubType(PDActionURI.SUB_TYPE);
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param a The action dictionary.
-     */
-    public PDActionURI(COSDictionary a)
-    {
-        super(a);
-    }
+  /**
+   * Constructor.
+   *
+   * @param a The action dictionary.
+   */
+  public PDActionURI(final COSDictionary a) {
+    super(a);
+  }
 
-    /**
-     * This will get the uniform resource identifier to resolve. It should be encoded in 7-bit
-     * ASCII, but UTF-8 and UTF-16 are supported too.
-     *
-     * @return The URI entry of the specific URI action dictionary or null if there isn't any.
-     */
-    public String getURI()
-    {
-        COSBase base = action.getDictionaryObject(COSName.URI);
-        if (base instanceof COSString)
-        {
-            byte[] bytes = ((COSString) base).getBytes();
-            if (bytes.length >= 2)
-            {
-                // UTF-16 (BE)
-                if ((bytes[0] & 0xFF) == 0xFE && (bytes[1] & 0xFF) == 0xFF)
-                {
-                    return action.getString(COSName.URI);
-                }
-                // UTF-16 (LE)
-                if ((bytes[0] & 0xFF) == 0xFF && (bytes[1] & 0xFF) == 0xFE)
-                {
-                    return action.getString(COSName.URI);
-                }
-            }
-            return new String(bytes, Charsets.UTF_8);
-        }
-        return null;
+  /**
+   * This will get the uniform resource identifier to resolve. It should be
+   * encoded in 7-bit ASCII, but UTF-8 and UTF-16 are supported too.
+   *
+   * @return The URI entry of the specific URI action dictionary or null if there
+   *         isn't any.
+   */
+  public String getURI() {
+    final COSBase base = action.getDictionaryObject(COSName.URI);
+    if (base instanceof COSString) {
+      final byte[] bytes = ((COSString) base).getBytes();
+      if (bytes.length >= 2) {
+        // UTF-16 (BE)
+        if ((bytes[0] & 0xFF) == 0xFE && (bytes[1] & 0xFF) == 0xFF)
+          return action.getString(COSName.URI);
+        // UTF-16 (LE)
+        if ((bytes[0] & 0xFF) == 0xFF && (bytes[1] & 0xFF) == 0xFE)
+          return action.getString(COSName.URI);
+      }
+      return new String(bytes, Charsets.UTF_8);
     }
+    return null;
+  }
 
-    /**
-     * This will set the uniform resource identifier to resolve, encoded in
-     * 7-bit ASCII.
-     *
-     * @param uri The uniform resource identifier.
-     */
-    public void setURI(String uri)
-    {
-        action.setString(COSName.URI, uri);
-    }
+  /**
+   * This will set the uniform resource identifier to resolve, encoded in 7-bit
+   * ASCII.
+   *
+   * @param uri The uniform resource identifier.
+   */
+  public void setURI(final String uri) {
+    action.setString(COSName.URI, uri);
+  }
 
-    /**
-     * This will specify whether to track the mouse position when the URI is
-     * resolved. Default value: false. This entry applies only to actions
-     * triggered by the user's clicking an annotation; it is ignored for actions
-     * associated with outline items or with a document's OpenAction entry.
-     *
-     * @return A flag specifying whether to track the mouse position when the
-     * URI is resolved.
-     */
-    public boolean shouldTrackMousePosition()
-    {
-        return this.action.getBoolean("IsMap", false);
-    }
+  /**
+   * This will specify whether to track the mouse position when the URI is
+   * resolved. Default value: false. This entry applies only to actions triggered
+   * by the user's clicking an annotation; it is ignored for actions associated
+   * with outline items or with a document's OpenAction entry.
+   *
+   * @return A flag specifying whether to track the mouse position when the URI is
+   *         resolved.
+   */
+  public boolean shouldTrackMousePosition() {
+    return action.getBoolean("IsMap", false);
+  }
 
-    /**
-     * This will specify whether to track the mouse position when the URI is
-     * resolved.
-     *
-     * @param value The flag value.
-     */
-    public void setTrackMousePosition(boolean value)
-    {
-        this.action.setBoolean("IsMap", value);
-    }
+  /**
+   * This will specify whether to track the mouse position when the URI is
+   * resolved.
+   *
+   * @param value The flag value.
+   */
+  public void setTrackMousePosition(final boolean value) {
+    action.setBoolean("IsMap", value);
+  }
 }
