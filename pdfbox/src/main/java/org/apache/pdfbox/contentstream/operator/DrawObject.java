2591b716483b1e94d18c9dbfebf3429d03b6123d
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:40:36 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/DrawObject.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/DrawObject.java
@@ -16,14 +16,14 @@
  */
 package org.apache.pdfbox.contentstream.operator;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
 import org.apache.pdfbox.pdmodel.graphics.form.PDTransparencyGroup;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Do: Draws an XObject.
@@ -31,44 +31,32 @@ import java.util.List;
  * @author Ben Litchfield
  * @author Mario Ivankovits
  */
-public class DrawObject extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        if (arguments.isEmpty())
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
-        COSBase base0 = arguments.get(0);
-        if (!(base0 instanceof COSName))
-        {
-            return;
-        }
-        COSName name = (COSName) base0;
+public class DrawObject extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    if (arguments.isEmpty())
+      throw new MissingOperandException(operator, arguments);
+    final COSBase base0 = arguments.get(0);
+    if (!(base0 instanceof COSName))
+      return;
+    final COSName name = (COSName) base0;
 
-        if (context.getResources().isImageXObject(name))
-        {
-            // we're done here, don't decode images when doing text extraction
-            return;
-        }
-        
-        PDXObject xobject = context.getResources().getXObject(name);
+    if (context.getResources().isImageXObject(name))
+      // we're done here, don't decode images when doing text extraction
+      return;
 
-        if (xobject instanceof PDTransparencyGroup)
-        {
-            context.showTransparencyGroup((PDTransparencyGroup) xobject);
-        }
-        else if (xobject instanceof PDFormXObject)
-        {
-            PDFormXObject form = (PDFormXObject) xobject;
-            context.showForm(form);
-        }
+    final PDXObject xobject = context.getResources().getXObject(name);
+
+    if (xobject instanceof PDTransparencyGroup) {
+      context.showTransparencyGroup((PDTransparencyGroup) xobject);
+    } else if (xobject instanceof PDFormXObject) {
+      final PDFormXObject form = (PDFormXObject) xobject;
+      context.showForm(form);
     }
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.DRAW_OBJECT;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.DRAW_OBJECT;
+  }
 }
