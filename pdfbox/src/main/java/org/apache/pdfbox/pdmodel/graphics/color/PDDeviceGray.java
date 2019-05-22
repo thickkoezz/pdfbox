77238b3550615684111fee0c1d246639fd27c873
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:27:34 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceGray.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceGray.java
@@ -16,11 +16,11 @@
  */
 package org.apache.pdfbox.pdmodel.graphics.color;
 
-import org.apache.pdfbox.cos.COSName;
-
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.IOException;
+
+import org.apache.pdfbox.cos.COSName;
 
 /**
  * A color space with black, white, and intermediate shades of gray.
@@ -28,69 +28,59 @@ import java.io.IOException;
  * @author Ben Litchfield
  * @author John Hewson
  */
-public final class PDDeviceGray extends PDDeviceColorSpace
-{
-    /** The single instance of this class. */
-    public static final PDDeviceGray INSTANCE = new PDDeviceGray();
-    
-    private final PDColor initialColor = new PDColor(new float[] { 0 }, this);
+public final class PDDeviceGray extends PDDeviceColorSpace {
+  /** The single instance of this class. */
+  public static final PDDeviceGray INSTANCE = new PDDeviceGray();
 
-    private PDDeviceGray()
-    {
+  private final PDColor initialColor = new PDColor(new float[] { 0 }, this);
+
+  private PDDeviceGray() {
+  }
+
+  @Override
+  public String getName() {
+    return COSName.DEVICEGRAY.getName();
+  }
+
+  @Override
+  public int getNumberOfComponents() {
+    return 1;
+  }
+
+  @Override
+  public float[] getDefaultDecode(final int bitsPerComponent) {
+    return new float[] { 0, 1 };
+  }
+
+  @Override
+  public PDColor getInitialColor() {
+    return initialColor;
+  }
+
+  @Override
+  public float[] toRGB(final float[] value) {
+    return new float[] { value[0], value[0], value[0] };
+  }
+
+  @Override
+  public BufferedImage toRGBImage(final WritableRaster raster) throws IOException {
+    final int width = raster.getWidth();
+    final int height = raster.getHeight();
+
+    final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+    final int[] gray = new int[1];
+    final int[] rgb = new int[3];
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        raster.getPixel(x, y, gray);
+        rgb[0] = gray[0];
+        rgb[1] = gray[0];
+        rgb[2] = gray[0];
+        image.getRaster().setPixel(x, y, rgb);
+      }
     }
 
-    @Override
-    public String getName()
-    {
-        return COSName.DEVICEGRAY.getName();
-    }
-
-    @Override
-    public int getNumberOfComponents()
-    {
-        return 1;
-    }
-
-    @Override
-    public float[] getDefaultDecode(int bitsPerComponent)
-    {
-        return new float[] { 0, 1 };
-    }
-
-    @Override
-    public PDColor getInitialColor()
-    {
-        return initialColor;
-    }
-
-    @Override
-    public float[] toRGB(float[] value)
-    {
-        return new float[] { value[0], value[0], value[0] };
-    }
-
-    @Override
-    public BufferedImage toRGBImage(WritableRaster raster) throws IOException
-    {
-        int width = raster.getWidth();
-        int height = raster.getHeight();
-
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        int[] gray = new int[1];
-        int[] rgb = new int[3];
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                raster.getPixel(x, y, gray);
-                rgb[0] = gray[0];
-                rgb[1] = gray[0];
-                rgb[2] = gray[0];
-                image.getRaster().setPixel(x, y, rgb);
-            }
-        }
-
-        return image;
-    }
+    return image;
+  }
 }
