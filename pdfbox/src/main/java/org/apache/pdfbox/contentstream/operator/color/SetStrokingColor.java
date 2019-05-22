de4b7658733190b1aaec6e8cb607f4a95b833df2
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:35:26 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetStrokingColor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetStrokingColor.java
@@ -25,41 +25,39 @@ import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
  *
  * @author John Hewson
  */
-public class SetStrokingColor extends SetColor
-{
-    /**
-     * Returns the stroking color.
-     * @return The stroking color.
-     */
-    @Override
-    protected PDColor getColor()
-    {
-        return context.getGraphicsState().getStrokingColor();
-    }
+public class SetStrokingColor extends SetColor {
+  /**
+   * Returns the stroking color.
+   *
+   * @return The stroking color.
+   */
+  @Override
+  protected PDColor getColor() {
+    return context.getGraphicsState().getStrokingColor();
+  }
 
-    /**
-     * Sets the stroking color.
-     * @param color The new stroking color.
-     */
-    @Override
-    protected void setColor(PDColor color)
-    {
-        context.getGraphicsState().setStrokingColor(color);
-    }
+  /**
+   * Sets the stroking color.
+   *
+   * @param color The new stroking color.
+   */
+  @Override
+  protected void setColor(final PDColor color) {
+    context.getGraphicsState().setStrokingColor(color);
+  }
 
-    /**
-     * Returns the stroking color space.
-     * @return The stroking color space.
-     */
-    @Override
-    protected PDColorSpace getColorSpace()
-    {
-        return context.getGraphicsState().getStrokingColorSpace();
-    }
+  /**
+   * Returns the stroking color space.
+   *
+   * @return The stroking color space.
+   */
+  @Override
+  protected PDColorSpace getColorSpace() {
+    return context.getGraphicsState().getStrokingColorSpace();
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.STROKING_COLOR;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.STROKING_COLOR;
+  }
 }
