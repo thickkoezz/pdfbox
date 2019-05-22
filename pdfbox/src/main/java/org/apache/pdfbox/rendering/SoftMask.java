2ff8ef5c410d20106031469a1cf1165daa8b37c6
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:37:09 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/SoftMask.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/SoftMask.java
@@ -22,6 +22,7 @@ import java.awt.Paint;
 import java.awt.PaintContext;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.Transparency;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -32,188 +33,157 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.apache.pdfbox.pdmodel.common.function.PDFunction;
 import org.apache.pdfbox.pdmodel.common.function.PDFunctionTypeIdentity;
 import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
 
 /**
  * A Paint which applies a soft mask to an underlying Paint.
- * 
+ *
  * @author Petr Slaby
  * @author John Hewson
  * @author Matthias Bläsing
  * @author Tilman Hausherr
  */
-class SoftMask implements Paint
-{
+class SoftMask implements Paint {
 
-    private static final Log LOG = LogFactory.getLog(SoftMask.class);
+  private static final Log LOG = LogFactory.getLog(SoftMask.class);
 
-    private static final ColorModel ARGB_COLOR_MODEL =
-            new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB).getColorModel();
+  private static final ColorModel ARGB_COLOR_MODEL = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB)
+      .getColorModel();
 
-    private final Paint paint;
-    private final BufferedImage mask;
-    private final Rectangle2D bboxDevice;
-    private int bc = 0;
-    private final PDFunction transferFunction;
+  private final Paint paint;
+  private final BufferedImage mask;
+  private final Rectangle2D bboxDevice;
+  private int bc = 0;
+  private final PDFunction transferFunction;
 
-    /**
-     * Creates a new soft mask paint.
-     *
-     * @param paint underlying paint.
-     * @param mask soft mask
-     * @param bboxDevice bbox of the soft mask in the underlying Graphics2D device space
-     * @param backdropColor the color to be used outside the transparency group’s bounding box; if
-     * null, black will be used.
-     * @param transferFunction the transfer function, may be null.
-     */
-    SoftMask(Paint paint, BufferedImage mask, Rectangle2D bboxDevice, PDColor backdropColor, PDFunction transferFunction)
-    {
-        this.paint = paint;
-        this.mask = mask;
-        this.bboxDevice = bboxDevice;
-        if (transferFunction instanceof PDFunctionTypeIdentity)
-        {
-            this.transferFunction = null;
-        }
-        else
-        {
-            this.transferFunction = transferFunction;
-        }
-        if (backdropColor != null)
-        {
-            try
-            {
-                Color color = new Color(backdropColor.toRGB());
-                // http://stackoverflow.com/a/25463098/535646
-                bc = (299 * color.getRed() + 587 * color.getGreen() + 114 * color.getBlue()) / 1000;
-            }
-            catch (IOException ex)
-            {
-                // keep default
-                LOG.debug("Couldn't convert backdropColor to RGB - keeping default", ex);
-            }
-        }
+  /**
+   * Creates a new soft mask paint.
+   *
+   * @param paint            underlying paint.
+   * @param mask             soft mask
+   * @param bboxDevice       bbox of the soft mask in the underlying Graphics2D
+   *                         device space
+   * @param backdropColor    the color to be used outside the transparency group’s
+   *                         bounding box; if null, black will be used.
+   * @param transferFunction the transfer function, may be null.
+   */
+  SoftMask(final Paint paint, final BufferedImage mask, final Rectangle2D bboxDevice, final PDColor backdropColor,
+      final PDFunction transferFunction) {
+    this.paint = paint;
+    this.mask = mask;
+    this.bboxDevice = bboxDevice;
+    if (transferFunction instanceof PDFunctionTypeIdentity) {
+      this.transferFunction = null;
+    } else {
+      this.transferFunction = transferFunction;
+    }
+    if (backdropColor != null) {
+      try {
+        final Color color = new Color(backdropColor.toRGB());
+        // http://stackoverflow.com/a/25463098/535646
+        bc = (299 * color.getRed() + 587 * color.getGreen() + 114 * color.getBlue()) / 1000;
+      } catch (final IOException ex) {
+        // keep default
+        SoftMask.LOG.debug("Couldn't convert backdropColor to RGB - keeping default", ex);
+      }
+    }
+  }
+
+  @Override
+  public PaintContext createContext(final ColorModel cm, final Rectangle deviceBounds, final Rectangle2D userBounds,
+      final AffineTransform xform, final RenderingHints hints) {
+    final PaintContext ctx = paint.createContext(cm, deviceBounds, userBounds, xform, hints);
+    return new SoftPaintContext(ctx);
+  }
+
+  @Override
+  public int getTransparency() {
+    return Transparency.TRANSLUCENT;
+  }
+
+  private class SoftPaintContext implements PaintContext {
+    private final PaintContext context;
+
+    SoftPaintContext(final PaintContext context) {
+      this.context = context;
     }
 
     @Override
-    public PaintContext createContext(ColorModel cm, Rectangle deviceBounds,
-                                      Rectangle2D userBounds, AffineTransform xform,
-                                      RenderingHints hints)
-    {
-        PaintContext ctx = paint.createContext(cm, deviceBounds, userBounds, xform, hints);
-        return new SoftPaintContext(ctx);
+    public ColorModel getColorModel() {
+      return SoftMask.ARGB_COLOR_MODEL;
     }
 
     @Override
-    public int getTransparency()
-    {
-        return TRANSLUCENT;
-    }
+    public Raster getRaster(int x1, int y1, final int w, final int h) {
+      final Raster raster = context.getRaster(x1, y1, w, h);
+      final ColorModel rasterCM = context.getColorModel();
+      float[] input = null;
+      Float[] map = null;
 
-    private class SoftPaintContext implements PaintContext
-    {
-        private final PaintContext context;
+      if (transferFunction != null) {
+        map = new Float[256];
+        input = new float[1];
+      }
 
-        SoftPaintContext(PaintContext context)
-        {
-            this.context = context;
-        }
+      // buffer
+      final WritableRaster output = getColorModel().createCompatibleWritableRaster(w, h);
 
-        @Override
-        public ColorModel getColorModel()
-        {
-            return ARGB_COLOR_MODEL;
-        }
+      // the soft mask has its own bbox
+      x1 = x1 - (int) bboxDevice.getX();
+      y1 = y1 - (int) bboxDevice.getY();
 
-        @Override
-        public Raster getRaster(int x1, int y1, int w, int h)
-        {
-            Raster raster = context.getRaster(x1, y1, w, h);
-            ColorModel rasterCM = context.getColorModel();
-            float[] input = null;
-            Float[] map = null;
+      final int[] gray = new int[4];
+      Object pixelInput = null;
+      final int[] pixelOutput = new int[4];
+      for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+          pixelInput = raster.getDataElements(x, y, pixelInput);
 
-            if (transferFunction != null)
-            {
-                map = new Float[256];
-                input = new float[1];
-            }
+          pixelOutput[0] = rasterCM.getRed(pixelInput);
+          pixelOutput[1] = rasterCM.getGreen(pixelInput);
+          pixelOutput[2] = rasterCM.getBlue(pixelInput);
+          pixelOutput[3] = rasterCM.getAlpha(pixelInput);
 
-            // buffer
-            WritableRaster output = getColorModel().createCompatibleWritableRaster(w, h);
-
-            // the soft mask has its own bbox
-            x1 = x1 - (int)bboxDevice.getX();
-            y1 = y1 - (int)bboxDevice.getY();
-
-            int[] gray = new int[4];
-            Object pixelInput = null;
-            int[] pixelOutput = new int[4];
-            for (int y = 0; y < h; y++)
-            {
-                for (int x = 0; x < w; x++)
-                {
-                    pixelInput = raster.getDataElements(x, y, pixelInput);
-
-                    pixelOutput[0] = rasterCM.getRed(pixelInput);
-                    pixelOutput[1] = rasterCM.getGreen(pixelInput);
-                    pixelOutput[2] = rasterCM.getBlue(pixelInput);
-                    pixelOutput[3] = rasterCM.getAlpha(pixelInput);
-                    
-                    // get the alpha value from the gray mask, if within mask bounds
-                    gray[0] = 0;
-                    if (x1 + x >= 0 && y1 + y >= 0 && x1 + x < mask.getWidth() && y1 + y < mask.getHeight())
-                    {
-                        mask.getRaster().getPixel(x1 + x, y1 + y, gray);
-                        int g = gray[0];
-                        if (transferFunction != null)
-                        {
-                            // apply transfer function
-                            try
-                            {
-                                if (map[g] != null)
-                                {
-                                    // was calculated before
-                                    pixelOutput[3] = Math.round(pixelOutput[3] * map[g]);
-                                }
-                                else
-                                {
-                                    // calculate and store in map
-                                    input[0] = g / 255f;
-                                    float f = transferFunction.eval(input)[0];
-                                    map[g] = f;
-                                    pixelOutput[3] = Math.round(pixelOutput[3] * f);
-                                }
-                            }
-                            catch (IOException ex)
-                            {
-                                // ignore exception, treat as outside
-                                LOG.debug("Couldn't apply transferFunction - treating as outside", ex);
-                                pixelOutput[3] = Math.round(pixelOutput[3] * (bc / 255f));
-                            }
-                        }
-                        else
-                        {
-                            pixelOutput[3] = Math.round(pixelOutput[3] * (g / 255f));
-                        }
-                    }
-                    else
-                    {
-                        pixelOutput[3] = Math.round(pixelOutput[3] * (bc / 255f));
-                    }
-                    output.setPixel(x, y, pixelOutput);
+          // get the alpha value from the gray mask, if within mask bounds
+          gray[0] = 0;
+          if (x1 + x >= 0 && y1 + y >= 0 && x1 + x < mask.getWidth() && y1 + y < mask.getHeight()) {
+            mask.getRaster().getPixel(x1 + x, y1 + y, gray);
+            final int g = gray[0];
+            if (transferFunction != null) {
+              // apply transfer function
+              try {
+                if (map[g] != null) {
+                  // was calculated before
+                  pixelOutput[3] = Math.round(pixelOutput[3] * map[g]);
+                } else {
+                  // calculate and store in map
+                  input[0] = g / 255f;
+                  final float f = transferFunction.eval(input)[0];
+                  map[g] = f;
+                  pixelOutput[3] = Math.round(pixelOutput[3] * f);
                 }
+              } catch (final IOException ex) {
+                // ignore exception, treat as outside
+                SoftMask.LOG.debug("Couldn't apply transferFunction - treating as outside", ex);
+                pixelOutput[3] = Math.round(pixelOutput[3] * (bc / 255f));
+              }
+            } else {
+              pixelOutput[3] = Math.round(pixelOutput[3] * (g / 255f));
             }
-
-            return output;
+          } else {
+            pixelOutput[3] = Math.round(pixelOutput[3] * (bc / 255f));
+          }
+          output.setPixel(x, y, pixelOutput);
         }
+      }
 
-        @Override
-        public void dispose()
-        {
-        }
+      return output;
     }
+
+    @Override
+    public void dispose() {
+    }
+  }
 }
