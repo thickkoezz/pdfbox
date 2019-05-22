806e57ec9271213d3b60711acf4a98c6b06a3f4b
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:46:13 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDListBox.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDListBox.java
@@ -22,67 +22,58 @@ import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 
 /**
- * A scrollable list box. Contains several text items, one or more of which shall be selected as the
- * field value.
- * 
+ * A scrollable list box. Contains several text items, one or more of which
+ * shall be selected as the field value.
+ *
  * @author John Hewson
  */
-public final class PDListBox extends PDChoice
-{
-    /**
-     * @see PDField#PDField(PDAcroForm)
-     *
-     * @param acroForm The acroform.
-     */
-    public PDListBox(PDAcroForm acroForm)
-    {
-        super(acroForm);
-    }
-    
-    /**
-     * Constructor.
-     * 
-     * @param acroForm The form that this field is part of.
-     * @param field the PDF object to represent as a field.
-     * @param parent the parent node of the node
-     */
-    PDListBox(PDAcroForm acroForm, COSDictionary field, PDNonTerminalField parent)
-    {
-        super(acroForm, field, parent);
-    }
+public final class PDListBox extends PDChoice {
+  /**
+   * @see PDField#PDField(PDAcroForm)
+   *
+   * @param acroForm The acroform.
+   */
+  public PDListBox(final PDAcroForm acroForm) {
+    super(acroForm);
+  }
 
-    /**
-     * This will get the top index "TI" value.
-     *
-     * @return the top index, default value 0.
-     */
-    public int getTopIndex()
-    {
-        return getCOSObject().getInt(COSName.TI, 0);
-    }
+  /**
+   * Constructor.
+   *
+   * @param acroForm The form that this field is part of.
+   * @param field    the PDF object to represent as a field.
+   * @param parent   the parent node of the node
+   */
+  PDListBox(final PDAcroForm acroForm, final COSDictionary field, final PDNonTerminalField parent) {
+    super(acroForm, field, parent);
+  }
 
-    /**
-     * This will set top index "TI" value.
-     *
-     * @param topIndex the value for the top index, null will remove the value.
-     */
-    public void setTopIndex(Integer topIndex)
-    {
-        if (topIndex != null)
-        {
-            getCOSObject().setInt(COSName.TI, topIndex);
-        }
-        else
-        {
-            getCOSObject().removeItem(COSName.TI);
-        }
+  /**
+   * This will get the top index "TI" value.
+   *
+   * @return the top index, default value 0.
+   */
+  public int getTopIndex() {
+    return getCOSObject().getInt(COSName.TI, 0);
+  }
+
+  /**
+   * This will set top index "TI" value.
+   *
+   * @param topIndex the value for the top index, null will remove the value.
+   */
+  public void setTopIndex(final Integer topIndex) {
+    if (topIndex != null) {
+      getCOSObject().setInt(COSName.TI, topIndex);
+    } else {
+      getCOSObject().removeItem(COSName.TI);
     }
-    
-    @Override
-    void constructAppearances() throws IOException
-    {
-        AppearanceGeneratorHelper apHelper;
-        apHelper = new AppearanceGeneratorHelper(this);
-        apHelper.setAppearanceValue("");
-    }
+  }
+
+  @Override
+  void constructAppearances() throws IOException {
+    AppearanceGeneratorHelper apHelper;
+    apHelper = new AppearanceGeneratorHelper(this);
+    apHelper.setAppearanceValue("");
+  }
 }
