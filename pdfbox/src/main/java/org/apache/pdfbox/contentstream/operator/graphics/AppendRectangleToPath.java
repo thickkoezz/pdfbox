a10ed60bdf6ae82489271528f31f8d766f11d237
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:22:05 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/AppendRectangleToPath.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/AppendRectangleToPath.java
@@ -19,54 +19,47 @@ package org.apache.pdfbox.contentstream.operator.graphics;
 import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.util.List;
-import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
+import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSNumber;
 
 /**
  * re Appends a rectangle to the path.
  *
  * @author Ben Litchfield
  */
-public final class AppendRectangleToPath extends GraphicsOperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> operands) throws IOException
-    {
-        if (operands.size() < 4)
-        {
-            throw new MissingOperandException(operator, operands);
-        }
-        if (!checkArrayTypesClass(operands, COSNumber.class))
-        {
-            return;
-        }
-        COSNumber x = (COSNumber) operands.get(0);
-        COSNumber y = (COSNumber) operands.get(1);
-        COSNumber w = (COSNumber) operands.get(2);
-        COSNumber h = (COSNumber) operands.get(3);
+public final class AppendRectangleToPath extends GraphicsOperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> operands) throws IOException {
+    if (operands.size() < 4)
+      throw new MissingOperandException(operator, operands);
+    if (!checkArrayTypesClass(operands, COSNumber.class))
+      return;
+    final COSNumber x = (COSNumber) operands.get(0);
+    final COSNumber y = (COSNumber) operands.get(1);
+    final COSNumber w = (COSNumber) operands.get(2);
+    final COSNumber h = (COSNumber) operands.get(3);
 
-        float x1 = x.floatValue();
-        float y1 = y.floatValue();
+    final float x1 = x.floatValue();
+    final float y1 = y.floatValue();
 
-        // create a pair of coordinates for the transformation
-        float x2 = w.floatValue() + x1;
-        float y2 = h.floatValue() + y1;
+    // create a pair of coordinates for the transformation
+    final float x2 = w.floatValue() + x1;
+    final float y2 = h.floatValue() + y1;
 
-        Point2D p0 = context.transformedPoint(x1, y1);
-        Point2D p1 = context.transformedPoint(x2, y1);
-        Point2D p2 = context.transformedPoint(x2, y2);
-        Point2D p3 = context.transformedPoint(x1, y2);
+    final Point2D p0 = context.transformedPoint(x1, y1);
+    final Point2D p1 = context.transformedPoint(x2, y1);
+    final Point2D p2 = context.transformedPoint(x2, y2);
+    final Point2D p3 = context.transformedPoint(x1, y2);
 
-        context.appendRectangle(p0, p1, p2, p3);
-    }
+    context.appendRectangle(p0, p1, p2, p3);
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.APPEND_RECT;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.APPEND_RECT;
+  }
 }
