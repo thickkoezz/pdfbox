54552254f00b109f354427df22c0d4a63d0eab35
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:42:31 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/util/TestQuickSort.java
+++ pdfbox/src/test/java/org/apache/pdfbox/util/TestQuickSort.java
@@ -19,80 +19,76 @@ package org.apache.pdfbox.util;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+
 import junit.framework.TestCase;
 
 /**
  *
  * @author Uwe Pachler
  */
-public class TestQuickSort extends TestCase
-{
+public class TestQuickSort extends TestCase {
 
-    <T extends Comparable<T>> void doTest(T[] input, T[] expected)
+  <T extends Comparable<T>> void doTest(final T[] input, final T[] expected) {
+    final List<T> list = Arrays.asList(input);
+    QuickSort.sort(list);
+
+    final boolean equal = Arrays.equals(list.toArray(new Object[input.length]), expected);
+
+    TestCase.assertTrue(equal);
+  }
+
+  /**
+   * Test for different cases.
+   */
+  public void testSort() {
+
     {
-        List<T> list = Arrays.asList(input);
-        QuickSort.sort(list);
-
-        boolean equal = Arrays.equals(list.toArray(new Object[input.length]), expected);
-
-        assertTrue(equal);
+      final Integer[] input = new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+      final Integer[] expected = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+      doTest(input, expected);
     }
 
-    /**
-     * Test for different cases.
-     */
-    public void testSort()
     {
-
-        {
-            Integer[] input = new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-            Integer[] expected = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            doTest(input, expected);
-        }
-
-        {
-            Integer[] input = new Integer[] { 4, 3, 2, 1, 9, 8, 7, 6, 5 };
-            Integer[] expected = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            doTest(input, expected);
-        }
-
-        {
-            Integer[] input = new Integer[] {};
-            Integer[] expected = new Integer[] {};
-            doTest(input, expected);
-        }
-
-        {
-            Integer[] input = new Integer[] { 5 };
-            Integer[] expected = new Integer[] { 5 };
-            doTest(input, expected);
-        }
-
-        {
-            Integer[] input = new Integer[] { 5, 6 };
-            Integer[] expected = new Integer[] { 5, 6 };
-            doTest(input, expected);
-        }
-
-        {
-            Integer[] input = new Integer[] { 6, 5 };
-            Integer[] expected = new Integer[] { 5, 6 };
-            doTest(input, expected);
-        }
-
-        Random rnd = new Random(12345);
-        for (int cnt = 0; cnt < 100; ++cnt)
-        {
-            int len = rnd.nextInt(20000) + 2;
-            Integer[] input = new Integer[len];
-            Integer[] expected = new Integer[len];
-            for (int i = 0; i < len; ++i)
-            {
-                // choose values so that there are some duplicates
-                expected[i] = input[i] = rnd.nextInt(rnd.nextInt(100)+1);
-            }
-            Arrays.sort(expected);
-            doTest(input, expected);
-        }
+      final Integer[] input = new Integer[] { 4, 3, 2, 1, 9, 8, 7, 6, 5 };
+      final Integer[] expected = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+      doTest(input, expected);
     }
+
+    {
+      final Integer[] input = new Integer[] {};
+      final Integer[] expected = new Integer[] {};
+      doTest(input, expected);
+    }
+
+    {
+      final Integer[] input = new Integer[] { 5 };
+      final Integer[] expected = new Integer[] { 5 };
+      doTest(input, expected);
+    }
+
+    {
+      final Integer[] input = new Integer[] { 5, 6 };
+      final Integer[] expected = new Integer[] { 5, 6 };
+      doTest(input, expected);
+    }
+
+    {
+      final Integer[] input = new Integer[] { 6, 5 };
+      final Integer[] expected = new Integer[] { 5, 6 };
+      doTest(input, expected);
+    }
+
+    final Random rnd = new Random(12345);
+    for (int cnt = 0; cnt < 100; ++cnt) {
+      final int len = rnd.nextInt(20000) + 2;
+      final Integer[] input = new Integer[len];
+      final Integer[] expected = new Integer[len];
+      for (int i = 0; i < len; ++i) {
+        // choose values so that there are some duplicates
+        expected[i] = input[i] = rnd.nextInt(rnd.nextInt(100) + 1);
+      }
+      Arrays.sort(expected);
+      doTest(input, expected);
+    }
+  }
 }
