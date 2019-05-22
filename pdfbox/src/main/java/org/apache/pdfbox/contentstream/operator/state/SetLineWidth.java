63d7aaa1ec8960b2c324e422501a6ee5730da1c1
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:34:43 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetLineWidth.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetLineWidth.java
@@ -16,38 +16,32 @@
  */
 package org.apache.pdfbox.contentstream.operator.state;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.pdfbox.contentstream.operator.MissingOperandException;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
-
-import java.io.IOException;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSNumber;
 
 /**
  * w: Set line width.
  *
  * @author Ben Litchfield
  */
-public class SetLineWidth extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        if (arguments.isEmpty())
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
-        COSNumber width = (COSNumber) arguments.get(0);
-        context.getGraphicsState().setLineWidth(width.floatValue());
-    }
+public class SetLineWidth extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    if (arguments.isEmpty())
+      throw new MissingOperandException(operator, arguments);
+    final COSNumber width = (COSNumber) arguments.get(0);
+    context.getGraphicsState().setLineWidth(width.floatValue());
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_LINE_WIDTH;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_LINE_WIDTH;
+  }
 }
