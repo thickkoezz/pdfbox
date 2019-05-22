df1234065f03ed43764eb0b6b31847222663dc77
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:52:08 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/documentinterchange/markedcontent/PDPropertyList.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/documentinterchange/markedcontent/PDPropertyList.java
@@ -23,53 +23,43 @@ import org.apache.pdfbox.pdmodel.graphics.optionalcontent.PDOptionalContentGroup
 import org.apache.pdfbox.pdmodel.graphics.optionalcontent.PDOptionalContentMembershipDictionary;
 
 /**
- * A property list is a dictionary containing private information meaningful to the conforming
- * writer creating the marked content.
+ * A property list is a dictionary containing private information meaningful to
+ * the conforming writer creating the marked content.
  */
-public class PDPropertyList implements COSObjectable
-{
-    protected final COSDictionary dict;
+public class PDPropertyList implements COSObjectable {
+  protected final COSDictionary dict;
 
-    /**
-     * Creates a property list from the given dictionary.
-     * @param dict COS dictionary
-     */
-    public static PDPropertyList create(COSDictionary dict)
-    {
-        if (COSName.OCG.equals(dict.getItem(COSName.TYPE)))
-        {
-            return new PDOptionalContentGroup(dict);
-        }
-        else if (COSName.OCMD.equals(dict.getItem(COSName.TYPE)))
-        {
-            return new PDOptionalContentMembershipDictionary(dict);
-        }
-        else
-        {
-            // todo: more types
-            return new PDPropertyList(dict);
-        }
-    }
+  /**
+   * Creates a property list from the given dictionary.
+   *
+   * @param dict COS dictionary
+   */
+  public static PDPropertyList create(final COSDictionary dict) {
+    if (COSName.OCG.equals(dict.getItem(COSName.TYPE)))
+      return new PDOptionalContentGroup(dict);
+    else if (COSName.OCMD.equals(dict.getItem(COSName.TYPE)))
+      return new PDOptionalContentMembershipDictionary(dict);
+    else
+      // todo: more types
+      return new PDPropertyList(dict);
+  }
 
-    /**
-     * Constructor for subclasses.
-     */
-    protected PDPropertyList()
-    {
-        this.dict = new COSDictionary();
-    }
+  /**
+   * Constructor for subclasses.
+   */
+  protected PDPropertyList() {
+    dict = new COSDictionary();
+  }
 
-    /**
-     * Constructor for subclasses.
-     */
-    protected PDPropertyList(COSDictionary dict)
-    {
-        this.dict = dict;
-    }
+  /**
+   * Constructor for subclasses.
+   */
+  protected PDPropertyList(final COSDictionary dict) {
+    this.dict = dict;
+  }
 
-    @Override
-    public COSDictionary getCOSObject()
-    {
-        return dict;
-    }
+  @Override
+  public COSDictionary getCOSObject() {
+    return dict;
+  }
 }
