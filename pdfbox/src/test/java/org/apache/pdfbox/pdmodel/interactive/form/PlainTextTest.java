eec5728a7b01854a0c617bf78d72ba9d485e6877
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:04:38 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/form/PlainTextTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/form/PlainTextTest.java
@@ -16,57 +16,50 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.form;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Test if a sequence of linebreak/paragraph characters produces the same
- * number of paragraphs as Adobe Acrobat produces when setting the value
- * via JavaScript.
- * 
+ * Test if a sequence of linebreak/paragraph characters produces the same number
+ * of paragraphs as Adobe Acrobat produces when setting the value via
+ * JavaScript.
+ *
  */
 public class PlainTextTest {
-    @Test
-    public void characterCR()
-    {
-    	PlainText text = new PlainText("CR\rCR");
-    	assertEquals(2,text.getParagraphs().size());
-    }
+  @Test
+  public void characterCR() {
+    final PlainText text = new PlainText("CR\rCR");
+    Assert.assertEquals(2, text.getParagraphs().size());
+  }
 
-    @Test
-    public void characterLF()
-    {
-    	PlainText text = new PlainText("LF\nLF");
-    	assertEquals(2,text.getParagraphs().size());
-    }
-    
-    @Test
-    public void characterCRLF()
-    {
-    	PlainText text = new PlainText("CRLF\r\nCRLF");
-    	assertEquals(2,text.getParagraphs().size());
-    }
+  @Test
+  public void characterLF() {
+    final PlainText text = new PlainText("LF\nLF");
+    Assert.assertEquals(2, text.getParagraphs().size());
+  }
 
-    @Test
-    public void characterLFCR()
-    {
-    	PlainText text = new PlainText("LFCR\n\rLFCR");
-    	assertEquals(3,text.getParagraphs().size());
-    }
-    
-    @Test
-    public void characterUnicodeLinebreak()
-    {
-    	PlainText text = new PlainText("linebreak\u2028linebreak");
-    	assertEquals(2,text.getParagraphs().size());
-    }
-    
-    @Test
-    public void characterUnicodeParagraphbreak()
-    {
-    	PlainText text = new PlainText("paragraphbreak\u2029paragraphbreak");
-    	assertEquals(2,text.getParagraphs().size());
-    }
+  @Test
+  public void characterCRLF() {
+    final PlainText text = new PlainText("CRLF\r\nCRLF");
+    Assert.assertEquals(2, text.getParagraphs().size());
+  }
+
+  @Test
+  public void characterLFCR() {
+    final PlainText text = new PlainText("LFCR\n\rLFCR");
+    Assert.assertEquals(3, text.getParagraphs().size());
+  }
+
+  @Test
+  public void characterUnicodeLinebreak() {
+    final PlainText text = new PlainText("linebreak\u2028linebreak");
+    Assert.assertEquals(2, text.getParagraphs().size());
+  }
+
+  @Test
+  public void characterUnicodeParagraphbreak() {
+    final PlainText text = new PlainText("paragraphbreak\u2029paragraphbreak");
+    Assert.assertEquals(2, text.getParagraphs().size());
+  }
 
 }
