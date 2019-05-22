235d9fe0c41eb050e3f0df81f1edb04c873ec118
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:40:21 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/DefaultResourceCache.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/DefaultResourceCache.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.pdfbox.cos.COSObject;
 import org.apache.pdfbox.pdmodel.documentinterchange.markedcontent.PDPropertyList;
 import org.apache.pdfbox.pdmodel.font.PDFont;
@@ -31,150 +32,114 @@ import org.apache.pdfbox.pdmodel.graphics.shading.PDShading;
 import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
 
 /**
- * A resource cached based on SoftReference, retains resources until memory pressure causes them
- * to be garbage collected.
+ * A resource cached based on SoftReference, retains resources until memory
+ * pressure causes them to be garbage collected.
  *
  * @author John Hewson
  */
-public class DefaultResourceCache implements ResourceCache
-{
-    private final Map<COSObject, SoftReference<PDFont>> fonts =
-            new HashMap<>();
-    
-    private final Map<COSObject, SoftReference<PDColorSpace>> colorSpaces =
-            new HashMap<>();
+public class DefaultResourceCache implements ResourceCache {
+  private final Map<COSObject, SoftReference<PDFont>> fonts = new HashMap<>();
 
-    private final Map<COSObject, SoftReference<PDXObject>> xobjects =
-            new HashMap<>();
+  private final Map<COSObject, SoftReference<PDColorSpace>> colorSpaces = new HashMap<>();
 
-    private final Map<COSObject, SoftReference<PDExtendedGraphicsState>> extGStates =
-            new HashMap<>();
+  private final Map<COSObject, SoftReference<PDXObject>> xobjects = new HashMap<>();
 
-    private final Map<COSObject, SoftReference<PDShading>> shadings =
-            new HashMap<>();
+  private final Map<COSObject, SoftReference<PDExtendedGraphicsState>> extGStates = new HashMap<>();
 
-    private final Map<COSObject, SoftReference<PDAbstractPattern>> patterns =
-            new HashMap<>();
+  private final Map<COSObject, SoftReference<PDShading>> shadings = new HashMap<>();
 
-    private final Map<COSObject, SoftReference<PDPropertyList>> properties =
-            new HashMap<>();
+  private final Map<COSObject, SoftReference<PDAbstractPattern>> patterns = new HashMap<>();
 
-    @Override
-    public PDFont getFont(COSObject indirect) throws IOException
-    {
-        SoftReference<PDFont> font = fonts.get(indirect);
-        if (font != null)
-        {
-            return font.get();
-        }
-        return null;
-    }
+  private final Map<COSObject, SoftReference<PDPropertyList>> properties = new HashMap<>();
 
-    @Override
-    public void put(COSObject indirect, PDFont font) throws IOException
-    {
-        fonts.put(indirect, new SoftReference<>(font));
-    }
+  @Override
+  public PDFont getFont(final COSObject indirect) throws IOException {
+    final SoftReference<PDFont> font = fonts.get(indirect);
+    if (font != null)
+      return font.get();
+    return null;
+  }
 
-    @Override
-    public PDColorSpace getColorSpace(COSObject indirect) throws IOException
-    {
-        SoftReference<PDColorSpace> colorSpace = colorSpaces.get(indirect);
-        if (colorSpace != null)
-        {
-            return colorSpace.get();
-        }
-        return null;
-    }
+  @Override
+  public void put(final COSObject indirect, final PDFont font) throws IOException {
+    fonts.put(indirect, new SoftReference<>(font));
+  }
 
-    @Override
-    public void put(COSObject indirect, PDColorSpace colorSpace) throws IOException
-    {
-        colorSpaces.put(indirect, new SoftReference<>(colorSpace));
-    }
+  @Override
+  public PDColorSpace getColorSpace(final COSObject indirect) throws IOException {
+    final SoftReference<PDColorSpace> colorSpace = colorSpaces.get(indirect);
+    if (colorSpace != null)
+      return colorSpace.get();
+    return null;
+  }
 
-    @Override
-    public PDExtendedGraphicsState getExtGState(COSObject indirect)
-    {
-        SoftReference<PDExtendedGraphicsState> extGState = extGStates.get(indirect);
-        if (extGState != null)
-        {
-            return extGState.get();
-        }
-        return null;
-    }
+  @Override
+  public void put(final COSObject indirect, final PDColorSpace colorSpace) throws IOException {
+    colorSpaces.put(indirect, new SoftReference<>(colorSpace));
+  }
 
-    @Override
-    public void put(COSObject indirect, PDExtendedGraphicsState extGState)
-    {
-        extGStates.put(indirect, new SoftReference<>(extGState));
-    }
+  @Override
+  public PDExtendedGraphicsState getExtGState(final COSObject indirect) {
+    final SoftReference<PDExtendedGraphicsState> extGState = extGStates.get(indirect);
+    if (extGState != null)
+      return extGState.get();
+    return null;
+  }
 
-    @Override
-    public PDShading getShading(COSObject indirect) throws IOException
-    {
-        SoftReference<PDShading> shading = shadings.get(indirect);
-        if (shading != null)
-        {
-            return shading.get();
-        }
-        return null;
-    }
+  @Override
+  public void put(final COSObject indirect, final PDExtendedGraphicsState extGState) {
+    extGStates.put(indirect, new SoftReference<>(extGState));
+  }
 
-    @Override
-    public void put(COSObject indirect, PDShading shading) throws IOException
-    {
-        shadings.put(indirect, new SoftReference<>(shading));
-    }
+  @Override
+  public PDShading getShading(final COSObject indirect) throws IOException {
+    final SoftReference<PDShading> shading = shadings.get(indirect);
+    if (shading != null)
+      return shading.get();
+    return null;
+  }
 
-    @Override
-    public PDAbstractPattern getPattern(COSObject indirect) throws IOException
-    {
-        SoftReference<PDAbstractPattern> pattern = patterns.get(indirect);
-        if (pattern != null)
-        {
-            return pattern.get();
-        }
-        return null;
-    }
+  @Override
+  public void put(final COSObject indirect, final PDShading shading) throws IOException {
+    shadings.put(indirect, new SoftReference<>(shading));
+  }
 
-    @Override
-    public void put(COSObject indirect, PDAbstractPattern pattern) throws IOException
-    {
-        patterns.put(indirect, new SoftReference<>(pattern));
-    }
-    
-    @Override
-    public PDPropertyList getProperties(COSObject indirect)
-    {
-        SoftReference<PDPropertyList> propertyList = properties.get(indirect);
-        if (propertyList != null)
-        {
-            return propertyList.get();
-        }
-        return null;
-    }
+  @Override
+  public PDAbstractPattern getPattern(final COSObject indirect) throws IOException {
+    final SoftReference<PDAbstractPattern> pattern = patterns.get(indirect);
+    if (pattern != null)
+      return pattern.get();
+    return null;
+  }
 
-    @Override
-    public void put(COSObject indirect, PDPropertyList propertyList)
-    {
-        properties.put(indirect, new SoftReference<>(propertyList));
-    }
+  @Override
+  public void put(final COSObject indirect, final PDAbstractPattern pattern) throws IOException {
+    patterns.put(indirect, new SoftReference<>(pattern));
+  }
 
-    @Override
-    public PDXObject getXObject(COSObject indirect) throws IOException
-    {
-        SoftReference<PDXObject> xobject = xobjects.get(indirect);
-        if (xobject != null)
-        {
-            return xobject.get();
-        }
-        return null;
-    }
+  @Override
+  public PDPropertyList getProperties(final COSObject indirect) {
+    final SoftReference<PDPropertyList> propertyList = properties.get(indirect);
+    if (propertyList != null)
+      return propertyList.get();
+    return null;
+  }
 
-    @Override
-    public void put(COSObject indirect, PDXObject xobject) throws IOException
-    {
-        xobjects.put(indirect, new SoftReference<>(xobject));
-    }
+  @Override
+  public void put(final COSObject indirect, final PDPropertyList propertyList) {
+    properties.put(indirect, new SoftReference<>(propertyList));
+  }
+
+  @Override
+  public PDXObject getXObject(final COSObject indirect) throws IOException {
+    final SoftReference<PDXObject> xobject = xobjects.get(indirect);
+    if (xobject != null)
+      return xobject.get();
+    return null;
+  }
+
+  @Override
+  public void put(final COSObject indirect, final PDXObject xobject) throws IOException {
+    xobjects.put(indirect, new SoftReference<>(xobject));
+  }
 }
