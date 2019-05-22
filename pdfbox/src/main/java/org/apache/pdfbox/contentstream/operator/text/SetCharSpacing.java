288b602477c357deb095d4110bf9265ed47092e9
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:08:22 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetCharSpacing.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetCharSpacing.java
@@ -20,41 +20,35 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.pdfbox.contentstream.operator.MissingOperandException;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSNumber;
 
 /**
  * Tc: Set character spacing.
  *
  * @author Laurent Huault
  */
-public class SetCharSpacing extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        if (arguments.isEmpty())
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
+public class SetCharSpacing extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    if (arguments.isEmpty())
+      throw new MissingOperandException(operator, arguments);
 
-        // there are some documents which are incorrectly structured, and have
-        // a wrong number of arguments to this, so we will assume the last argument
-        // in the list
-        Object charSpacing = arguments.get(arguments.size()-1);
-        if (charSpacing instanceof COSNumber)
-        {
-            COSNumber characterSpacing = (COSNumber)charSpacing;
-            context.getGraphicsState().getTextState().setCharacterSpacing(characterSpacing.floatValue());
-        }
+    // there are some documents which are incorrectly structured, and have
+    // a wrong number of arguments to this, so we will assume the last argument
+    // in the list
+    final Object charSpacing = arguments.get(arguments.size() - 1);
+    if (charSpacing instanceof COSNumber) {
+      final COSNumber characterSpacing = (COSNumber) charSpacing;
+      context.getGraphicsState().getTextState().setCharacterSpacing(characterSpacing.floatValue());
     }
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_CHAR_SPACING;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_CHAR_SPACING;
+  }
 }
