cf02720574b5bce61e4614f091a54bffca713dc3
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:04:29 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/InputStreamSource.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/InputStreamSource.java
@@ -24,130 +24,104 @@ import java.io.PushbackInputStream;
 /**
  * A SequentialSource backed by an InputStream.
  */
-final class InputStreamSource implements SequentialSource
-{
-    private final PushbackInputStream input;
-    private int position;
+final class InputStreamSource implements SequentialSource {
+  private final PushbackInputStream input;
+  private int position;
 
-    /**
-     * Constructor.
-     * 
-     * @param input The input stream to wrap.
-     */
-    InputStreamSource(InputStream input)
-    {
-        this.input = new PushbackInputStream(input, 32767); // maximum length of a PDF string
-        this.position = 0;
-    }
+  /**
+   * Constructor.
+   *
+   * @param input The input stream to wrap.
+   */
+  InputStreamSource(final InputStream input) {
+    this.input = new PushbackInputStream(input, 32767); // maximum length of a PDF string
+    position = 0;
+  }
 
-    @Override
-    public int read() throws IOException
-    {
-        int b = input.read();
-        position++;
-        return b;
-    }
+  @Override
+  public int read() throws IOException {
+    final int b = input.read();
+    position++;
+    return b;
+  }
 
-    @Override
-    public int read(byte[] b) throws IOException
-    {
-        int n = input.read(b);
-        if (n > 0)
-        {
-            position += n;
-            return n;
-        }
-        else
-        {
-            return -1;
-        }
-    }
+  @Override
+  public int read(final byte[] b) throws IOException {
+    final int n = input.read(b);
+    if (n > 0) {
+      position += n;
+      return n;
+    } else
+      return -1;
+  }
 
-    @Override
-    public int read(byte[] b, int offset, int length) throws IOException
-    {
-        int n = input.read(b, offset, length);
-        if (n > 0)
-        {
-            position += n;
-            return n;
-        }
-        else
-        {
-            return -1;
-        }
-    }
+  @Override
+  public int read(final byte[] b, final int offset, final int length) throws IOException {
+    final int n = input.read(b, offset, length);
+    if (n > 0) {
+      position += n;
+      return n;
+    } else
+      return -1;
+  }
 
-    @Override
-    public long getPosition() throws IOException
-    {
-        return position;
-    }
+  @Override
+  public long getPosition() throws IOException {
+    return position;
+  }
 
-    @Override
-    public int peek() throws IOException
-    {
-        int b = input.read();
-        if (b != -1)
-        {
-            input.unread(b);
-        }
-        return b;
+  @Override
+  public int peek() throws IOException {
+    final int b = input.read();
+    if (b != -1) {
+      input.unread(b);
     }
+    return b;
+  }
 
-    @Override
-    public void unread(int b) throws IOException
-    {
-        input.unread(b);
-        position--;
-    }
+  @Override
+  public void unread(final int b) throws IOException {
+    input.unread(b);
+    position--;
+  }
 
-    @Override
-    public void unread(byte[] bytes) throws IOException
-    {
-        input.unread(bytes);
-        position -= bytes.length;
-    }
+  @Override
+  public void unread(final byte[] bytes) throws IOException {
+    input.unread(bytes);
+    position -= bytes.length;
+  }
 
-    @Override
-    public void unread(byte[] bytes, int start, int len) throws IOException
-    {
-        input.unread(bytes, start, len);
-        position -= len - start;
-    }
+  @Override
+  public void unread(final byte[] bytes, final int start, final int len) throws IOException {
+    input.unread(bytes, start, len);
+    position -= len - start;
+  }
 
-    @Override
-    public byte[] readFully(int length) throws IOException
-    {
-        byte[] bytes = new byte[length];
-        int off = 0;
-        int len = length;
-        while (len > 0)
-        {
-            int n = this.read(bytes, off, len);
-            if (n > 0)
-            {
-                off += n;
-                len -= n;
-                position += n;
-            }
-            else
-            {
-                break;
-            }
-        }
-        return bytes;
+  @Override
+  public byte[] readFully(final int length) throws IOException {
+    final byte[] bytes = new byte[length];
+    int off = 0;
+    int len = length;
+    while (len > 0) {
+      final int n = this.read(bytes, off, len);
+      if (n > 0) {
+        off += n;
+        len -= n;
+        position += n;
+      } else {
+        break;
+      }
     }
+    return bytes;
+  }
 
-    @Override
-    public boolean isEOF() throws IOException
-    {
-        return peek() == -1;
-    }
+  @Override
+  public boolean isEOF() throws IOException {
+    return peek() == -1;
+  }
 
-    @Override
-    public void close() throws IOException
-    {
-        input.close();
-    }
+  @Override
+  public void close() throws IOException {
+    input.close();
+  }
 }
