b2b6b9c24775d45bcc2ad49c728730438b351f19
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:28:25 +0700

    Clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/blend/BlendModeTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/blend/BlendModeTest.java
@@ -16,69 +16,66 @@
  */
 package org.apache.pdfbox.pdmodel.graphics.blend;
 
-import static junit.framework.TestCase.assertEquals;
 import org.apache.pdfbox.cos.COSName;
 import org.junit.Test;
+
+import junit.framework.TestCase;
 
 /**
  *
  * @author Tilman Hausherr
  */
-public class BlendModeTest
-{
-    public BlendModeTest()
-    {
-    }
+public class BlendModeTest {
+  public BlendModeTest() {
+  }
 
-    /**
-     * Check that BlendMode.* constant instances are not null. This could happen if the declaration
-     * sequence is changed.
-     */
-    @Test
-    public void testInstances()
-    {
-        assertEquals(BlendMode.NORMAL, BlendMode.getInstance(COSName.NORMAL));
-        assertEquals(BlendMode.NORMAL, BlendMode.getInstance(COSName.COMPATIBLE));
-        assertEquals(BlendMode.MULTIPLY, BlendMode.getInstance(COSName.MULTIPLY));
-        assertEquals(BlendMode.SCREEN, BlendMode.getInstance(COSName.SCREEN));
-        assertEquals(BlendMode.OVERLAY, BlendMode.getInstance(COSName.OVERLAY));
-        assertEquals(BlendMode.DARKEN, BlendMode.getInstance(COSName.DARKEN));
-        assertEquals(BlendMode.LIGHTEN, BlendMode.getInstance(COSName.LIGHTEN));
-        assertEquals(BlendMode.COLOR_DODGE, BlendMode.getInstance(COSName.COLOR_DODGE));
-        assertEquals(BlendMode.COLOR_BURN, BlendMode.getInstance(COSName.COLOR_BURN));
-        assertEquals(BlendMode.HARD_LIGHT, BlendMode.getInstance(COSName.HARD_LIGHT));
-        assertEquals(BlendMode.SOFT_LIGHT, BlendMode.getInstance(COSName.SOFT_LIGHT));
-        assertEquals(BlendMode.DIFFERENCE, BlendMode.getInstance(COSName.DIFFERENCE));
-        assertEquals(BlendMode.EXCLUSION, BlendMode.getInstance(COSName.EXCLUSION));
-        assertEquals(BlendMode.HUE, BlendMode.getInstance(COSName.HUE));
-        assertEquals(BlendMode.SATURATION, BlendMode.getInstance(COSName.SATURATION));
-        assertEquals(BlendMode.LUMINOSITY, BlendMode.getInstance(COSName.LUMINOSITY));
-        assertEquals(BlendMode.COLOR, BlendMode.getInstance(COSName.COLOR));
-    }
+  /**
+   * Check that BlendMode.* constant instances are not null. This could happen if
+   * the declaration sequence is changed.
+   */
+  @Test
+  public void testInstances() {
+    TestCase.assertEquals(BlendMode.NORMAL, BlendMode.getInstance(COSName.NORMAL));
+    TestCase.assertEquals(BlendMode.NORMAL, BlendMode.getInstance(COSName.COMPATIBLE));
+    TestCase.assertEquals(BlendMode.MULTIPLY, BlendMode.getInstance(COSName.MULTIPLY));
+    TestCase.assertEquals(BlendMode.SCREEN, BlendMode.getInstance(COSName.SCREEN));
+    TestCase.assertEquals(BlendMode.OVERLAY, BlendMode.getInstance(COSName.OVERLAY));
+    TestCase.assertEquals(BlendMode.DARKEN, BlendMode.getInstance(COSName.DARKEN));
+    TestCase.assertEquals(BlendMode.LIGHTEN, BlendMode.getInstance(COSName.LIGHTEN));
+    TestCase.assertEquals(BlendMode.COLOR_DODGE, BlendMode.getInstance(COSName.COLOR_DODGE));
+    TestCase.assertEquals(BlendMode.COLOR_BURN, BlendMode.getInstance(COSName.COLOR_BURN));
+    TestCase.assertEquals(BlendMode.HARD_LIGHT, BlendMode.getInstance(COSName.HARD_LIGHT));
+    TestCase.assertEquals(BlendMode.SOFT_LIGHT, BlendMode.getInstance(COSName.SOFT_LIGHT));
+    TestCase.assertEquals(BlendMode.DIFFERENCE, BlendMode.getInstance(COSName.DIFFERENCE));
+    TestCase.assertEquals(BlendMode.EXCLUSION, BlendMode.getInstance(COSName.EXCLUSION));
+    TestCase.assertEquals(BlendMode.HUE, BlendMode.getInstance(COSName.HUE));
+    TestCase.assertEquals(BlendMode.SATURATION, BlendMode.getInstance(COSName.SATURATION));
+    TestCase.assertEquals(BlendMode.LUMINOSITY, BlendMode.getInstance(COSName.LUMINOSITY));
+    TestCase.assertEquals(BlendMode.COLOR, BlendMode.getInstance(COSName.COLOR));
+  }
 
-    /**
-     * Check that COSName constants returned for BlendMode.* instances are not null. This could
-     * happen if the declaration sequence is changed.
-     */
-    @Test
-    public void testCOSNames()
-    {
-        assertEquals(COSName.NORMAL, BlendMode.getCOSName(BlendMode.NORMAL));
-        assertEquals(COSName.NORMAL, BlendMode.getCOSName(BlendMode.COMPATIBLE));
-        assertEquals(COSName.MULTIPLY, BlendMode.getCOSName(BlendMode.MULTIPLY));
-        assertEquals(COSName.SCREEN, BlendMode.getCOSName(BlendMode.SCREEN));
-        assertEquals(COSName.OVERLAY, BlendMode.getCOSName(BlendMode.OVERLAY));
-        assertEquals(COSName.DARKEN, BlendMode.getCOSName(BlendMode.DARKEN));
-        assertEquals(COSName.LIGHTEN, BlendMode.getCOSName(BlendMode.LIGHTEN));
-        assertEquals(COSName.COLOR_DODGE, BlendMode.getCOSName(BlendMode.COLOR_DODGE));
-        assertEquals(COSName.COLOR_BURN, BlendMode.getCOSName(BlendMode.COLOR_BURN));
-        assertEquals(COSName.HARD_LIGHT, BlendMode.getCOSName(BlendMode.HARD_LIGHT));
-        assertEquals(COSName.SOFT_LIGHT, BlendMode.getCOSName(BlendMode.SOFT_LIGHT));
-        assertEquals(COSName.DIFFERENCE, BlendMode.getCOSName(BlendMode.DIFFERENCE));
-        assertEquals(COSName.EXCLUSION, BlendMode.getCOSName(BlendMode.EXCLUSION));
-        assertEquals(COSName.HUE, BlendMode.getCOSName(BlendMode.HUE));
-        assertEquals(COSName.SATURATION, BlendMode.getCOSName(BlendMode.SATURATION));
-        assertEquals(COSName.LUMINOSITY, BlendMode.getCOSName(BlendMode.LUMINOSITY));
-        assertEquals(COSName.COLOR, BlendMode.getCOSName(BlendMode.COLOR));
-    }
+  /**
+   * Check that COSName constants returned for BlendMode.* instances are not null.
+   * This could happen if the declaration sequence is changed.
+   */
+  @Test
+  public void testCOSNames() {
+    TestCase.assertEquals(COSName.NORMAL, BlendMode.getCOSName(BlendMode.NORMAL));
+    TestCase.assertEquals(COSName.NORMAL, BlendMode.getCOSName(BlendMode.COMPATIBLE));
+    TestCase.assertEquals(COSName.MULTIPLY, BlendMode.getCOSName(BlendMode.MULTIPLY));
+    TestCase.assertEquals(COSName.SCREEN, BlendMode.getCOSName(BlendMode.SCREEN));
+    TestCase.assertEquals(COSName.OVERLAY, BlendMode.getCOSName(BlendMode.OVERLAY));
+    TestCase.assertEquals(COSName.DARKEN, BlendMode.getCOSName(BlendMode.DARKEN));
+    TestCase.assertEquals(COSName.LIGHTEN, BlendMode.getCOSName(BlendMode.LIGHTEN));
+    TestCase.assertEquals(COSName.COLOR_DODGE, BlendMode.getCOSName(BlendMode.COLOR_DODGE));
+    TestCase.assertEquals(COSName.COLOR_BURN, BlendMode.getCOSName(BlendMode.COLOR_BURN));
+    TestCase.assertEquals(COSName.HARD_LIGHT, BlendMode.getCOSName(BlendMode.HARD_LIGHT));
+    TestCase.assertEquals(COSName.SOFT_LIGHT, BlendMode.getCOSName(BlendMode.SOFT_LIGHT));
+    TestCase.assertEquals(COSName.DIFFERENCE, BlendMode.getCOSName(BlendMode.DIFFERENCE));
+    TestCase.assertEquals(COSName.EXCLUSION, BlendMode.getCOSName(BlendMode.EXCLUSION));
+    TestCase.assertEquals(COSName.HUE, BlendMode.getCOSName(BlendMode.HUE));
+    TestCase.assertEquals(COSName.SATURATION, BlendMode.getCOSName(BlendMode.SATURATION));
+    TestCase.assertEquals(COSName.LUMINOSITY, BlendMode.getCOSName(BlendMode.LUMINOSITY));
+    TestCase.assertEquals(COSName.COLOR, BlendMode.getCOSName(BlendMode.COLOR));
+  }
 }
