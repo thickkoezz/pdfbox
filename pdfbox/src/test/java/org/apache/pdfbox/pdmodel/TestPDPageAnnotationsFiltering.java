fd652b56174a0ae4917dfa077b90531dfb16df35
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:42:09 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/TestPDPageAnnotationsFiltering.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/TestPDPageAnnotationsFiltering.java
@@ -22,7 +22,6 @@ import java.util.List;
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.pdmodel.interactive.annotation.AnnotationFilter;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationRubberStamp;
@@ -34,64 +33,46 @@ import org.junit.Test;
 /**
  * This unit test validates the correct working behavior of PDPage annotations
  * filtering
- * 
+ *
  * @author <a href="mailto:maxime.veron.pro@gmail.com">Maxime Veron</a>
  *
  */
-public class TestPDPageAnnotationsFiltering
-{
-    // test mock page for annotations filtering
-    private PDPage page;
+public class TestPDPageAnnotationsFiltering {
+  // test mock page for annotations filtering
+  private PDPage page;
 
-    @Before
-    public void initMock()
-    {
-        COSDictionary mockedPageWithAnnotations = new COSDictionary();
-        COSArray annotsDictionnary = new COSArray();
-        annotsDictionnary.add(new PDAnnotationRubberStamp().getCOSObject());
-        annotsDictionnary.add(new PDAnnotationSquare().getCOSObject());
-        annotsDictionnary.add(new PDAnnotationLink().getCOSObject());
-        mockedPageWithAnnotations.setItem(COSName.ANNOTS, annotsDictionnary);
-        page = new PDPage(mockedPageWithAnnotations);
-    }
+  @Before
+  public void initMock() {
+    final COSDictionary mockedPageWithAnnotations = new COSDictionary();
+    final COSArray annotsDictionnary = new COSArray();
+    annotsDictionnary.add(new PDAnnotationRubberStamp().getCOSObject());
+    annotsDictionnary.add(new PDAnnotationSquare().getCOSObject());
+    annotsDictionnary.add(new PDAnnotationLink().getCOSObject());
+    mockedPageWithAnnotations.setItem(COSName.ANNOTS, annotsDictionnary);
+    page = new PDPage(mockedPageWithAnnotations);
+  }
 
-    @Test
-    public void validateNoFiltering() throws IOException
-    {
-        List<PDAnnotation> annotations = page.getAnnotations();
-        Assert.assertEquals(3, annotations.size());
-        Assert.assertTrue(annotations.get(0) instanceof PDAnnotationRubberStamp);
-        Assert.assertTrue(annotations.get(1) instanceof PDAnnotationSquare);
-        Assert.assertTrue(annotations.get(2) instanceof PDAnnotationLink);
-    }
+  @Test
+  public void validateNoFiltering() throws IOException {
+    final List<PDAnnotation> annotations = page.getAnnotations();
+    Assert.assertEquals(3, annotations.size());
+    Assert.assertTrue(annotations.get(0) instanceof PDAnnotationRubberStamp);
+    Assert.assertTrue(annotations.get(1) instanceof PDAnnotationSquare);
+    Assert.assertTrue(annotations.get(2) instanceof PDAnnotationLink);
+  }
 
-    @Test
-    public void validateAllFiltered() throws IOException
-    {
-        List<PDAnnotation> annotations = page.getAnnotations(new AnnotationFilter()
-        {
-            @Override
-            public boolean accept(PDAnnotation annotation)
-            {
-                return false;
-            }
-        });
-        Assert.assertEquals(0, annotations.size());
-    }
+  @Test
+  public void validateAllFiltered() throws IOException {
+    final List<PDAnnotation> annotations = page.getAnnotations(annotation -> false);
+    Assert.assertEquals(0, annotations.size());
+  }
 
-    @Test
-    public void validateSelectedFew() throws IOException
-    {
-        List<PDAnnotation> annotations = page.getAnnotations(new AnnotationFilter()
-        {
-            @Override
-            public boolean accept(PDAnnotation annotation)
-            {
-                return (annotation instanceof PDAnnotationLink || annotation instanceof PDAnnotationSquare);
-            }
-        });
-        Assert.assertEquals(2, annotations.size());
-        Assert.assertTrue(annotations.get(0) instanceof PDAnnotationSquare);
-        Assert.assertTrue(annotations.get(1) instanceof PDAnnotationLink);
-    }
+  @Test
+  public void validateSelectedFew() throws IOException {
+    final List<PDAnnotation> annotations = page.getAnnotations(
+        annotation -> annotation instanceof PDAnnotationLink || annotation instanceof PDAnnotationSquare);
+    Assert.assertEquals(2, annotations.size());
+    Assert.assertTrue(annotations.get(0) instanceof PDAnnotationSquare);
+    Assert.assertTrue(annotations.get(1) instanceof PDAnnotationLink);
+  }
 }
