99206fff06f43cf3cf336a92e50e52bc2344a5cb
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:34:19 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetFontAndSize.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetFontAndSize.java
@@ -17,17 +17,16 @@
 
 package org.apache.pdfbox.contentstream.operator.text;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.pdfbox.contentstream.operator.MissingOperandException;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
-
-import java.io.IOException;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 
 /**
@@ -35,36 +34,27 @@ import org.apache.pdfbox.pdmodel.font.PDFont;
  *
  * @author Laurent Huault
  */
-public class SetFontAndSize extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        if (arguments.size() < 2)
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
+public class SetFontAndSize extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    if (arguments.size() < 2)
+      throw new MissingOperandException(operator, arguments);
 
-        COSBase base0 = arguments.get(0);
-        COSBase base1 = arguments.get(1);
-        if (!(base0 instanceof COSName))
-        {
-            return;
-        }
-        if (!(base1 instanceof COSNumber))
-        {
-            return;
-        }
-        COSName fontName = (COSName) base0;
-        float fontSize = ((COSNumber) base1).floatValue();
-        context.getGraphicsState().getTextState().setFontSize(fontSize);
-        PDFont font = context.getResources().getFont(fontName);
-        context.getGraphicsState().getTextState().setFont(font);
-    }
+    final COSBase base0 = arguments.get(0);
+    final COSBase base1 = arguments.get(1);
+    if (!(base0 instanceof COSName))
+      return;
+    if (!(base1 instanceof COSNumber))
+      return;
+    final COSName fontName = (COSName) base0;
+    final float fontSize = ((COSNumber) base1).floatValue();
+    context.getGraphicsState().getTextState().setFontSize(fontSize);
+    final PDFont font = context.getResources().getFont(fontName);
+    context.getGraphicsState().getTextState().setFont(font);
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_FONT_AND_SIZE;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_FONT_AND_SIZE;
+  }
 }
