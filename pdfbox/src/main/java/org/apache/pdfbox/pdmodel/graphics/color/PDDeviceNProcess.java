00d92867bcc24f9893ff306274a4c202fd89fea8
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:28:43 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceNProcess.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceNProcess.java
@@ -16,113 +16,100 @@
  */
 package org.apache.pdfbox.pdmodel.graphics.color;
 
-import org.apache.pdfbox.cos.COSArray;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.cos.COSName;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
 
 /**
  * A DeviceN Process Dictionary
  *
  * @author John Hewson
  */
-public class PDDeviceNProcess
-{
-    /**
-     * Log instance.
-     */
-    private static final Log LOG = LogFactory.getLog(PDDeviceNProcess.class);
+public class PDDeviceNProcess {
+  /**
+   * Log instance.
+   */
+  private static final Log LOG = LogFactory.getLog(PDDeviceNProcess.class);
 
-    private final COSDictionary dictionary;
+  private final COSDictionary dictionary;
 
-    /**
-     * Creates a new DeviceN Process Dictionary.
-     */
-    public PDDeviceNProcess()
-    {
-        dictionary = new COSDictionary();
+  /**
+   * Creates a new DeviceN Process Dictionary.
+   */
+  public PDDeviceNProcess() {
+    dictionary = new COSDictionary();
+  }
+
+  /**
+   * Creates a new DeviceN Process Dictionary from the given attributes.
+   *
+   * @param attributes a DeviceN attributes dictionary
+   */
+  public PDDeviceNProcess(final COSDictionary attributes) {
+    dictionary = attributes;
+  }
+
+  /**
+   * Returns the underlying COS dictionary.
+   *
+   * @return the underlying COS dictionary.
+   */
+  public COSDictionary getCOSDictionary() {
+    return dictionary;
+  }
+
+  /**
+   * Returns the process color space
+   *
+   * @return the process color space
+   * @throws IOException if the color space cannot be read
+   */
+  public PDColorSpace getColorSpace() throws IOException {
+    final COSBase cosColorSpace = dictionary.getDictionaryObject(COSName.COLORSPACE);
+    if (cosColorSpace == null)
+      return null; // TODO: return a default?
+    return PDColorSpace.create(cosColorSpace);
+  }
+
+  /**
+   * Returns the names of the color components.
+   *
+   * @return the names of the color components
+   */
+  public List<String> getComponents() {
+    final List<String> components = new ArrayList<>();
+    final COSArray cosComponents = (COSArray) dictionary.getDictionaryObject(COSName.COMPONENTS);
+    if (cosComponents == null)
+      return components;
+    for (final COSBase name : cosComponents) {
+      components.add(((COSName) name).getName());
     }
+    return components;
+  }
 
-    /**
-     * Creates a new  DeviceN Process Dictionary from the given attributes.
-     * @param attributes a DeviceN attributes dictionary
-     */
-    public PDDeviceNProcess(COSDictionary attributes)
-    {
-        dictionary = attributes;
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("Process{");
+    try {
+      sb.append(getColorSpace());
+      for (final String component : getComponents()) {
+        sb.append(" \"");
+        sb.append(component);
+        sb.append('\"');
+      }
+    } catch (final IOException e) {
+      PDDeviceNProcess.LOG.debug("Couldn't get the colorants information - returning 'ERROR' instead'", e);
+      sb.append("ERROR");
     }
-
-    /**
-     * Returns the underlying COS dictionary.
-     * @return the underlying COS dictionary.
-     */
-    public COSDictionary getCOSDictionary()
-    {
-        return dictionary;
-    }
-
-    /**
-     * Returns the process color space
-     * @return the process color space
-     * @throws IOException if the color space cannot be read
-     */
-    public PDColorSpace getColorSpace() throws IOException
-    {
-        COSBase cosColorSpace = dictionary.getDictionaryObject(COSName.COLORSPACE);
-        if (cosColorSpace == null)
-        {
-            return null; // TODO: return a default?
-        }
-        return PDColorSpace.create(cosColorSpace);
-    }
-
-    /**
-     * Returns the names of the color components.
-     * @return the names of the color components
-     */
-    public List<String> getComponents()
-    {
-        List<String> components = new ArrayList<>();
-        COSArray cosComponents = (COSArray)dictionary.getDictionaryObject(COSName.COMPONENTS);
-        if (cosComponents == null)
-        {
-            return components;
-        }
-        for (COSBase name : cosComponents)
-        {
-            components.add(((COSName)name).getName());
-        }
-        return components;
-    }
-
-    @Override
-    public String toString()
-    {
-        StringBuilder sb = new StringBuilder("Process{");
-        try
-        {
-            sb.append(getColorSpace());
-            for (String component : getComponents())
-            {
-                sb.append(" \"");
-                sb.append(component);
-                sb.append('\"');
-            }
-        }
-        catch (IOException e)
-        {
-            LOG.debug("Couldn't get the colorants information - returning 'ERROR' instead'", e);
-            sb.append("ERROR");
-        }
-        sb.append('}');
-        return sb.toString();
-    }
+    sb.append('}');
+    return sb.toString();
+  }
 
 }
