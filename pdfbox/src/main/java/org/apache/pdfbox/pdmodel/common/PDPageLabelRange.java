20bf104beba6d58dca0d1d2d2303ca725f7dc234
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:49:59 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDPageLabelRange.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDPageLabelRange.java
@@ -21,165 +21,141 @@ import org.apache.pdfbox.cos.COSName;
 
 /**
  * Contains information for a page label range.
- * 
+ *
  * @author Igor Podolskiy
- * 
+ *
  * @see PDPageLabels
- * 
+ *
  */
-public class PDPageLabelRange implements COSObjectable
-{
+public class PDPageLabelRange implements COSObjectable {
 
-    private COSDictionary root;
+  private final COSDictionary root;
 
-    // Page label dictonary (PDF32000-1:2008 Section 12.4.2, Table 159)
-    private static final COSName KEY_START = COSName.ST;
-    private static final COSName KEY_PREFIX = COSName.P;
-    private static final COSName KEY_STYLE = COSName.S;
+  // Page label dictonary (PDF32000-1:2008 Section 12.4.2, Table 159)
+  private static final COSName KEY_START = COSName.ST;
+  private static final COSName KEY_PREFIX = COSName.P;
+  private static final COSName KEY_STYLE = COSName.S;
 
-    // Style entry values (PDF32000-1:2008 Section 12.4.2, Table 159)
+  // Style entry values (PDF32000-1:2008 Section 12.4.2, Table 159)
 
-    /**
-     * Decimal page numbering style (1, 2, 3, ...).
-     */
-    public static final String STYLE_DECIMAL = "D";
+  /**
+   * Decimal page numbering style (1, 2, 3, ...).
+   */
+  public static final String STYLE_DECIMAL = "D";
 
-    /**
-     * Roman numbers (upper case) numbering style (I, II, III, IV, ...).
-     */
-    public static final String STYLE_ROMAN_UPPER = "R";
+  /**
+   * Roman numbers (upper case) numbering style (I, II, III, IV, ...).
+   */
+  public static final String STYLE_ROMAN_UPPER = "R";
 
-    /**
-     * Roman numbers (lower case) numbering style (i, ii, iii, iv, ...).
-     */
-    public static final String STYLE_ROMAN_LOWER = "r";
+  /**
+   * Roman numbers (lower case) numbering style (i, ii, iii, iv, ...).
+   */
+  public static final String STYLE_ROMAN_LOWER = "r";
 
-    /**
-     * Letter (upper case) numbering style (A, B, ..., Z, AA, BB, ..., ZZ, AAA,
-     * ...).
-     */
-    public static final String STYLE_LETTERS_UPPER = "A";
+  /**
+   * Letter (upper case) numbering style (A, B, ..., Z, AA, BB, ..., ZZ, AAA,
+   * ...).
+   */
+  public static final String STYLE_LETTERS_UPPER = "A";
 
-    /**
-     * Letter (lower case) numbering style (a, b, ..., z, aa, bb, ..., zz, aaa,
-     * ...).
-     */
-    public static final String STYLE_LETTERS_LOWER = "a";
+  /**
+   * Letter (lower case) numbering style (a, b, ..., z, aa, bb, ..., zz, aaa,
+   * ...).
+   */
+  public static final String STYLE_LETTERS_LOWER = "a";
 
-    /**
-     * Creates a new empty page label range object.
-     */
-    public PDPageLabelRange()
-    {
-        this(new COSDictionary());
+  /**
+   * Creates a new empty page label range object.
+   */
+  public PDPageLabelRange() {
+    this(new COSDictionary());
+  }
+
+  /**
+   * Creates a new page label range object from the given dictionary.
+   *
+   * @param dict the base dictionary for the new object.
+   */
+  public PDPageLabelRange(final COSDictionary dict) {
+    root = dict;
+  }
+
+  /**
+   * Returns the underlying dictionary.
+   *
+   * @return the underlying dictionary.
+   */
+  @Override
+  public COSDictionary getCOSObject() {
+    return root;
+  }
+
+  /**
+   * Returns the numbering style for this page range.
+   *
+   * @return one of the STYLE_* constants
+   */
+  public String getStyle() {
+    return root.getNameAsString(PDPageLabelRange.KEY_STYLE);
+  }
+
+  /**
+   * Sets the numbering style for this page range.
+   *
+   * @param style one of the STYLE_* constants or {@code null} if no page
+   *              numbering is desired.
+   */
+  public void setStyle(final String style) {
+    if (style != null) {
+      root.setName(PDPageLabelRange.KEY_STYLE, style);
+    } else {
+      root.removeItem(PDPageLabelRange.KEY_STYLE);
     }
+  }
 
-    /**
-     * Creates a new page label range object from the given dictionary.
-     * 
-     * @param dict
-     *            the base dictionary for the new object.
-     */
-    public PDPageLabelRange(COSDictionary dict)
-    {
-        root = dict;
-    }
+  /**
+   * Returns the start value for page numbering in this page range.
+   *
+   * @return a positive integer the start value for numbering.
+   */
+  public int getStart() {
+    return root.getInt(PDPageLabelRange.KEY_START, 1);
+  }
 
-    /**
-     * Returns the underlying dictionary.
-     * 
-     * @return the underlying dictionary.
-     */
-    @Override
-    public COSDictionary getCOSObject()
-    {
-        return root;
-    }
+  /**
+   * Sets the start value for page numbering in this page range.
+   *
+   * @param start a positive integer representing the start value.
+   * @throws IllegalArgumentException if {@code start} is not a positive integer
+   */
+  public void setStart(final int start) {
+    if (start <= 0)
+      throw new IllegalArgumentException("The page numbering start value must be a positive integer");
+    root.setInt(PDPageLabelRange.KEY_START, start);
+  }
 
-    /**
-     * Returns the numbering style for this page range.
-     * 
-     * @return one of the STYLE_* constants
-     */
-    public String getStyle()
-    {
-        return root.getNameAsString(KEY_STYLE);
-    }
+  /**
+   * Returns the page label prefix for this page range.
+   *
+   * @return the page label prefix for this page range, or {@code null} if no
+   *         prefix has been defined.
+   */
+  public String getPrefix() {
+    return root.getString(PDPageLabelRange.KEY_PREFIX);
+  }
 
-    /**
-     * Sets the numbering style for this page range.
-     * 
-     * @param style
-     *            one of the STYLE_* constants or {@code null} if no page
-     *            numbering is desired.
-     */
-    public void setStyle(String style)
-    {
-        if (style != null)
-        {
-            root.setName(KEY_STYLE, style);
-        }
-        else
-        {
-            root.removeItem(KEY_STYLE);
-        }
+  /**
+   * Sets the page label prefix for this page range.
+   *
+   * @param prefix the page label prefix for this page range, or {@code null} to
+   *               unset the prefix.
+   */
+  public void setPrefix(final String prefix) {
+    if (prefix != null) {
+      root.setString(PDPageLabelRange.KEY_PREFIX, prefix);
+    } else {
+      root.removeItem(PDPageLabelRange.KEY_PREFIX);
     }
-
-    /**
-     * Returns the start value for page numbering in this page range.
-     * 
-     * @return a positive integer the start value for numbering.
-     */
-    public int getStart()
-    {
-        return root.getInt(KEY_START, 1);
-    }
-
-    /**
-     * Sets the start value for page numbering in this page range.
-     * 
-     * @param start
-     *            a positive integer representing the start value.
-     * @throws IllegalArgumentException
-     *             if {@code start} is not a positive integer
-     */
-    public void setStart(int start)
-    {
-        if (start <= 0)
-        {
-            throw new IllegalArgumentException(
-                    "The page numbering start value must be a positive integer");
-        }
-        root.setInt(KEY_START, start);
-    }
-
-    /**
-     * Returns the page label prefix for this page range.
-     * 
-     * @return the page label prefix for this page range, or {@code null} if no
-     *         prefix has been defined.
-     */
-    public String getPrefix()
-    {
-        return root.getString(KEY_PREFIX);
-    }
-
-    /**
-     * Sets the page label prefix for this page range.
-     * 
-     * @param prefix
-     *            the page label prefix for this page range, or {@code null} to
-     *            unset the prefix.
-     */
-    public void setPrefix(String prefix)
-    {
-        if (prefix != null)
-        {
-            root.setString(KEY_PREFIX, prefix);
-        }
-        else
-        {
-            root.removeItem(KEY_PREFIX);
-        }
-    }
+  }
 }
