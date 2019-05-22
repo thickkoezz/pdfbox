d6ddb102f338ecf1aac2a3bcf56020ff15966783
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:07:39 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/Save.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/Save.java
@@ -18,27 +18,24 @@ package org.apache.pdfbox.contentstream.operator.state;
 
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
 
 /**
  * q: Save the graphics state.
  *
  * @author Laurent Huault
  */
-public class Save extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments)
-    {
-        context.saveGraphicsState();
-    }
+public class Save extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) {
+    context.saveGraphicsState();
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SAVE;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SAVE;
+  }
 }
