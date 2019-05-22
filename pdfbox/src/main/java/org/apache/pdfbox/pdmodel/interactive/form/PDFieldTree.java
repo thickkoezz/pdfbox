8f19bb0994af4c65f34518735d8fbb1765f589e0
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:35:11 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDFieldTree.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDFieldTree.java
@@ -18,90 +18,74 @@ package org.apache.pdfbox.pdmodel.interactive.form;
 
 import java.util.ArrayDeque;
 import java.util.Iterator;
-import java.util.Queue;
-
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
  * The field tree.
  */
-public class PDFieldTree implements Iterable<PDField>
-{
-    private final PDAcroForm acroForm;
+public class PDFieldTree implements Iterable<PDField> {
+  private final PDAcroForm acroForm;
 
-    /**
-     * Constructor for reading.
-     *
-     * @param acroForm the AcroForm containing the fields.
-     */
-    public PDFieldTree(PDAcroForm acroForm)
-    {
-        if (acroForm == null)
-        {
-            throw new IllegalArgumentException("root cannot be null");
-        }
-        this.acroForm = acroForm;
+  /**
+   * Constructor for reading.
+   *
+   * @param acroForm the AcroForm containing the fields.
+   */
+  public PDFieldTree(final PDAcroForm acroForm) {
+    if (acroForm == null)
+      throw new IllegalArgumentException("root cannot be null");
+    this.acroForm = acroForm;
+  }
+
+  /**
+   * Returns an iterator which walks all fields in the tree, in order.
+   */
+  @Override
+  public Iterator<PDField> iterator() {
+    return new FieldIterator(acroForm);
+  }
+
+  /**
+   * Iterator which walks all fields in the tree, in order.
+   */
+  private static final class FieldIterator implements Iterator<PDField> {
+    private final Queue<PDField> queue = new ArrayDeque<>();
+
+    private FieldIterator(final PDAcroForm form) {
+      final List<PDField> fields = form.getFields();
+      for (final PDField field : fields) {
+        enqueueKids(field);
+      }
     }
 
-    /**
-     * Returns an iterator which walks all fields in the tree, in order.
-     */
     @Override
-    public Iterator<PDField> iterator()
-    {
-        return new FieldIterator(acroForm);
+    public boolean hasNext() {
+      return !queue.isEmpty();
     }
 
-    /**
-     * Iterator which walks all fields in the tree, in order.
-     */
-    private static final class FieldIterator implements Iterator<PDField>
-    {
-        private final Queue<PDField> queue = new ArrayDeque<>();
-        
-        private FieldIterator(PDAcroForm form)
-        {
-            List<PDField> fields = form.getFields();
-            for (PDField field : fields)
-            {
-                enqueueKids(field);
-            }
-        }
+    @Override
+    public PDField next() {
+      if (!hasNext())
+        throw new NoSuchElementException();
 
-        @Override
-        public boolean hasNext()
-        {
-            return !queue.isEmpty();
-        }
-
-        @Override
-        public PDField next()
-        {
-            if(!hasNext()){
-                throw new NoSuchElementException();
-            }
-            
-            return queue.poll();
-        }
-
-        @Override
-        public void remove()
-        {
-            throw new UnsupportedOperationException();
-        }
-        
-        private void enqueueKids(PDField node)
-        {
-            queue.add(node);
-            if (node instanceof PDNonTerminalField)
-            {
-                List<PDField> kids = ((PDNonTerminalField) node).getChildren();
-                for (PDField kid : kids)
-                {
-                    enqueueKids(kid);
-                }
-            }
-        }
+      return queue.poll();
     }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+    private void enqueueKids(final PDField node) {
+      queue.add(node);
+      if (node instanceof PDNonTerminalField) {
+        final List<PDField> kids = ((PDNonTerminalField) node).getChildren();
+        for (final PDField kid : kids) {
+          enqueueKids(kid);
+        }
+      }
+    }
+  }
 }
