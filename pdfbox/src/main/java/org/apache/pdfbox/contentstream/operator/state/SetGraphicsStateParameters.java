3212fbe924a073cab663cfaf4c3200334ef62836
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:34:23 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetGraphicsStateParameters.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetGraphicsStateParameters.java
@@ -21,50 +21,42 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
+import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
-import org.apache.pdfbox.contentstream.operator.MissingOperandException;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
 
 /**
  * gs: Set parameters from graphics state parameter dictionary.
  *
  * @author Ben Litchfield
  */
-public class SetGraphicsStateParameters extends OperatorProcessor
-{
-    private static final Log LOG = LogFactory.getLog(SetGraphicsStateParameters.class);
+public class SetGraphicsStateParameters extends OperatorProcessor {
+  private static final Log LOG = LogFactory.getLog(SetGraphicsStateParameters.class);
 
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        if (arguments.isEmpty())
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
-        COSBase base0 = arguments.get(0);
-        if (!(base0 instanceof COSName))
-        {
-            return;
-        }
-        
-        // set parameters from graphics state parameter dictionary
-        COSName graphicsName = (COSName) base0;
-        PDExtendedGraphicsState gs = context.getResources().getExtGState(graphicsName);
-        if (gs == null)
-        {
-            LOG.error("name for 'gs' operator not found in resources: /" + graphicsName.getName());
-            return;
-        }
-        gs.copyIntoGraphicsState( context.getGraphicsState() );
-    }
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    if (arguments.isEmpty())
+      throw new MissingOperandException(operator, arguments);
+    final COSBase base0 = arguments.get(0);
+    if (!(base0 instanceof COSName))
+      return;
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_GRAPHICS_STATE_PARAMS;
+    // set parameters from graphics state parameter dictionary
+    final COSName graphicsName = (COSName) base0;
+    final PDExtendedGraphicsState gs = context.getResources().getExtGState(graphicsName);
+    if (gs == null) {
+      SetGraphicsStateParameters.LOG.error("name for 'gs' operator not found in resources: /" + graphicsName.getName());
+      return;
     }
+    gs.copyIntoGraphicsState(context.getGraphicsState());
+  }
+
+  @Override
+  public String getName() {
+    return OperatorName.SET_GRAPHICS_STATE_PARAMS;
+  }
 }
