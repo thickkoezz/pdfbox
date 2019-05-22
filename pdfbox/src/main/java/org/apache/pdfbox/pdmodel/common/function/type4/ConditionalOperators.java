cdbb51cc3fdf125074ffe2dd9301e0ad8be59b15
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:29:09 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/ConditionalOperators.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/ConditionalOperators.java
@@ -22,46 +22,37 @@ import java.util.Stack;
  * Provides the conditional operators such as "if" and "ifelse".
  *
  */
-class ConditionalOperators
-{
+class ConditionalOperators {
 
-    /** Implements the "if" operator. */
-    static class If implements Operator
-    {
-        @Override
-        public void execute(ExecutionContext context)
-        {
-            Stack<Object> stack = context.getStack();
-            InstructionSequence proc = (InstructionSequence)stack.pop();
-            Boolean condition = (Boolean)stack.pop();
-            if (condition)
-            {
-                proc.execute(context);
-            }
-        }
-
+  /** Implements the "if" operator. */
+  static class If implements Operator {
+    @Override
+    public void execute(final ExecutionContext context) {
+      final Stack<Object> stack = context.getStack();
+      final InstructionSequence proc = (InstructionSequence) stack.pop();
+      final Boolean condition = (Boolean) stack.pop();
+      if (condition) {
+        proc.execute(context);
+      }
     }
 
-    /** Implements the "ifelse" operator. */
-    static class IfElse implements Operator
-    {
-        @Override
-        public void execute(ExecutionContext context)
-        {
-            Stack<Object> stack = context.getStack();
-            InstructionSequence proc2 = (InstructionSequence)stack.pop();
-            InstructionSequence proc1 = (InstructionSequence)stack.pop();
-            Boolean condition = (Boolean)stack.pop();
-            if (condition)
-            {
-                proc1.execute(context);
-            }
-            else
-            {
-                proc2.execute(context);
-            }
-        }
+  }
 
+  /** Implements the "ifelse" operator. */
+  static class IfElse implements Operator {
+    @Override
+    public void execute(final ExecutionContext context) {
+      final Stack<Object> stack = context.getStack();
+      final InstructionSequence proc2 = (InstructionSequence) stack.pop();
+      final InstructionSequence proc1 = (InstructionSequence) stack.pop();
+      final Boolean condition = (Boolean) stack.pop();
+      if (condition) {
+        proc1.execute(context);
+      } else {
+        proc2.execute(context);
+      }
     }
+
+  }
 
 }
