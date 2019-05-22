2f8880027a15e4cc67a9e798b2f2ae6639cdcc2c
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:35:31 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetStrokingColorN.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetStrokingColorN.java
@@ -20,16 +20,14 @@ package org.apache.pdfbox.contentstream.operator.color;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 
 /**
- * SCN: Sets the colour to use for stroking stroking operations.
- * Supports Pattern, Separation, DeviceN and ICCBased colour spaces.
+ * SCN: Sets the colour to use for stroking stroking operations. Supports
+ * Pattern, Separation, DeviceN and ICCBased colour spaces.
  *
  * @author John Hewson
  */
-public class SetStrokingColorN extends SetStrokingColor
-{
-    @Override
-    public String getName()
-    {
-        return OperatorName.STROKING_COLOR_N;
-    }
+public class SetStrokingColorN extends SetStrokingColor {
+  @Override
+  public String getName() {
+    return OperatorName.STROKING_COLOR_N;
+  }
 }
