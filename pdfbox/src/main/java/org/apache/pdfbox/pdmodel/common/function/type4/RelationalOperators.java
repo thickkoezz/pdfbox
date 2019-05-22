9fe94a9fa5d731e6084decc3af9b933317858b8d
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:06:39 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/RelationalOperators.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/RelationalOperators.java
@@ -22,118 +22,99 @@ import java.util.Stack;
  * Provides the relational operators such as "eq" and "le".
  *
  */
-class RelationalOperators
-{
+class RelationalOperators {
 
-    /** Implements the "eq" operator. */
-    static class Eq implements Operator
-    {
-        @Override
-        public void execute(ExecutionContext context)
-        {
-            Stack<Object> stack = context.getStack();
-            Object op2 = stack.pop();
-            Object op1 = stack.pop();
-            boolean result = isEqual(op1, op2);
-            stack.push(result);
-        }
-
-        protected boolean isEqual(Object op1, Object op2)
-        {
-            boolean result;
-            if (op1 instanceof Number && op2 instanceof Number)
-            {
-                Number num1 = (Number)op1;
-                Number num2 = (Number)op2;
-                result = Float.compare(num1.floatValue(),num2.floatValue()) == 0;
-            }
-            else
-            {
-                result = op1.equals(op2);
-            }
-            return result;
-        }
-
+  /** Implements the "eq" operator. */
+  static class Eq implements Operator {
+    @Override
+    public void execute(final ExecutionContext context) {
+      final Stack<Object> stack = context.getStack();
+      final Object op2 = stack.pop();
+      final Object op1 = stack.pop();
+      final boolean result = isEqual(op1, op2);
+      stack.push(result);
     }
 
-    /** Abstract base class for number comparison operators. */
-    private abstract static class AbstractNumberComparisonOperator implements Operator
-    {
-        @Override
-        public void execute(ExecutionContext context)
-        {
-            Stack<Object> stack = context.getStack();
-            Object op2 = stack.pop();
-            Object op1 = stack.pop();
-            Number num1 = (Number)op1;
-            Number num2 = (Number)op2;
-            boolean result = compare(num1, num2);
-            stack.push(result);
-        }
-
-        protected abstract boolean compare(Number num1, Number num2);
-
+    protected boolean isEqual(final Object op1, final Object op2) {
+      boolean result;
+      if (op1 instanceof Number && op2 instanceof Number) {
+        final Number num1 = (Number) op1;
+        final Number num2 = (Number) op2;
+        result = Float.compare(num1.floatValue(), num2.floatValue()) == 0;
+      } else {
+        result = op1.equals(op2);
+      }
+      return result;
     }
 
-    /** Implements the "ge" operator. */
-    static class Ge extends AbstractNumberComparisonOperator
-    {
+  }
 
-        @Override
-        protected boolean compare(Number num1, Number num2)
-        {
-            return num1.floatValue() >= num2.floatValue();
-        }
-
+  /** Abstract base class for number comparison operators. */
+  private abstract static class AbstractNumberComparisonOperator implements Operator {
+    @Override
+    public void execute(final ExecutionContext context) {
+      final Stack<Object> stack = context.getStack();
+      final Object op2 = stack.pop();
+      final Object op1 = stack.pop();
+      final Number num1 = (Number) op1;
+      final Number num2 = (Number) op2;
+      final boolean result = compare(num1, num2);
+      stack.push(result);
     }
 
-    /** Implements the "gt" operator. */
-    static class Gt extends AbstractNumberComparisonOperator
-    {
+    protected abstract boolean compare(Number num1, Number num2);
 
-        @Override
-        protected boolean compare(Number num1, Number num2)
-        {
-            return num1.floatValue() > num2.floatValue();
-        }
+  }
 
+  /** Implements the "ge" operator. */
+  static class Ge extends AbstractNumberComparisonOperator {
+
+    @Override
+    protected boolean compare(final Number num1, final Number num2) {
+      return num1.floatValue() >= num2.floatValue();
     }
 
-    /** Implements the "le" operator. */
-    static class Le extends AbstractNumberComparisonOperator
-    {
+  }
 
-        @Override
-        protected boolean compare(Number num1, Number num2)
-        {
-            return num1.floatValue() <= num2.floatValue();
-        }
+  /** Implements the "gt" operator. */
+  static class Gt extends AbstractNumberComparisonOperator {
 
+    @Override
+    protected boolean compare(final Number num1, final Number num2) {
+      return num1.floatValue() > num2.floatValue();
     }
 
-    /** Implements the "lt" operator. */
-    static class Lt extends AbstractNumberComparisonOperator
-    {
+  }
 
-        @Override
-        protected boolean compare(Number num1, Number num2)
-        {
-            return num1.floatValue() < num2.floatValue();
-        }
+  /** Implements the "le" operator. */
+  static class Le extends AbstractNumberComparisonOperator {
 
+    @Override
+    protected boolean compare(final Number num1, final Number num2) {
+      return num1.floatValue() <= num2.floatValue();
     }
 
-    /** Implements the "ne" operator. */
-    static class Ne extends Eq
-    {
+  }
 
-        @Override
-        protected boolean isEqual(Object op1, Object op2)
-        {
-            boolean result = super.isEqual(op1, op2);
-            return !result;
-        }
+  /** Implements the "lt" operator. */
+  static class Lt extends AbstractNumberComparisonOperator {
 
+    @Override
+    protected boolean compare(final Number num1, final Number num2) {
+      return num1.floatValue() < num2.floatValue();
     }
+
+  }
+
+  /** Implements the "ne" operator. */
+  static class Ne extends Eq {
+
+    @Override
+    protected boolean isEqual(final Object op1, final Object op2) {
+      final boolean result = super.isEqual(op1, op2);
+      return !result;
+    }
+
+  }
 
 }
