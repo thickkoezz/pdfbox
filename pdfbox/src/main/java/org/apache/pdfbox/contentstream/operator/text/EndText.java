ff184c001337d996a9d2e92b419b76070cc19297
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:47:36 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/EndText.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/EndText.java
@@ -19,29 +19,26 @@ package org.apache.pdfbox.contentstream.operator.text;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
 
 /**
  * ET: End text.
  *
  * @author Laurent Huault
  */
-public class EndText extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        context.setTextMatrix(null);
-        context.setTextLineMatrix(null);
-        context.endText();
-    }
+public class EndText extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    context.setTextMatrix(null);
+    context.setTextLineMatrix(null);
+    context.endText();
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.END_TEXT;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.END_TEXT;
+  }
 }
