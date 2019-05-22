f5603d4c9504f4d122b4e5c9ea83483138861796
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:14:27 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionImportData.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionImportData.java
@@ -17,60 +17,55 @@
 
 package org.apache.pdfbox.pdmodel.interactive.action;
 
-import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.pdmodel.common.filespecification.PDFileSpecification;
-
 import java.io.IOException;
+
+import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDFileSpecification;
 
 /**
  * @author Timur Kamalov
  */
-public class PDActionImportData extends PDAction
-{
+public class PDActionImportData extends PDAction {
 
-    /**
-     * This type of action this object represents.
-     */
-    public static final String SUB_TYPE = "ImportData";
+  /**
+   * This type of action this object represents.
+   */
+  public static final String SUB_TYPE = "ImportData";
 
-    /**
-     * Default constructor.
-     */
-    public PDActionImportData()
-    {
-        setSubType(SUB_TYPE);
-    }
+  /**
+   * Default constructor.
+   */
+  public PDActionImportData() {
+    setSubType(PDActionImportData.SUB_TYPE);
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param a The action dictionary.
-     */
-    public PDActionImportData(COSDictionary a)
-    {
-        super(a);
-    }
+  /**
+   * Constructor.
+   *
+   * @param a The action dictionary.
+   */
+  public PDActionImportData(final COSDictionary a) {
+    super(a);
+  }
 
-    /**
-     * This will get the file in which the destination is located.
-     *
-     * @return The F entry of the specific Submit-From action dictionary.
-     * @throws IOException If there is an error creating the file spec.
-     */
-    public PDFileSpecification getFile() throws IOException
-    {
-        return PDFileSpecification.createFS(action.getDictionaryObject(COSName.F));
-    }
+  /**
+   * This will get the file in which the destination is located.
+   *
+   * @return The F entry of the specific Submit-From action dictionary.
+   * @throws IOException If there is an error creating the file spec.
+   */
+  public PDFileSpecification getFile() throws IOException {
+    return PDFileSpecification.createFS(action.getDictionaryObject(COSName.F));
+  }
 
-    /**
-     * This will set the file in which the destination is located.
-     *
-     * @param fs The file specification.
-     */
-    public void setFile(PDFileSpecification fs)
-    {
-        action.setItem(COSName.F, fs);
-    }
+  /**
+   * This will set the file in which the destination is located.
+   *
+   * @param fs The file specification.
+   */
+  public void setFile(final PDFileSpecification fs) {
+    action.setItem(COSName.F, fs);
+  }
 
 }
