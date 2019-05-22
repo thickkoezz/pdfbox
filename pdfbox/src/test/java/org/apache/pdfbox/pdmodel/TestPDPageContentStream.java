ec1d280cc9ed53da223a06ae42289e9f32c4df76
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:42:13 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/TestPDPageContentStream.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/TestPDPageContentStream.java
@@ -18,102 +18,96 @@ package org.apache.pdfbox.pdmodel;
 
 import java.io.IOException;
 import java.util.List;
-import junit.framework.TestCase;
+
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.cos.COSFloat;
 import org.apache.pdfbox.pdfparser.PDFStreamParser;
 import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
 
+import junit.framework.TestCase;
+
 /**
  * @author Yegor Kozlov
  */
-public class TestPDPageContentStream extends TestCase
-{
-    public void testSetCmykColors() throws IOException
-    {
-        try (PDDocument doc = new PDDocument())
-        {
-            PDPage page = new PDPage();
-            doc.addPage(page);
+public class TestPDPageContentStream extends TestCase {
+  public void testSetCmykColors() throws IOException {
+    try (PDDocument doc = new PDDocument()) {
+      PDPage page = new PDPage();
+      doc.addPage(page);
 
-            try (PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.OVERWRITE, true))
-            {
-                // pass a non-stroking color in CMYK color space
-                contentStream.setNonStrokingColor(0.1f, 0.2f, 0.3f, 0.4f);
-            }
+      try (PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.OVERWRITE, true)) {
+        // pass a non-stroking color in CMYK color space
+        contentStream.setNonStrokingColor(0.1f, 0.2f, 0.3f, 0.4f);
+      }
 
-            // now read the PDF stream and verify that the CMYK values are correct
-            PDFStreamParser parser = new PDFStreamParser(page.getContents());
-            parser.parse();
-            java.util.List<Object>  pageTokens = parser.getTokens();
-            // expected five tokens :
-            // [0] = COSFloat{0.1}
-            // [1] = COSFloat{0.2}
-            // [2] = COSFloat{0.3}
-            // [3] = COSFloat{0.4}
-            // [4] = PDFOperator{"k"}
-            assertEquals(0.1f, ((COSFloat)pageTokens.get(0)).floatValue());
-            assertEquals(0.2f, ((COSFloat)pageTokens.get(1)).floatValue());
-            assertEquals(0.3f, ((COSFloat)pageTokens.get(2)).floatValue());
-            assertEquals(0.4f, ((COSFloat)pageTokens.get(3)).floatValue());
-            assertEquals("k", ((Operator) pageTokens.get(4)).getName());
+      // now read the PDF stream and verify that the CMYK values are correct
+      PDFStreamParser parser = new PDFStreamParser(page.getContents());
+      parser.parse();
+      java.util.List<Object> pageTokens = parser.getTokens();
+      // expected five tokens :
+      // [0] = COSFloat{0.1}
+      // [1] = COSFloat{0.2}
+      // [2] = COSFloat{0.3}
+      // [3] = COSFloat{0.4}
+      // [4] = PDFOperator{"k"}
+      TestCase.assertEquals(0.1f, ((COSFloat) pageTokens.get(0)).floatValue());
+      TestCase.assertEquals(0.2f, ((COSFloat) pageTokens.get(1)).floatValue());
+      TestCase.assertEquals(0.3f, ((COSFloat) pageTokens.get(2)).floatValue());
+      TestCase.assertEquals(0.4f, ((COSFloat) pageTokens.get(3)).floatValue());
+      TestCase.assertEquals("k", ((Operator) pageTokens.get(4)).getName());
 
-            // same as above but for PDPageContentStream#setStrokingColor
-            page = new PDPage();
-            doc.addPage(page);
+      // same as above but for PDPageContentStream#setStrokingColor
+      page = new PDPage();
+      doc.addPage(page);
 
-            try ( PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.OVERWRITE, false))
-            {
-                // pass a non-stroking color in CMYK color space
-                contentStream.setStrokingColor(0.5f, 0.6f, 0.7f, 0.8f);
-            }
+      try (PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.OVERWRITE, false)) {
+        // pass a non-stroking color in CMYK color space
+        contentStream.setStrokingColor(0.5f, 0.6f, 0.7f, 0.8f);
+      }
 
-            // now read the PDF stream and verify that the CMYK values are correct
-            parser = new PDFStreamParser(page.getContents());
-            parser.parse();
-            pageTokens = parser.getTokens();
-            // expected five tokens  :
-            // [0] = COSFloat{0.5}
-            // [1] = COSFloat{0.6}
-            // [2] = COSFloat{0.7}
-            // [3] = COSFloat{0.8}
-            // [4] = PDFOperator{"K"}
-            assertEquals(0.5f, ((COSFloat)pageTokens.get(0)).floatValue());
-            assertEquals(0.6f, ((COSFloat)pageTokens.get(1)).floatValue());
-            assertEquals(0.7f, ((COSFloat)pageTokens.get(2)).floatValue());
-            assertEquals(0.8f, ((COSFloat)pageTokens.get(3)).floatValue());
-            assertEquals("K", ((Operator)pageTokens.get(4)).getName());
-        }
+      // now read the PDF stream and verify that the CMYK values are correct
+      parser = new PDFStreamParser(page.getContents());
+      parser.parse();
+      pageTokens = parser.getTokens();
+      // expected five tokens :
+      // [0] = COSFloat{0.5}
+      // [1] = COSFloat{0.6}
+      // [2] = COSFloat{0.7}
+      // [3] = COSFloat{0.8}
+      // [4] = PDFOperator{"K"}
+      TestCase.assertEquals(0.5f, ((COSFloat) pageTokens.get(0)).floatValue());
+      TestCase.assertEquals(0.6f, ((COSFloat) pageTokens.get(1)).floatValue());
+      TestCase.assertEquals(0.7f, ((COSFloat) pageTokens.get(2)).floatValue());
+      TestCase.assertEquals(0.8f, ((COSFloat) pageTokens.get(3)).floatValue());
+      TestCase.assertEquals("K", ((Operator) pageTokens.get(4)).getName());
     }
+  }
 
-    /**
-     * PDFBOX-3510: missing content stream should not fail.
-     * 
-     * @throws IOException 
-     */
-    public void testMissingContentStream() throws IOException
-    {
-        PDPage page = new PDPage();
-        PDFStreamParser parser = new PDFStreamParser(page.getContents());
-        parser.parse();
-        List<Object> tokens = parser.getTokens();
-        assertEquals(0, tokens.size());
-    }
+  /**
+   * PDFBOX-3510: missing content stream should not fail.
+   * 
+   * @throws IOException
+   */
+  public void testMissingContentStream() throws IOException {
+    final PDPage page = new PDPage();
+    final PDFStreamParser parser = new PDFStreamParser(page.getContents());
+    parser.parse();
+    final List<Object> tokens = parser.getTokens();
+    TestCase.assertEquals(0, tokens.size());
+  }
 
-    /**
-     * Check that close() can be called twice.
-     *
-     * @throws IOException 
-     */
-    public void testCloseContract() throws IOException
-    {
-        try (PDDocument doc = new PDDocument())
-        {
-            PDPage page = new PDPage();
-            doc.addPage(page);
-            PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.OVERWRITE, true);
-            contentStream.close();
-            contentStream.close();
-        }
+  /**
+   * Check that close() can be called twice.
+   *
+   * @throws IOException
+   */
+  public void testCloseContract() throws IOException {
+    try (PDDocument doc = new PDDocument()) {
+      final PDPage page = new PDPage();
+      doc.addPage(page);
+      final PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.OVERWRITE, true);
+      contentStream.close();
+      contentStream.close();
     }
+  }
 }
