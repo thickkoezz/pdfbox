d90459f00e98d3247b8ee79c4d33014b68e663b0
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:59:53 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/pagenavigation/PDTransitionTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/pagenavigation/PDTransitionTest.java
@@ -16,106 +16,92 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.pagenavigation;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSInteger;
 import org.apache.pdfbox.cos.COSName;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * @author Andrea Vacondio
  *
  */
-public class PDTransitionTest
-{
+public class PDTransitionTest {
 
-    @Test
-    public void defaultStyle()
-    {
-        PDTransition transition = new PDTransition();
-        assertEquals(COSName.TRANS, transition.getCOSObject().getCOSName(COSName.TYPE));
-        assertEquals(PDTransitionStyle.R.name(), transition.getStyle());
-    }
+  @Test
+  public void defaultStyle() {
+    final PDTransition transition = new PDTransition();
+    Assert.assertEquals(COSName.TRANS, transition.getCOSObject().getCOSName(COSName.TYPE));
+    Assert.assertEquals(PDTransitionStyle.R.name(), transition.getStyle());
+  }
 
-    @Test
-    public void getStyle()
-    {
-        PDTransition transition = new PDTransition(PDTransitionStyle.Fade);
-        assertEquals(COSName.TRANS, transition.getCOSObject().getCOSName(COSName.TYPE));
-        assertEquals(PDTransitionStyle.Fade.name(), transition.getStyle());
-    }
+  @Test
+  public void getStyle() {
+    final PDTransition transition = new PDTransition(PDTransitionStyle.Fade);
+    Assert.assertEquals(COSName.TRANS, transition.getCOSObject().getCOSName(COSName.TYPE));
+    Assert.assertEquals(PDTransitionStyle.Fade.name(), transition.getStyle());
+  }
 
-    @Test
-    public void defaultValues()
-    {
-        PDTransition transition = new PDTransition(new COSDictionary());
-        assertEquals(PDTransitionStyle.R.name(), transition.getStyle());
-        assertEquals(PDTransitionDimension.H.name(), transition.getDimension());
-        assertEquals(PDTransitionMotion.I.name(), transition.getMotion());
-        assertEquals(COSInteger.ZERO, transition.getDirection());
-        assertEquals(1, transition.getDuration(), 0);
-        assertEquals(1, transition.getFlyScale(), 0);
-        assertFalse(transition.isFlyAreaOpaque());
-    }
+  @Test
+  public void defaultValues() {
+    final PDTransition transition = new PDTransition(new COSDictionary());
+    Assert.assertEquals(PDTransitionStyle.R.name(), transition.getStyle());
+    Assert.assertEquals(PDTransitionDimension.H.name(), transition.getDimension());
+    Assert.assertEquals(PDTransitionMotion.I.name(), transition.getMotion());
+    Assert.assertEquals(COSInteger.ZERO, transition.getDirection());
+    Assert.assertEquals(1, transition.getDuration(), 0);
+    Assert.assertEquals(1, transition.getFlyScale(), 0);
+    Assert.assertFalse(transition.isFlyAreaOpaque());
+  }
 
-    @Test
-    public void dimension()
-    {
-        PDTransition transition = new PDTransition();
-        transition.setDimension(PDTransitionDimension.H);
-        assertEquals(PDTransitionDimension.H.name(), transition.getDimension());
-    }
+  @Test
+  public void dimension() {
+    final PDTransition transition = new PDTransition();
+    transition.setDimension(PDTransitionDimension.H);
+    Assert.assertEquals(PDTransitionDimension.H.name(), transition.getDimension());
+  }
 
-    @Test
-    public void directionNone()
-    {
-        PDTransition transition = new PDTransition();
-        transition.setDirection(PDTransitionDirection.NONE);
-        assertEquals(COSName.class.getName(), transition.getDirection().getClass().getName());
-        assertEquals(COSName.NONE, transition.getDirection());
-    }
+  @Test
+  public void directionNone() {
+    final PDTransition transition = new PDTransition();
+    transition.setDirection(PDTransitionDirection.NONE);
+    Assert.assertEquals(COSName.class.getName(), transition.getDirection().getClass().getName());
+    Assert.assertEquals(COSName.NONE, transition.getDirection());
+  }
 
-    @Test
-    public void directionNumber()
-    {
-        PDTransition transition = new PDTransition();
-        transition.setDirection(PDTransitionDirection.LEFT_TO_RIGHT);
-        assertEquals(COSInteger.class.getName(), transition.getDirection().getClass().getName());
-        assertEquals(COSInteger.ZERO, transition.getDirection());
-    }
+  @Test
+  public void directionNumber() {
+    final PDTransition transition = new PDTransition();
+    transition.setDirection(PDTransitionDirection.LEFT_TO_RIGHT);
+    Assert.assertEquals(COSInteger.class.getName(), transition.getDirection().getClass().getName());
+    Assert.assertEquals(COSInteger.ZERO, transition.getDirection());
+  }
 
-    @Test
-    public void motion()
-    {
-        PDTransition transition = new PDTransition();
-        transition.setMotion(PDTransitionMotion.O);
-        assertEquals(PDTransitionMotion.O.name(), transition.getMotion());
-    }
+  @Test
+  public void motion() {
+    final PDTransition transition = new PDTransition();
+    transition.setMotion(PDTransitionMotion.O);
+    Assert.assertEquals(PDTransitionMotion.O.name(), transition.getMotion());
+  }
 
-    @Test
-    public void duration()
-    {
-        PDTransition transition = new PDTransition();
-        transition.setDuration(4);
-        assertEquals(4, transition.getDuration(), 0);
-    }
+  @Test
+  public void duration() {
+    final PDTransition transition = new PDTransition();
+    transition.setDuration(4);
+    Assert.assertEquals(4, transition.getDuration(), 0);
+  }
 
-    @Test
-    public void flyScale()
-    {
-        PDTransition transition = new PDTransition();
-        transition.setFlyScale(4);
-        assertEquals(4, transition.getFlyScale(), 0);
-    }
+  @Test
+  public void flyScale() {
+    final PDTransition transition = new PDTransition();
+    transition.setFlyScale(4);
+    Assert.assertEquals(4, transition.getFlyScale(), 0);
+  }
 
-    @Test
-    public void flyArea()
-    {
-        PDTransition transition = new PDTransition();
-        transition.setFlyAreaOpaque(true);
-        assertTrue(transition.isFlyAreaOpaque());
-    }
+  @Test
+  public void flyArea() {
+    final PDTransition transition = new PDTransition();
+    transition.setFlyAreaOpaque(true);
+    Assert.assertTrue(transition.isFlyAreaOpaque());
+  }
 }
