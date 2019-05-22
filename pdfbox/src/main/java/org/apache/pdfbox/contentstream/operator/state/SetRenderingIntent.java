8c3fa7647d8ebce9f7141f363abb32a374771eb8
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:35:22 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetRenderingIntent.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetRenderingIntent.java
@@ -19,12 +19,13 @@ package org.apache.pdfbox.contentstream.operator.state;
 
 import java.io.IOException;
 import java.util.List;
+
 import org.apache.pdfbox.contentstream.operator.MissingOperandException;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.graphics.state.RenderingIntent;
 
 /**
@@ -32,27 +33,19 @@ import org.apache.pdfbox.pdmodel.graphics.state.RenderingIntent;
  *
  * @author John Hewson
  */
-public class SetRenderingIntent extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> operands) throws IOException
-    {
-        if (operands.isEmpty())
-        {
-            throw new MissingOperandException(operator, operands);
-        }
-        COSBase base = operands.get(0);
-        if (!(base instanceof COSName))
-        {
-            return;
-        }
-        context.getGraphicsState().setRenderingIntent(
-                RenderingIntent.fromString(((COSName)base).getName()));
-    }
+public class SetRenderingIntent extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> operands) throws IOException {
+    if (operands.isEmpty())
+      throw new MissingOperandException(operator, operands);
+    final COSBase base = operands.get(0);
+    if (!(base instanceof COSName))
+      return;
+    context.getGraphicsState().setRenderingIntent(RenderingIntent.fromString(((COSName) base).getName()));
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_RENDERINGINTENT;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.SET_RENDERINGINTENT;
+  }
 }
