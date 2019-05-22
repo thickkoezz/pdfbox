3ea6a56e062859ce9d04ca4b44ee5ad8ab3a38b0
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:52:58 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/PDSeedValueMDP.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/PDSeedValueMDP.java
@@ -20,66 +20,58 @@ import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 
 /**
- * This MDP dictionary is a part of the seed value dictionary and define
- * if a author signature or a certification signature should be use.
+ * This MDP dictionary is a part of the seed value dictionary and define if a
+ * author signature or a certification signature should be use.
  *
  * @author Thomas Chojecki
  */
-public class PDSeedValueMDP
-{
-    private final COSDictionary dictionary;
+public class PDSeedValueMDP {
+  private final COSDictionary dictionary;
 
-    /**
-     * Default constructor.
-     */
-    public PDSeedValueMDP()
-    {
-        dictionary = new COSDictionary();
-        dictionary.setDirect(true);
-    }
+  /**
+   * Default constructor.
+   */
+  public PDSeedValueMDP() {
+    dictionary = new COSDictionary();
+    dictionary.setDirect(true);
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param dict The signature dictionary.
-     */
-    public PDSeedValueMDP(COSDictionary dict)
-    {
-        dictionary = dict;
-        dictionary.setDirect(true);
-    }
+  /**
+   * Constructor.
+   *
+   * @param dict The signature dictionary.
+   */
+  public PDSeedValueMDP(final COSDictionary dict) {
+    dictionary = dict;
+    dictionary.setDirect(true);
+  }
 
-    /**
-     * Convert this standard java object to a COS dictionary.
-     *
-     * @return The COS dictionary that matches this Java object.
-     */
-    public COSDictionary getCOSObject()
-    {
-        return dictionary;
-    }
+  /**
+   * Convert this standard java object to a COS dictionary.
+   *
+   * @return The COS dictionary that matches this Java object.
+   */
+  public COSDictionary getCOSObject() {
+    return dictionary;
+  }
 
-    /**
-     * Return the P value.
-     * 
-     * @return the P value
-     */
-    public int getP()
-    {
-        return dictionary.getInt(COSName.P);
-    }
+  /**
+   * Return the P value.
+   *
+   * @return the P value
+   */
+  public int getP() {
+    return dictionary.getInt(COSName.P);
+  }
 
-    /**
-     * Set the P value.
-     * 
-     * @param p the value to be set as P
-     */
-    public void setP(int p)
-    {
-        if (p < 0 || p > 3)
-        {
-            throw new IllegalArgumentException("Only values between 0 and 3 nare allowed.");
-        }
-        dictionary.setInt(COSName.P, p);
-    }
+  /**
+   * Set the P value.
+   *
+   * @param p the value to be set as P
+   */
+  public void setP(final int p) {
+    if (p < 0 || p > 3)
+      throw new IllegalArgumentException("Only values between 0 and 3 nare allowed.");
+    dictionary.setInt(COSName.P, p);
+  }
 }
