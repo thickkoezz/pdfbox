690287a6554fe17fc92965547aa29ac5eedaa651
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:38:34 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/cos/TestCOSNumber.java
+++ pdfbox/src/test/java/org/apache/pdfbox/cos/TestCOSNumber.java
@@ -19,70 +19,64 @@ package org.apache.pdfbox.cos;
 
 import java.io.IOException;
 
+import junit.framework.TestCase;
+
 /**
  * Test class for {@link COSNumber}
  */
-public abstract class TestCOSNumber extends TestCOSBase
-{
-    /**
-     * Test floatValue() - test that the correct float value is returned.
-     */
-    public abstract void testFloatValue();
+public abstract class TestCOSNumber extends TestCOSBase {
+  /**
+   * Test floatValue() - test that the correct float value is returned.
+   */
+  public abstract void testFloatValue();
 
-    /**
-     * Test doubleValue() - test that the correct double value is returned.
-     */
-    public abstract void testDoubleValue();
+  /**
+   * Test doubleValue() - test that the correct double value is returned.
+   */
+  public abstract void testDoubleValue();
 
-    /**
-     * Test intValue() - test that the correct int value is returned.
-     */
-    public abstract void testIntValue();
+  /**
+   * Test intValue() - test that the correct int value is returned.
+   */
+  public abstract void testIntValue();
 
-    /**
-     * Test longValue() - test that the correct long value is returned.
-     */
-    public abstract void testLongValue();
+  /**
+   * Test longValue() - test that the correct long value is returned.
+   */
+  public abstract void testLongValue();
 
-    /**
-     * Tests get() - tests a static constructor for COSNumber classes.
-     */
-    public void testGet()
-    {
-        try
-        {
-            // Ensure the basic static numbers are recognized
-            assertEquals(COSInteger.ZERO, COSNumber.get("0"));
-            assertEquals(COSInteger.ONE, COSNumber.get("1"));
-            assertEquals(COSInteger.TWO, COSNumber.get("2"));
-            assertEquals(COSInteger.THREE, COSNumber.get("3"));
-            // Test some arbitrary ints
-            assertEquals(COSInteger.get(100), COSNumber.get("100"));
-            assertEquals(COSInteger.get(256), COSNumber.get("256"));
-            assertEquals(COSInteger.get(-1000), COSNumber.get("-1000"));
-            assertEquals(COSInteger.get(2000), COSNumber.get("+2000"));
-            // Some arbitrary floats
-            assertEquals(new COSFloat(1.1f), COSNumber.get("1.1"));
-            assertEquals(new COSFloat(100f), COSNumber.get("100.0"));
-            assertEquals(new COSFloat(-100.001f), COSNumber.get("-100.001"));
-            // according to the specs the exponential shall not be used
-            // but obviously there some
-            assertNotNull(COSNumber.get("-2e-006"));
-            assertNotNull(COSNumber.get("-8e+05"));
-            try
-            {
-                assertEquals("Null Value...", COSNumber.get(null));
-                fail("Failed to throw a NullPointerException");
-            }
-            catch (NullPointerException e)
-            {
-                // PASS
-            }
+  /**
+   * Tests get() - tests a static constructor for COSNumber classes.
+   */
+  public void testGet() {
+    try {
+      // Ensure the basic static numbers are recognized
+      TestCase.assertEquals(COSInteger.ZERO, COSNumber.get("0"));
+      TestCase.assertEquals(COSInteger.ONE, COSNumber.get("1"));
+      TestCase.assertEquals(COSInteger.TWO, COSNumber.get("2"));
+      TestCase.assertEquals(COSInteger.THREE, COSNumber.get("3"));
+      // Test some arbitrary ints
+      TestCase.assertEquals(COSInteger.get(100), COSNumber.get("100"));
+      TestCase.assertEquals(COSInteger.get(256), COSNumber.get("256"));
+      TestCase.assertEquals(COSInteger.get(-1000), COSNumber.get("-1000"));
+      TestCase.assertEquals(COSInteger.get(2000), COSNumber.get("+2000"));
+      // Some arbitrary floats
+      TestCase.assertEquals(new COSFloat(1.1f), COSNumber.get("1.1"));
+      TestCase.assertEquals(new COSFloat(100f), COSNumber.get("100.0"));
+      TestCase.assertEquals(new COSFloat(-100.001f), COSNumber.get("-100.001"));
+      // according to the specs the exponential shall not be used
+      // but obviously there some
+      TestCase.assertNotNull(COSNumber.get("-2e-006"));
+      TestCase.assertNotNull(COSNumber.get("-8e+05"));
+      try {
+        TestCase.assertEquals("Null Value...", COSNumber.get(null));
+        TestCase.fail("Failed to throw a NullPointerException");
+      } catch (final NullPointerException e) {
+        // PASS
+      }
 
-        }
-        catch (IOException e)
-        {
-            fail("Failed to convert a number " + e.getMessage());
-        }
+    } catch (final IOException e) {
+      TestCase.fail("Failed to convert a number " + e.getMessage());
     }
+  }
 }
