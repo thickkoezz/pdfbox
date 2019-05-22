5610560403776a56d4c850fe0d3aa3c14f1789ad
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:40:31 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/filter/TestFilters.java
+++ pdfbox/src/test/java/org/apache/pdfbox/filter/TestFilters.java
@@ -23,137 +23,110 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 
-import junit.framework.TestCase;
-
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
+import junit.framework.TestCase;
+
 /**
  * This will test all of the filters in the PDFBox system.
  */
-public class TestFilters extends TestCase
-{
-    /**
-     * This will test all of the filters in the system. There will be COUNT
-     * of deterministic tests and COUNT of non-deterministic tests, see also
-     * the discussion in PDFBOX-1977.
-     *
-     * @throws IOException If there is an exception while encoding.
-     */
-    public void testFilters() throws IOException
-    {
-        final int COUNT = 10;
-        Random rd = new Random(123456);
-        for (int iter = 0; iter < COUNT * 2; iter++)
-        {
-            long seed;
-            if (iter < COUNT)
-            {
-                // deterministic seed
-                seed = rd.nextLong();
-            }
-            else
-            {
-                // non-deterministic seed
-                seed = new Random().nextLong();
-            }
-            boolean success = false;
-            try 
-            {
-                final Random random = new Random(seed);
-                final int numBytes = 10000 + random.nextInt(20000);
-                byte[] original = new byte[numBytes];
+public class TestFilters extends TestCase {
+  /**
+   * This will test all of the filters in the system. There will be COUNT of
+   * deterministic tests and COUNT of non-deterministic tests, see also the
+   * discussion in PDFBOX-1977.
+   *
+   * @throws IOException If there is an exception while encoding.
+   */
+  public void testFilters() throws IOException {
+    final int COUNT = 10;
+    final Random rd = new Random(123456);
+    for (int iter = 0; iter < COUNT * 2; iter++) {
+      long seed;
+      if (iter < COUNT) {
+        // deterministic seed
+        seed = rd.nextLong();
+      } else {
+        // non-deterministic seed
+        seed = new Random().nextLong();
+      }
+      boolean success = false;
+      try {
+        final Random random = new Random(seed);
+        final int numBytes = 10000 + random.nextInt(20000);
+        final byte[] original = new byte[numBytes];
 
-                int upto = 0;
-                while(upto < numBytes) 
-                {
-                    final int left = numBytes - upto;
-                    if (random.nextBoolean() || left < 2) 
-                    {
-                        // Fill w/ pseudo-random bytes:
-                        final int end = upto + Math.min(left, 10+random.nextInt(100));
-                        while(upto < end) 
-                        {
-                            original[upto++] = (byte) random.nextInt();
-                        }
-                    } 
-                    else 
-                    {
-                        // Fill w/ very predictable bytes:
-                        final int end = upto + Math.min(left, 2+random.nextInt(10));
-                        final byte value = (byte) random.nextInt(4);
-                        while(upto < end) 
-                        {
-                            original[upto++] = value;
-                        }
-                    }
-                }
-
-                for( Filter filter : FilterFactory.INSTANCE.getAllFilters() )
-                {
-                    // Skip filters that don't currently support roundtripping
-                    if( filter instanceof DCTFilter ||
-                        filter instanceof CCITTFaxFilter ||
-                        filter instanceof JPXFilter ||
-                        filter instanceof JBIG2Filter ||
-                        filter instanceof RunLengthDecodeFilter )
-                        {
-                            continue;
-                        }
-
-                    checkEncodeDecode(filter, original);
-                }
-                success = true;
-            } 
-            finally 
-            {
-                if (!success) 
-                {
-                    System.err.println("NOTE: test failed with seed=" + seed);
-                }
+        int upto = 0;
+        while (upto < numBytes) {
+          final int left = numBytes - upto;
+          if (random.nextBoolean() || left < 2) {
+            // Fill w/ pseudo-random bytes:
+            final int end = upto + Math.min(left, 10 + random.nextInt(100));
+            while (upto < end) {
+              original[upto++] = (byte) random.nextInt();
             }
+          } else {
+            // Fill w/ very predictable bytes:
+            final int end = upto + Math.min(left, 2 + random.nextInt(10));
+            final byte value = (byte) random.nextInt(4);
+            while (upto < end) {
+              original[upto++] = value;
+            }
+          }
         }
-    }
-    
-    /**
-     * This will test the use of identity filter to decode stream and string.
-     * This test threw an IOException before the correction.
-     * 
-     * @throws IOException
-     */
-    public void testPDFBOX4517() throws IOException
-    {
-        PDDocument.load(new File("target/pdfs/PDFBOX-4517-cryptfilter.pdf"),
-                "userpassword1234");
-    }
 
-    /**
-     * This will test the LZW filter with the sequence that failed in PDFBOX-1777.
-     * To check that the test itself is legit, revert LZWFilter.java to rev 1571801,
-     * which should fail this test.
-     * 
-     * @throws IOException 
-     */
-    public void testPDFBOX1777() throws IOException
-    {
-        Filter lzwFilter = FilterFactory.INSTANCE.getFilter(COSName.LZW_DECODE);
-        byte[] byteArray = IOUtils.toByteArray(this.getClass().getResourceAsStream("PDFBOX-1777.bin"));
-        checkEncodeDecode(lzwFilter, byteArray);
-    }
+        for (final Filter filter : FilterFactory.INSTANCE.getAllFilters()) {
+          // Skip filters that don't currently support roundtripping
+          if (filter instanceof DCTFilter || filter instanceof CCITTFaxFilter || filter instanceof JPXFilter
+              || filter instanceof JBIG2Filter || filter instanceof RunLengthDecodeFilter) {
+            continue;
+          }
 
-    private void checkEncodeDecode(Filter filter, byte[] original) throws IOException
-    {
-        ByteArrayOutputStream encoded = new ByteArrayOutputStream();
-        filter.encode(new ByteArrayInputStream(original), encoded, new COSDictionary());
-        ByteArrayOutputStream decoded = new ByteArrayOutputStream();
-        filter.decode(new ByteArrayInputStream(encoded.toByteArray()),
-                decoded, new COSDictionary(), 0);
-
-        assertTrue(
-                "Data that is encoded and then decoded through "
-                + filter.getClass() + " does not match the original data",
-                Arrays.equals(original, decoded.toByteArray()));
+          checkEncodeDecode(filter, original);
+        }
+        success = true;
+      } finally {
+        if (!success) {
+          System.err.println("NOTE: test failed with seed=" + seed);
+        }
+      }
     }
+  }
+
+  /**
+   * This will test the use of identity filter to decode stream and string. This
+   * test threw an IOException before the correction.
+   * 
+   * @throws IOException
+   */
+  public void testPDFBOX4517() throws IOException {
+    PDDocument.load(new File("target/pdfs/PDFBOX-4517-cryptfilter.pdf"), "userpassword1234");
+  }
+
+  /**
+   * This will test the LZW filter with the sequence that failed in PDFBOX-1777.
+   * To check that the test itself is legit, revert LZWFilter.java to rev 1571801,
+   * which should fail this test.
+   * 
+   * @throws IOException
+   */
+  public void testPDFBOX1777() throws IOException {
+    final Filter lzwFilter = FilterFactory.INSTANCE.getFilter(COSName.LZW_DECODE);
+    final byte[] byteArray = IOUtils.toByteArray(this.getClass().getResourceAsStream("PDFBOX-1777.bin"));
+    checkEncodeDecode(lzwFilter, byteArray);
+  }
+
+  private void checkEncodeDecode(final Filter filter, final byte[] original) throws IOException {
+    final ByteArrayOutputStream encoded = new ByteArrayOutputStream();
+    filter.encode(new ByteArrayInputStream(original), encoded, new COSDictionary());
+    final ByteArrayOutputStream decoded = new ByteArrayOutputStream();
+    filter.decode(new ByteArrayInputStream(encoded.toByteArray()), decoded, new COSDictionary(), 0);
+
+    TestCase.assertTrue(
+        "Data that is encoded and then decoded through " + filter.getClass() + " does not match the original data",
+        Arrays.equals(original, decoded.toByteArray()));
+  }
 }
