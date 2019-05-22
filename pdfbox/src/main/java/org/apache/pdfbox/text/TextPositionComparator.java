8e76ef3b7faf4482d6f9edb3585fb8036142eacb
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:43:12 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/text/TextPositionComparator.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/TextPositionComparator.java
@@ -19,52 +19,41 @@ package org.apache.pdfbox.text;
 import java.util.Comparator;
 
 /**
- * This class is a comparator for TextPosition operators.  It handles
- * pages with text in different directions by grouping the text based
- * on direction and sorting in that direction. This allows continuous text
- * in a given direction to be more easily grouped together.  
+ * This class is a comparator for TextPosition operators. It handles pages with
+ * text in different directions by grouping the text based on direction and
+ * sorting in that direction. This allows continuous text in a given direction
+ * to be more easily grouped together.
  *
  * @author Ben Litchfield
  */
-public class TextPositionComparator implements Comparator<TextPosition>
-{
-    @Override
-    public int compare(TextPosition pos1, TextPosition pos2)
-    {
-        // only compare text that is in the same direction
-        int cmp1 = Float.compare(pos1.getDir(), pos2.getDir());
-        if (cmp1 != 0)
-        {
-            return cmp1;
-        }
-        
-        // get the text direction adjusted coordinates
-        float x1 = pos1.getXDirAdj();
-        float x2 = pos2.getXDirAdj();
-        
-        float pos1YBottom = pos1.getYDirAdj();
-        float pos2YBottom = pos2.getYDirAdj();
+public class TextPositionComparator implements Comparator<TextPosition> {
+  @Override
+  public int compare(final TextPosition pos1, final TextPosition pos2) {
+    // only compare text that is in the same direction
+    final int cmp1 = Float.compare(pos1.getDir(), pos2.getDir());
+    if (cmp1 != 0)
+      return cmp1;
 
-        // note that the coordinates have been adjusted so 0,0 is in upper left
-        float pos1YTop = pos1YBottom - pos1.getHeightDir();
-        float pos2YTop = pos2YBottom - pos2.getHeightDir();
+    // get the text direction adjusted coordinates
+    final float x1 = pos1.getXDirAdj();
+    final float x2 = pos2.getXDirAdj();
 
-        float yDifference = Math.abs(pos1YBottom - pos2YBottom);
+    final float pos1YBottom = pos1.getYDirAdj();
+    final float pos2YBottom = pos2.getYDirAdj();
 
-        // we will do a simple tolerance comparison
-        if (yDifference < .1 ||
-            pos2YBottom >= pos1YTop && pos2YBottom <= pos1YBottom ||
-            pos1YBottom >= pos2YTop && pos1YBottom <= pos2YBottom)
-        {
-            return Float.compare(x1, x2);
-        }
-        else if (pos1YBottom < pos2YBottom)
-        {
-            return -1;
-        }
-        else
-        {
-            return 1;
-        }
-    }
+    // note that the coordinates have been adjusted so 0,0 is in upper left
+    final float pos1YTop = pos1YBottom - pos1.getHeightDir();
+    final float pos2YTop = pos2YBottom - pos2.getHeightDir();
+
+    final float yDifference = Math.abs(pos1YBottom - pos2YBottom);
+
+    // we will do a simple tolerance comparison
+    if (yDifference < .1 || pos2YBottom >= pos1YTop && pos2YBottom <= pos1YBottom
+        || pos1YBottom >= pos2YTop && pos1YBottom <= pos2YBottom)
+      return Float.compare(x1, x2);
+    else if (pos1YBottom < pos2YBottom)
+      return -1;
+    else
+      return 1;
+  }
 }
