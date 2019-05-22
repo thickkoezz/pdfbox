7fa6f2d85ec235c66a2a4db489b15f32a1e346a3
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:05:38 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/shading/RadialShadingPaint.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/shading/RadialShadingPaint.java
@@ -33,39 +33,33 @@ import org.apache.pdfbox.util.Matrix;
  * AWT Paint for radial shading.
  *
  */
-public class RadialShadingPaint extends ShadingPaint<PDShadingType3>
-{
-    private static final Log LOG = LogFactory.getLog(RadialShadingPaint.class);
+public class RadialShadingPaint extends ShadingPaint<PDShadingType3> {
+  private static final Log LOG = LogFactory.getLog(RadialShadingPaint.class);
 
-    /**
-     * Constructor.
-     *
-     * @param shading the shading resources
-     * @param matrix the pattern matrix concatenated with that of the parent content stream
-     */
-    RadialShadingPaint(PDShadingType3 shading, Matrix matrix)
-    {
-        super(shading, matrix);
-    }
+  /**
+   * Constructor.
+   *
+   * @param shading the shading resources
+   * @param matrix  the pattern matrix concatenated with that of the parent
+   *                content stream
+   */
+  RadialShadingPaint(final PDShadingType3 shading, final Matrix matrix) {
+    super(shading, matrix);
+  }
 
-    @Override
-    public int getTransparency()
-    {
-        return 0;
-    }
+  @Override
+  public int getTransparency() {
+    return 0;
+  }
 
-    @Override
-    public PaintContext createContext(ColorModel cm, Rectangle deviceBounds, Rectangle2D userBounds,
-                                      AffineTransform xform, RenderingHints hints)
-    {
-        try
-        {
-            return new RadialShadingContext(shading, cm, xform, matrix, deviceBounds);
-        }
-        catch (IOException e)
-        {
-            LOG.error("An error occurred while painting", e);
-            return new Color(0, 0, 0, 0).createContext(cm, deviceBounds, userBounds, xform, hints);
-        }
+  @Override
+  public PaintContext createContext(final ColorModel cm, final Rectangle deviceBounds, final Rectangle2D userBounds,
+      final AffineTransform xform, final RenderingHints hints) {
+    try {
+      return new RadialShadingContext(shading, cm, xform, matrix, deviceBounds);
+    } catch (final IOException e) {
+      RadialShadingPaint.LOG.error("An error occurred while painting", e);
+      return new Color(0, 0, 0, 0).createContext(cm, deviceBounds, userBounds, xform, hints);
     }
+  }
 }
