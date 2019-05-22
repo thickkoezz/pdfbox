f90a9059bc75dc8dd6c81a31e65f453f69ccca4c
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:54:47 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFJavaScript.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFJavaScript.java
@@ -18,6 +18,7 @@ package org.apache.pdfbox.pdmodel.fdf;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
@@ -29,141 +30,116 @@ import org.apache.pdfbox.pdmodel.interactive.action.PDActionFactory;
 import org.apache.pdfbox.pdmodel.interactive.action.PDActionJavaScript;
 
 /**
- * This represents an FDF JavaScript dictionary that is part of the FDF document.
+ * This represents an FDF JavaScript dictionary that is part of the FDF
+ * document.
  *
  * @author Ben Litchfield
  */
-public class FDFJavaScript implements COSObjectable
-{
-    private final COSDictionary dictionary;
+public class FDFJavaScript implements COSObjectable {
+  private final COSDictionary dictionary;
 
-    /**
-     * Default constructor.
-     */
-    public FDFJavaScript()
-    {
-        dictionary = new COSDictionary();
-    }
+  /**
+   * Default constructor.
+   */
+  public FDFJavaScript() {
+    dictionary = new COSDictionary();
+  }
 
-    /**
-     * Constructor.
-     *
-     * @param javaScript The FDF java script.
-     */
-    public FDFJavaScript(COSDictionary javaScript)
-    {
-        dictionary = javaScript;
-    }
+  /**
+   * Constructor.
+   *
+   * @param javaScript The FDF java script.
+   */
+  public FDFJavaScript(final COSDictionary javaScript) {
+    dictionary = javaScript;
+  }
 
-    /**
-     * Convert this standard java object to a COS object.
-     *
-     * @return The cos object that matches this Java object.
-     */
-    @Override
-    public COSDictionary getCOSObject()
-    {
-        return dictionary;
-    }
+  /**
+   * Convert this standard java object to a COS object.
+   *
+   * @return The cos object that matches this Java object.
+   */
+  @Override
+  public COSDictionary getCOSObject() {
+    return dictionary;
+  }
 
-    /**
-     * This will get the javascript that is executed before the import.
-     *
-     * @return Some javascript code.
-     */
-    public String getBefore()
-    {
-        COSBase base = dictionary.getDictionaryObject(COSName.BEFORE);
-        if (base instanceof COSString)
-        {
-            return ((COSString) base).getString();
-        }
-        else if (base instanceof COSStream)
-        {
-            return ((COSStream) base).toTextString();
-        }
-        else
-        {
-            return null;
-        }
-    }
+  /**
+   * This will get the javascript that is executed before the import.
+   *
+   * @return Some javascript code.
+   */
+  public String getBefore() {
+    final COSBase base = dictionary.getDictionaryObject(COSName.BEFORE);
+    if (base instanceof COSString)
+      return ((COSString) base).getString();
+    else if (base instanceof COSStream)
+      return ((COSStream) base).toTextString();
+    else
+      return null;
+  }
 
-    /**
-     * This will set the javascript code the will get execute before the import.
-     *
-     * @param before A reference to some javascript code.
-     */
-    public void setBefore(String before)
-    {
-        dictionary.setItem(COSName.BEFORE, new COSString(before));
-    }
+  /**
+   * This will set the javascript code the will get execute before the import.
+   *
+   * @param before A reference to some javascript code.
+   */
+  public void setBefore(final String before) {
+    dictionary.setItem(COSName.BEFORE, new COSString(before));
+  }
 
-    /**
-     * This will get the javascript that is executed after the import.
-     *
-     * @return Some javascript code.
-     */
-    public String getAfter()
-    {
-        COSBase base = dictionary.getDictionaryObject(COSName.AFTER);
-        if (base instanceof COSString)
-        {
-            return ((COSString) base).getString();
-        }
-        else if (base instanceof COSStream)
-        {
-            return ((COSStream) base).toTextString();
-        }
-        else
-        {
-            return null;
-        }
-    }
+  /**
+   * This will get the javascript that is executed after the import.
+   *
+   * @return Some javascript code.
+   */
+  public String getAfter() {
+    final COSBase base = dictionary.getDictionaryObject(COSName.AFTER);
+    if (base instanceof COSString)
+      return ((COSString) base).getString();
+    else if (base instanceof COSStream)
+      return ((COSStream) base).toTextString();
+    else
+      return null;
+  }
 
-    /**
-     * This will set the javascript code the will get execute after the import.
-     *
-     * @param after A reference to some javascript code.
-     */
-    public void setAfter(String after)
-    {
-        dictionary.setItem(COSName.AFTER, new COSString(after));
-    }
+  /**
+   * This will set the javascript code the will get execute after the import.
+   *
+   * @param after A reference to some javascript code.
+   */
+  public void setAfter(final String after) {
+    dictionary.setItem(COSName.AFTER, new COSString(after));
+  }
 
-    /**
-     * Returns the dictionary's "Doc" entry, that is, a map of key value pairs to be added to the document's JavaScript
-     * name tree.
-     * 
-     * @return Map of named "JavaScript" dictionaries.
-     */
-    public Map<String, PDActionJavaScript> getDoc()
-    {
-        Map<String, PDActionJavaScript> map = new LinkedHashMap<>();
-        COSArray array = (COSArray) dictionary.getDictionaryObject(COSName.DOC);
-        if (array == null)
-        {
-            return null;
-        }
-        for (int i = 0; i < array.size(); i++)
-        {
-            PDActionFactory.createAction((COSDictionary) array.getObject(i));
-        }
-        return map;
+  /**
+   * Returns the dictionary's "Doc" entry, that is, a map of key value pairs to be
+   * added to the document's JavaScript name tree.
+   *
+   * @return Map of named "JavaScript" dictionaries.
+   */
+  public Map<String, PDActionJavaScript> getDoc() {
+    final Map<String, PDActionJavaScript> map = new LinkedHashMap<>();
+    final COSArray array = (COSArray) dictionary.getDictionaryObject(COSName.DOC);
+    if (array == null)
+      return null;
+    for (int i = 0; i < array.size(); i++) {
+      PDActionFactory.createAction((COSDictionary) array.getObject(i));
     }
+    return map;
+  }
 
-    /**
-     * Sets the dictionary's "Doc" entry.
-     * 
-     * @param map Map of named "JavaScript" dictionaries.
-     */
-    public void setDoc(Map<String, PDActionJavaScript> map)
-    {
-        COSArray array = new COSArray();
-        for (Map.Entry<String, PDActionJavaScript> entry : map.entrySet())
-        {
-            array.add(new COSString(entry.getKey()));
-            array.add(entry.getValue());
-        }
-        dictionary.setItem(COSName.DOC, array);
+  /**
+   * Sets the dictionary's "Doc" entry.
+   *
+   * @param map Map of named "JavaScript" dictionaries.
+   */
+  public void setDoc(final Map<String, PDActionJavaScript> map) {
+    final COSArray array = new COSArray();
+    for (final Map.Entry<String, PDActionJavaScript> entry : map.entrySet()) {
+      array.add(new COSString(entry.getKey()));
+      array.add(entry.getValue());
     }
+    dictionary.setItem(COSName.DOC, array);
+  }
 }
