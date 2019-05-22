3f69ac85f3fccec8374e00b7ce8b5caf40f21b1c
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:18:37 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationPopup.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationPopup.java
@@ -20,91 +20,83 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 
 /**
- * This is the class that represents a popup annotation. Introduced in PDF 1.3 specification
+ * This is the class that represents a popup annotation. Introduced in PDF 1.3
+ * specification
  *
  * @author Paul King
  */
-public class PDAnnotationPopup extends PDAnnotation
-{
-    private static final Log LOG = LogFactory.getLog(PDAnnotationPopup.class);
+public class PDAnnotationPopup extends PDAnnotation {
+  private static final Log LOG = LogFactory.getLog(PDAnnotationPopup.class);
 
-    /**
-     * The type of annotation.
-     */
-    public static final String SUB_TYPE = "Popup";
+  /**
+   * The type of annotation.
+   */
+  public static final String SUB_TYPE = "Popup";
 
-    /**
-     * Constructor.
-     */
-    public PDAnnotationPopup()
-    {
-        getCOSObject().setName(COSName.SUBTYPE, SUB_TYPE);
+  /**
+   * Constructor.
+   */
+  public PDAnnotationPopup() {
+    getCOSObject().setName(COSName.SUBTYPE, PDAnnotationPopup.SUB_TYPE);
+  }
+
+  /**
+   * Creates a popup annotation from a COSDictionary, expected to be a correct
+   * object definition.
+   *
+   * @param field the PDF object to represent as a field.
+   */
+  public PDAnnotationPopup(final COSDictionary field) {
+    super(field);
+  }
+
+  /**
+   * This will set inital state of the annotation, open or closed.
+   *
+   * @param open Boolean value, true = open false = closed.
+   */
+  public void setOpen(final boolean open) {
+    getCOSObject().setBoolean("Open", open);
+  }
+
+  /**
+   * This will retrieve the initial state of the annotation, open Or closed
+   * (default closed).
+   *
+   * @return The initial state, true = open false = closed.
+   */
+  public boolean getOpen() {
+    return getCOSObject().getBoolean("Open", false);
+  }
+
+  /**
+   * This will set the markup annotation which this popup relates to.
+   *
+   * @param annot the markup annotation.
+   */
+  public void setParent(final PDAnnotationMarkup annot) {
+    getCOSObject().setItem(COSName.PARENT, annot.getCOSObject());
+  }
+
+  /**
+   * This will retrieve the markup annotation which this popup relates to.
+   *
+   * @return The parent markup annotation.
+   */
+  public PDAnnotationMarkup getParent() {
+    PDAnnotationMarkup am = null;
+    try {
+      am = (PDAnnotationMarkup) PDAnnotation
+          .createAnnotation(getCOSObject().getDictionaryObject(COSName.PARENT, COSName.P));
+    } catch (final IOException ioe) {
+      PDAnnotationPopup.LOG.debug("An exception while trying to get the parent markup - ignoring", ioe);
+      // Couldn't construct the annotation, so return null i.e. do nothing
     }
-
-    /**
-     * Creates a popup annotation from a COSDictionary, expected to be a correct object definition.
-     *
-     * @param field the PDF object to represent as a field.
-     */
-    public PDAnnotationPopup(COSDictionary field)
-    {
-        super(field);
-    }
-
-    /**
-     * This will set inital state of the annotation, open or closed.
-     *
-     * @param open Boolean value, true = open false = closed.
-     */
-    public void setOpen(boolean open)
-    {
-        getCOSObject().setBoolean("Open", open);
-    }
-
-    /**
-     * This will retrieve the initial state of the annotation, open Or closed (default closed).
-     *
-     * @return The initial state, true = open false = closed.
-     */
-    public boolean getOpen()
-    {
-        return getCOSObject().getBoolean("Open", false);
-    }
-
-    /**
-     * This will set the markup annotation which this popup relates to.
-     *
-     * @param annot the markup annotation.
-     */
-    public void setParent(PDAnnotationMarkup annot)
-    {
-        getCOSObject().setItem(COSName.PARENT, annot.getCOSObject());
-    }
-
-    /**
-     * This will retrieve the markup annotation which this popup relates to.
-     *
-     * @return The parent markup annotation.
-     */
-    public PDAnnotationMarkup getParent()
-    {
-        PDAnnotationMarkup am = null;
-        try
-        {
-            am = (PDAnnotationMarkup) PDAnnotation.createAnnotation(getCOSObject()
-                    .getDictionaryObject(COSName.PARENT, COSName.P));
-        }
-        catch (IOException ioe)
-        {
-            LOG.debug("An exception while trying to get the parent markup - ignoring", ioe);
-            // Couldn't construct the annotation, so return null i.e. do nothing
-        }
-        return am;
-    }
+    return am;
+  }
 
 }
