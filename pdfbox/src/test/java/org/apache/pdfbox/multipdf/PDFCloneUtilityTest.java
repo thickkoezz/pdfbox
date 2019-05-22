3a827dac93e9c70905aaf2d84fb973af87b6b324
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 11:34:28 +0700

    clean it up

--- pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFCloneUtilityTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFCloneUtilityTest.java
@@ -18,11 +18,13 @@ package org.apache.pdfbox.multipdf;
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
-import junit.framework.TestCase;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
+
+import junit.framework.TestCase;
 
 /**
  * Test suite for PDFCloneUtility, see PDFBOX-2052.
@@ -30,75 +32,67 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
  * @author Cornelis Hoeflake
  * @author Tilman Hausherr
  */
-public class PDFCloneUtilityTest extends TestCase
-{
-    /**
-     * original (minimal) test from PDFBOX-2052.
-     * 
-     * @throws IOException 
-     */
-    public void testClonePDFWithCosArrayStream() throws IOException
-    {
-        try (PDDocument srcDoc = new PDDocument();
-             PDDocument dstDoc = new PDDocument())
-        {
+public class PDFCloneUtilityTest extends TestCase {
+  /**
+   * original (minimal) test from PDFBOX-2052.
+   * 
+   * @throws IOException
+   */
+  public void testClonePDFWithCosArrayStream() throws IOException {
+    try (PDDocument srcDoc = new PDDocument(); PDDocument dstDoc = new PDDocument()) {
 
-            PDPage pdPage = new PDPage();
-            srcDoc.addPage(pdPage);
-            new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, true).close();
-            new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, true).close();
-            new PDFCloneUtility(dstDoc).cloneForNewDocument(pdPage.getCOSObject());
-        }
+      final PDPage pdPage = new PDPage();
+      srcDoc.addPage(pdPage);
+      new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, true).close();
+      new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, true).close();
+      new PDFCloneUtility(dstDoc).cloneForNewDocument(pdPage.getCOSObject());
+    }
+  }
+
+  /**
+   * broader test that saves to a real PDF document.
+   * 
+   * @throws IOException
+   */
+  public void testClonePDFWithCosArrayStream2() throws IOException {
+    final String TESTDIR = "target/test-output/clone/";
+    final String CLONESRC = "clone-src.pdf";
+    final String CLONEDST = "clone-dst.pdf";
+
+    new File(TESTDIR).mkdirs();
+
+    final PDDocument srcDoc = new PDDocument();
+    final PDPage pdPage = new PDPage();
+    srcDoc.addPage(pdPage);
+    try (PDPageContentStream pdPageContentStream1 = new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, false)) {
+      pdPageContentStream1.setNonStrokingColor(Color.black);
+      pdPageContentStream1.addRect(100, 600, 300, 100);
+      pdPageContentStream1.fill();
+    }
+    try (PDPageContentStream pdPageContentStream2 = new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, false)) {
+      pdPageContentStream2.setNonStrokingColor(Color.red);
+      pdPageContentStream2.addRect(100, 500, 300, 100);
+      pdPageContentStream2.fill();
+    }
+    try (PDPageContentStream pdPageContentStream3 = new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, false)) {
+      pdPageContentStream3.setNonStrokingColor(Color.yellow);
+      pdPageContentStream3.addRect(100, 400, 300, 100);
+      pdPageContentStream3.fill();
     }
 
-    /**
-     * broader test that saves to a real PDF document.
-     * 
-     * @throws IOException 
-     */
-    public void testClonePDFWithCosArrayStream2() throws IOException
-    {
-        final String TESTDIR = "target/test-output/clone/";
-        final String CLONESRC = "clone-src.pdf";
-        final String CLONEDST = "clone-dst.pdf";
+    srcDoc.save(TESTDIR + CLONESRC);
+    final PDFMergerUtility merger = new PDFMergerUtility();
+    final PDDocument dstDoc = new PDDocument();
 
-        new File(TESTDIR).mkdirs();
+    // this calls PDFCloneUtility.cloneForNewDocument(),
+    // which would fail before the fix in PDFBOX-2052
+    merger.appendDocument(dstDoc, srcDoc);
 
-        PDDocument srcDoc = new PDDocument();
-        PDPage pdPage = new PDPage();
-        srcDoc.addPage(pdPage);
-        try (PDPageContentStream pdPageContentStream1 = new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, false))
-        {
-            pdPageContentStream1.setNonStrokingColor(Color.black);
-            pdPageContentStream1.addRect(100, 600, 300, 100);
-            pdPageContentStream1.fill();
-        }
-        try (PDPageContentStream pdPageContentStream2 = new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, false))
-        {
-            pdPageContentStream2.setNonStrokingColor(Color.red);
-            pdPageContentStream2.addRect(100, 500, 300, 100);
-            pdPageContentStream2.fill();
-        }
-        try (PDPageContentStream pdPageContentStream3 = new PDPageContentStream(srcDoc, pdPage, AppendMode.APPEND, false))
-        {
-            pdPageContentStream3.setNonStrokingColor(Color.yellow);
-            pdPageContentStream3.addRect(100, 400, 300, 100);
-            pdPageContentStream3.fill();
-        }
-
-        srcDoc.save(TESTDIR + CLONESRC);
-        PDFMergerUtility merger = new PDFMergerUtility();
-        PDDocument dstDoc = new PDDocument();
-
-        // this calls PDFCloneUtility.cloneForNewDocument(), 
-        // which would fail before the fix in PDFBOX-2052
-        merger.appendDocument(dstDoc, srcDoc);
-
-        // save and reload PDF, so that one can see that the files are legit
-        dstDoc.save(TESTDIR + CLONEDST);
-        PDDocument.load(new File(TESTDIR + CLONESRC)).close();
-        PDDocument.load(new File(TESTDIR + CLONESRC), (String)null).close();
-        PDDocument.load(new File(TESTDIR + CLONEDST)).close();
-        PDDocument.load(new File(TESTDIR + CLONEDST), (String)null).close();
-    }
+    // save and reload PDF, so that one can see that the files are legit
+    dstDoc.save(TESTDIR + CLONEDST);
+    PDDocument.load(new File(TESTDIR + CLONESRC)).close();
+    PDDocument.load(new File(TESTDIR + CLONESRC), (String) null).close();
+    PDDocument.load(new File(TESTDIR + CLONEDST)).close();
+    PDDocument.load(new File(TESTDIR + CLONEDST), (String) null).close();
+  }
 }
