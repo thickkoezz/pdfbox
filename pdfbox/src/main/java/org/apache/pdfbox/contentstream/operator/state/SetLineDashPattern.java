3f7d7c49528347ab2a775d6d4e6e0cddec74add2
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:34:30 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetLineDashPattern.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/SetLineDashPattern.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
@@ -34,60 +33,45 @@ import org.apache.pdfbox.cos.COSNumber;
  *
  * @author Ben Litchfield
  */
-public class SetLineDashPattern extends OperatorProcessor
-{
-    private static final Log LOG = LogFactory.getLog(SetLineDashPattern.class);
-    
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws MissingOperandException
-    {
-        if (arguments.size() < 2)
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
-        COSBase base0 = arguments.get(0);
-        if (!(base0 instanceof COSArray))
-        {
-            return;
-        }
-        COSBase base1 = arguments.get(1);
-        if (!(base1 instanceof COSNumber))
-        {
-            return;
-        }
-        COSArray dashArray = (COSArray) base0;
-        int dashPhase = ((COSNumber) base1).intValue();
-        
-        boolean allZero = true;
-        for (COSBase base : dashArray)
-        {
-            if (base instanceof COSNumber)
-            {
-                COSNumber num = (COSNumber) base;
-                if (Float.compare(num.floatValue(), 0) != 0)
-                {
-                    allZero = false;
-                    break;
-                }
-            }
-            else
-            {
-                LOG.warn("dash array has non number element " + base + ", ignored");
-                dashArray = new COSArray();
-                break;
-            }
-        }
-        if (dashArray.size() > 0 && allZero)
-        {
-            LOG.warn("dash lengths all zero, ignored");
-            dashArray = new COSArray();
-        }
-        context.setLineDashPattern(dashArray, dashPhase);
-    }
+public class SetLineDashPattern extends OperatorProcessor {
+  private static final Log LOG = LogFactory.getLog(SetLineDashPattern.class);
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.SET_LINE_DASHPATTERN;
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws MissingOperandException {
+    if (arguments.size() < 2)
+      throw new MissingOperandException(operator, arguments);
+    final COSBase base0 = arguments.get(0);
+    if (!(base0 instanceof COSArray))
+      return;
+    final COSBase base1 = arguments.get(1);
+    if (!(base1 instanceof COSNumber))
+      return;
+    COSArray dashArray = (COSArray) base0;
+    final int dashPhase = ((COSNumber) base1).intValue();
+
+    boolean allZero = true;
+    for (final COSBase base : dashArray) {
+      if (base instanceof COSNumber) {
+        final COSNumber num = (COSNumber) base;
+        if (Float.compare(num.floatValue(), 0) != 0) {
+          allZero = false;
+          break;
+        }
+      } else {
+        SetLineDashPattern.LOG.warn("dash array has non number element " + base + ", ignored");
+        dashArray = new COSArray();
+        break;
+      }
     }
+    if (dashArray.size() > 0 && allZero) {
+      SetLineDashPattern.LOG.warn("dash lengths all zero, ignored");
+      dashArray = new COSArray();
+    }
+    context.setLineDashPattern(dashArray, dashPhase);
+  }
+
+  @Override
+  public String getName() {
+    return OperatorName.SET_LINE_DASHPATTERN;
+  }
 }
