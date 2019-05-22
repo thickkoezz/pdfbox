294b05cbda2d3764c7df55e3a3ab451cbc6b8d6b
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 12:35:17 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetNonStrokingDeviceRGBColor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/color/SetNonStrokingDeviceRGBColor.java
@@ -16,34 +16,31 @@
  */
 package org.apache.pdfbox.contentstream.operator.color;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
-import org.apache.pdfbox.contentstream.operator.Operator;
-import org.apache.pdfbox.contentstream.operator.OperatorName;
-
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.pdfbox.contentstream.operator.Operator;
+import org.apache.pdfbox.contentstream.operator.OperatorName;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
+
 /**
- * rg: Set the non-stroking colour space to DeviceRGB and set the colour to
- * use for non-stroking operations.
+ * rg: Set the non-stroking colour space to DeviceRGB and set the colour to use
+ * for non-stroking operations.
  *
  * @author John Hewson
  */
-public class SetNonStrokingDeviceRGBColor extends SetNonStrokingColor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        PDColorSpace cs = context.getResources().getColorSpace(COSName.DEVICERGB);
-        context.getGraphicsState().setNonStrokingColorSpace(cs);
-        super.process(operator, arguments);
-    }
+public class SetNonStrokingDeviceRGBColor extends SetNonStrokingColor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    final PDColorSpace cs = context.getResources().getColorSpace(COSName.DEVICERGB);
+    context.getGraphicsState().setNonStrokingColorSpace(cs);
+    super.process(operator, arguments);
+  }
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.NON_STROKING_RGB;
-    }
+  @Override
+  public String getName() {
+    return OperatorName.NON_STROKING_RGB;
+  }
 }
