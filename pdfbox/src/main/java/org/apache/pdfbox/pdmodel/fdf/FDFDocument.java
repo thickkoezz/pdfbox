52c67be78f07ff10b52d1c523fa10f8e3d550144
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:54:19 +0700

    clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFDocument.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFDocument.java
@@ -22,8 +22,8 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -38,306 +38,274 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 /**
- * This is the in-memory representation of the FDF document. You need to call close() on this object when you are done
- * using it!!
+ * This is the in-memory representation of the FDF document. You need to call
+ * close() on this object when you are done using it!!
  *
  * @author Ben Litchfield
  */
-public class FDFDocument implements Closeable
-{
-    private COSDocument document;
+public class FDFDocument implements Closeable {
+  private final COSDocument document;
 
-    /**
-     * Constructor, creates a new FDF document.
-     *
-     */
-    public FDFDocument()
-    {
-        document = new COSDocument();
-        document.setVersion(1.2f);
+  /**
+   * Constructor, creates a new FDF document.
+   *
+   */
+  public FDFDocument() {
+    document = new COSDocument();
+    document.setVersion(1.2f);
 
-        // First we need a trailer
-        document.setTrailer(new COSDictionary());
+    // First we need a trailer
+    document.setTrailer(new COSDictionary());
 
-        // Next we need the root dictionary.
-        FDFCatalog catalog = new FDFCatalog();
-        setCatalog(catalog);
+    // Next we need the root dictionary.
+    final FDFCatalog catalog = new FDFCatalog();
+    setCatalog(catalog);
+  }
+
+  /**
+   * Constructor that uses an existing document. The COSDocument that is passed in
+   * must be valid.
+   *
+   * @param doc The COSDocument that this document wraps.
+   */
+  public FDFDocument(final COSDocument doc) {
+    document = doc;
+  }
+
+  /**
+   * This will create an FDF document from an XFDF XML document.
+   *
+   * @param doc The XML document that contains the XFDF data.
+   * @throws IOException If there is an error reading from the dom.
+   */
+  public FDFDocument(final Document doc) throws IOException {
+    this();
+    final Element xfdf = doc.getDocumentElement();
+    if (!xfdf.getNodeName().equals("xfdf"))
+      throw new IOException(
+          "Error while importing xfdf document, " + "root should be 'xfdf' and not '" + xfdf.getNodeName() + "'");
+    final FDFCatalog cat = new FDFCatalog(xfdf);
+    setCatalog(cat);
+  }
+
+  /**
+   * This will write this element as an XML document.
+   *
+   * @param output The stream to write the xml to.
+   *
+   * @throws IOException If there is an error writing the XML.
+   */
+  public void writeXML(final Writer output) throws IOException {
+    output.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+    output.write("<xfdf xmlns=\"http://ns.adobe.com/xfdf/\" xml:space=\"preserve\">\n");
+
+    getCatalog().writeXML(output);
+
+    output.write("</xfdf>\n");
+  }
+
+  /**
+   * This will get the low level document.
+   *
+   * @return The document that this layer sits on top of.
+   */
+  public COSDocument getDocument() {
+    return document;
+  }
+
+  /**
+   * This will get the FDF Catalog. This is guaranteed to not return null.
+   *
+   * @return The documents /Root dictionary
+   */
+  public FDFCatalog getCatalog() {
+    FDFCatalog retval = null;
+    final COSDictionary trailer = document.getTrailer();
+    final COSDictionary root = trailer.getCOSDictionary(COSName.ROOT);
+    if (root == null) {
+      retval = new FDFCatalog();
+      setCatalog(retval);
+    } else {
+      retval = new FDFCatalog(root);
     }
+    return retval;
+  }
 
-    /**
-     * Constructor that uses an existing document. The COSDocument that is passed in must be valid.
-     *
-     * @param doc The COSDocument that this document wraps.
-     */
-    public FDFDocument(COSDocument doc)
-    {
-        document = doc;
+  /**
+   * This will set the FDF catalog for this FDF document.
+   *
+   * @param cat The FDF catalog.
+   */
+  public final void setCatalog(final FDFCatalog cat) {
+    final COSDictionary trailer = document.getTrailer();
+    trailer.setItem(COSName.ROOT, cat);
+  }
+
+  /**
+   * This will load a document from a file.
+   *
+   * @param filename The name of the file to load.
+   *
+   * @return The document that was loaded.
+   *
+   * @throws IOException If there is an error reading from the stream.
+   */
+  public static FDFDocument load(final String filename) throws IOException {
+    final FDFParser parser = new FDFParser(filename);
+    parser.parse();
+    return new FDFDocument(parser.getDocument());
+  }
+
+  /**
+   * This will load a document from a file.
+   *
+   * @param file The name of the file to load.
+   *
+   * @return The document that was loaded.
+   *
+   * @throws IOException If there is an error reading from the stream.
+   */
+  public static FDFDocument load(final File file) throws IOException {
+    final FDFParser parser = new FDFParser(file);
+    parser.parse();
+    return new FDFDocument(parser.getDocument());
+  }
+
+  /**
+   * This will load a document from an input stream.
+   *
+   * @param input The stream that contains the document.
+   *
+   * @return The document that was loaded.
+   *
+   * @throws IOException If there is an error reading from the stream.
+   */
+  public static FDFDocument load(final InputStream input) throws IOException {
+    final FDFParser parser = new FDFParser(input);
+    parser.parse();
+    return new FDFDocument(parser.getDocument());
+  }
+
+  /**
+   * This will load a document from a file.
+   *
+   * @param filename The name of the file to load.
+   *
+   * @return The document that was loaded.
+   *
+   * @throws IOException If there is an error reading from the stream.
+   */
+  public static FDFDocument loadXFDF(final String filename) throws IOException {
+    return FDFDocument.loadXFDF(new BufferedInputStream(new FileInputStream(filename)));
+  }
+
+  /**
+   * This will load a document from a file.
+   *
+   * @param file The name of the file to load.
+   *
+   * @return The document that was loaded.
+   *
+   * @throws IOException If there is an error reading from the stream.
+   */
+  public static FDFDocument loadXFDF(final File file) throws IOException {
+    return FDFDocument.loadXFDF(new BufferedInputStream(new FileInputStream(file)));
+  }
+
+  /**
+   * This will load a document from an input stream.
+   *
+   * @param input The stream that contains the document.
+   *
+   * @return The document that was loaded.
+   *
+   * @throws IOException If there is an error reading from the stream.
+   */
+  public static FDFDocument loadXFDF(final InputStream input) throws IOException {
+    return new FDFDocument(XMLUtil.parse(input));
+  }
+
+  /**
+   * This will save this document to the filesystem.
+   *
+   * @param fileName The file to save as.
+   *
+   * @throws IOException If there is an error saving the document.
+   */
+  public void save(final File fileName) throws IOException {
+    save(new FileOutputStream(fileName));
+  }
+
+  /**
+   * This will save this document to the filesystem.
+   *
+   * @param fileName The file to save as.
+   *
+   * @throws IOException If there is an error saving the document.
+   */
+  public void save(final String fileName) throws IOException {
+    save(new FileOutputStream(fileName));
+  }
+
+  /**
+   * This will save the document to an output stream.
+   *
+   * @param output The stream to write to.
+   *
+   * @throws IOException If there is an error writing the document.
+   */
+  public void save(final OutputStream output) throws IOException {
+    try (COSWriter writer = new COSWriter(output)) {
+      writer.write(this);
     }
+  }
 
-    /**
-     * This will create an FDF document from an XFDF XML document.
-     *
-     * @param doc The XML document that contains the XFDF data.
-     * @throws IOException If there is an error reading from the dom.
-     */
-    public FDFDocument(Document doc) throws IOException
-    {
-        this();
-        Element xfdf = doc.getDocumentElement();
-        if (!xfdf.getNodeName().equals("xfdf"))
-        {
-            throw new IOException("Error while importing xfdf document, "
-                    + "root should be 'xfdf' and not '" + xfdf.getNodeName() + "'");
-        }
-        FDFCatalog cat = new FDFCatalog(xfdf);
-        setCatalog(cat);
+  /**
+   * This will save this document to the filesystem.
+   *
+   * @param fileName The file to save as.
+   *
+   * @throws IOException If there is an error saving the document.
+   */
+  public void saveXFDF(final File fileName) throws IOException {
+    saveXFDF(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8")));
+  }
+
+  /**
+   * This will save this document to the filesystem.
+   *
+   * @param fileName The file to save as.
+   *
+   * @throws IOException If there is an error saving the document.
+   */
+  public void saveXFDF(final String fileName) throws IOException {
+    saveXFDF(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8")));
+  }
+
+  /**
+   * This will save the document to an output stream and close the stream.
+   *
+   * @param output The stream to write to.
+   *
+   * @throws IOException If there is an error writing the document.
+   */
+  public void saveXFDF(final Writer output) throws IOException {
+    try {
+      writeXML(output);
+    } finally {
+      if (output != null) {
+        output.close();
+      }
     }
+  }
 
-    /**
-     * This will write this element as an XML document.
-     *
-     * @param output The stream to write the xml to.
-     *
-     * @throws IOException If there is an error writing the XML.
-     */
-    public void writeXML(Writer output) throws IOException
-    {
-        output.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-        output.write("<xfdf xmlns=\"http://ns.adobe.com/xfdf/\" xml:space=\"preserve\">\n");
-
-        getCatalog().writeXML(output);
-
-        output.write("</xfdf>\n");
-    }
-
-    /**
-     * This will get the low level document.
-     *
-     * @return The document that this layer sits on top of.
-     */
-    public COSDocument getDocument()
-    {
-        return document;
-    }
-
-    /**
-     * This will get the FDF Catalog. This is guaranteed to not return null.
-     *
-     * @return The documents /Root dictionary
-     */
-    public FDFCatalog getCatalog()
-    {
-        FDFCatalog retval = null;
-        COSDictionary trailer = document.getTrailer();
-        COSDictionary root = trailer.getCOSDictionary(COSName.ROOT);
-        if (root == null)
-        {
-            retval = new FDFCatalog();
-            setCatalog(retval);
-        }
-        else
-        {
-            retval = new FDFCatalog(root);
-        }
-        return retval;
-    }
-
-    /**
-     * This will set the FDF catalog for this FDF document.
-     *
-     * @param cat The FDF catalog.
-     */
-    public final void setCatalog(FDFCatalog cat)
-    {
-        COSDictionary trailer = document.getTrailer();
-        trailer.setItem(COSName.ROOT, cat);
-    }
-
-    /**
-     * This will load a document from a file.
-     *
-     * @param filename The name of the file to load.
-     *
-     * @return The document that was loaded.
-     *
-     * @throws IOException If there is an error reading from the stream.
-     */
-    public static FDFDocument load(String filename) throws IOException
-    {
-        FDFParser parser = new FDFParser(filename);
-        parser.parse();
-        return new FDFDocument(parser.getDocument());
-    }
-
-    /**
-     * This will load a document from a file.
-     *
-     * @param file The name of the file to load.
-     *
-     * @return The document that was loaded.
-     *
-     * @throws IOException If there is an error reading from the stream.
-     */
-    public static FDFDocument load(File file) throws IOException
-    {
-        FDFParser parser = new FDFParser(file);
-        parser.parse();
-        return new FDFDocument(parser.getDocument());
-    }
-
-    /**
-     * This will load a document from an input stream.
-     *
-     * @param input The stream that contains the document.
-     *
-     * @return The document that was loaded.
-     *
-     * @throws IOException If there is an error reading from the stream.
-     */
-    public static FDFDocument load(InputStream input) throws IOException
-    {
-        FDFParser parser = new FDFParser(input);
-        parser.parse();
-        return new FDFDocument(parser.getDocument());
-    }
-
-    /**
-     * This will load a document from a file.
-     *
-     * @param filename The name of the file to load.
-     *
-     * @return The document that was loaded.
-     *
-     * @throws IOException If there is an error reading from the stream.
-     */
-    public static FDFDocument loadXFDF(String filename) throws IOException
-    {
-        return loadXFDF(new BufferedInputStream(new FileInputStream(filename)));
-    }
-
-    /**
-     * This will load a document from a file.
-     *
-     * @param file The name of the file to load.
-     *
-     * @return The document that was loaded.
-     *
-     * @throws IOException If there is an error reading from the stream.
-     */
-    public static FDFDocument loadXFDF(File file) throws IOException
-    {
-        return loadXFDF(new BufferedInputStream(new FileInputStream(file)));
-    }
-
-    /**
-     * This will load a document from an input stream.
-     *
-     * @param input The stream that contains the document.
-     *
-     * @return The document that was loaded.
-     *
-     * @throws IOException If there is an error reading from the stream.
-     */
-    public static FDFDocument loadXFDF(InputStream input) throws IOException
-    {
-        return new FDFDocument(XMLUtil.parse(input));
-    }
-
-    /**
-     * This will save this document to the filesystem.
-     *
-     * @param fileName The file to save as.
-     *
-     * @throws IOException If there is an error saving the document.
-     */
-    public void save(File fileName) throws IOException
-    {
-        save(new FileOutputStream(fileName));
-    }
-
-    /**
-     * This will save this document to the filesystem.
-     *
-     * @param fileName The file to save as.
-     *
-     * @throws IOException If there is an error saving the document.
-     */
-    public void save(String fileName) throws IOException
-    {
-        save(new FileOutputStream(fileName));
-    }
-
-    /**
-     * This will save the document to an output stream.
-     *
-     * @param output The stream to write to.
-     *
-     * @throws IOException If there is an error writing the document.
-     */
-    public void save(OutputStream output) throws IOException
-    {
-        try (COSWriter writer = new COSWriter(output))
-        {
-            writer.write(this);
-        }
-    }
-
-    /**
-     * This will save this document to the filesystem.
-     *
-     * @param fileName The file to save as.
-     *
-     * @throws IOException If there is an error saving the document.
-     */
-    public void saveXFDF(File fileName) throws IOException
-    {
-        saveXFDF(new BufferedWriter(
-                new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8")));
-    }
-
-    /**
-     * This will save this document to the filesystem.
-     *
-     * @param fileName The file to save as.
-     *
-     * @throws IOException If there is an error saving the document.
-     */
-    public void saveXFDF(String fileName) throws IOException
-    {
-        saveXFDF(new BufferedWriter(
-                new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8")));
-    }
-
-    /**
-     * This will save the document to an output stream and close the stream.
-     *
-     * @param output The stream to write to.
-     *
-     * @throws IOException If there is an error writing the document.
-     */
-    public void saveXFDF(Writer output) throws IOException
-    {
-        try
-        {
-            writeXML(output);
-        }
-        finally
-        {
-            if (output != null)
-            {
-                output.close();
-            }
-        }
-    }
-
-    /**
-     * This will close the underlying COSDocument object.
-     *
-     * @throws IOException If there is an error releasing resources.
-     */
-    @Override
-    public void close() throws IOException
-    {
-        document.close();
-    }
+  /**
+   * This will close the underlying COSDocument object.
+   *
+   * @throws IOException If there is an error releasing resources.
+   */
+  @Override
+  public void close() throws IOException {
+    document.close();
+  }
 }
