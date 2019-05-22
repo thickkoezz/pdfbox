9dfe9a4502b50c80cf5b080dc3ffdaf9c3ebccd0
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:31:50 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/Concatenate.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/state/Concatenate.java
@@ -16,53 +16,46 @@
  */
 package org.apache.pdfbox.contentstream.operator.state;
 
-import java.util.List;
 import java.io.IOException;
-import org.apache.pdfbox.contentstream.operator.MissingOperandException;
+import java.util.List;
 
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
-import org.apache.pdfbox.util.Matrix;
+import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSNumber;
+import org.apache.pdfbox.util.Matrix;
 
 /**
  * cm: Concatenate matrix to current transformation matrix.
  *
  * @author Laurent Huault
  */
-public class Concatenate extends OperatorProcessor
-{
-    @Override
-    public void process(Operator operator, List<COSBase> arguments) throws IOException
-    {
-        if (arguments.size() < 6)
-        {
-            throw new MissingOperandException(operator, arguments);
-        }
-        if (!checkArrayTypesClass(arguments, COSNumber.class))
-        {
-            return;
-        }
-        
-        // concatenate matrix to current transformation matrix
-        COSNumber a = (COSNumber) arguments.get(0);
-        COSNumber b = (COSNumber) arguments.get(1);
-        COSNumber c = (COSNumber) arguments.get(2);
-        COSNumber d = (COSNumber) arguments.get(3);
-        COSNumber e = (COSNumber) arguments.get(4);
-        COSNumber f = (COSNumber) arguments.get(5);
+public class Concatenate extends OperatorProcessor {
+  @Override
+  public void process(final Operator operator, final List<COSBase> arguments) throws IOException {
+    if (arguments.size() < 6)
+      throw new MissingOperandException(operator, arguments);
+    if (!checkArrayTypesClass(arguments, COSNumber.class))
+      return;
 
-        Matrix matrix = new Matrix(a.floatValue(), b.floatValue(), c.floatValue(),
-                                   d.floatValue(), e.floatValue(), f.floatValue());
+    // concatenate matrix to current transformation matrix
+    final COSNumber a = (COSNumber) arguments.get(0);
+    final COSNumber b = (COSNumber) arguments.get(1);
+    final COSNumber c = (COSNumber) arguments.get(2);
+    final COSNumber d = (COSNumber) arguments.get(3);
+    final COSNumber e = (COSNumber) arguments.get(4);
+    final COSNumber f = (COSNumber) arguments.get(5);
 
-        context.getGraphicsState().getCurrentTransformationMatrix().concatenate(matrix);
-    }
+    final Matrix matrix = new Matrix(a.floatValue(), b.floatValue(), c.floatValue(), d.floatValue(), e.floatValue(),
+        f.floatValue());
 
-    @Override
-    public String getName()
-    {
-        return OperatorName.CONCAT;
-    }
+    context.getGraphicsState().getCurrentTransformationMatrix().concatenate(matrix);
+  }
+
+  @Override
+  public String getName() {
+    return OperatorName.CONCAT;
+  }
 }
