87580646559034d4e2d30fe63a8410079adcbc77
Mieky Sofyan Yudinata <miki211287@gmail.com>
2019-05-22 10:40:14 +0700

    Clean it up

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/DecryptionMaterial.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/DecryptionMaterial.java
@@ -18,12 +18,11 @@
 package org.apache.pdfbox.pdmodel.encryption;
 
 /**
- * This class represents data required to decrypt PDF documents. This can
- * be a password for standard security or a X509 certificate with a private
- * key for public key security.
+ * This class represents data required to decrypt PDF documents. This can be a
+ * password for standard security or a X509 certificate with a private key for
+ * public key security.
  *
  * @author Benoit Guillon
  */
-public abstract class DecryptionMaterial
-{
+public abstract class DecryptionMaterial {
 }
